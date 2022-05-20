139888e077d6031f75bf4749ef177eb95455106e
Brendan Woo <woo.brendan@gmail.com>
2022-05-20 17:33:33 -0500

    add driver search component into main

--- podium-helper/src/App.js
+++ podium-helper/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import DriverSearch from './components/DriverSearch';
 import Series from './components/Series';
 
 function App() {
   return (
     <div className="App">
       <Series />
+      <DriverSearch />
     </div>
   );
 }
