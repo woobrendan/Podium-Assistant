a1b018382bce7af040e754abcda140c523fef8c4
Brendan Woo <woo.brendan@gmail.com>
2022-05-23 09:23:51 -0400

    set up component for class and placement selector

---
+++ podium-helper/src/components/Placement.js
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+
+const classification = ['Pro', 'Pro-Am', 'Silver', 'Am']
+
+function Placement() {
+  const [finish, setFinish] = useState('');
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Placement
