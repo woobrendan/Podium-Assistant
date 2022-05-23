0a53ad31a17b0251ba6cdebf9b3fe981a6f9cbc8
Brendan Woo <woo.brendan@gmail.com>
2022-05-23 09:54:55 -0400

    wrap boxes in card and card content

--- podium-helper/src/components/Placement.js
+++ podium-helper/src/components/Placement.js
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import Box from '@mui/material/Box';
 import MenuItem from '@mui/material/MenuItem';
-import { FormControl, InputLabel, Select } from '@mui/material';
+import { FormControl, InputLabel, Select, Card, CardContent } from '@mui/material';
 
 const classification = ['Pro', 'Pro-Am', 'Silver', 'Am'];
 const placement = ['1st', '2nd', '3rd'];
@@ -26,52 +26,54 @@ function Placement() {
   ))
 
   return (
-    <>
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <FormControl sx={{ minWidth: 300 }}>
-        <InputLabel htmlFor="exampleFormControlSelect2">
-          Class/Rating
-        </InputLabel>
-        <Select
-          className="form-control"
-          name="Rating"
-          value={classRating}
-          onChange={handleChange}
+    <Card>
+      <CardContent>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
         >
-          {mappedClassification}
-        </Select>
-      </FormControl>
-    </Box>
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <FormControl sx={{ minWidth: 300 }}>
-        <InputLabel htmlFor="exampleFormControlSelect2">
-          Placement
-        </InputLabel>
-        <Select
-          className="form-control"
-          name="Placement"
-          value={finish}
-          onChange={handleChange}
+          <FormControl sx={{ minWidth: 300 }}>
+            <InputLabel htmlFor="exampleFormControlSelect2">
+              Class/Rating
+            </InputLabel>
+            <Select
+              className="form-control"
+              name="Rating"
+              value={classRating}
+              onChange={handleChange}
+            >
+              {mappedClassification}
+            </Select>
+          </FormControl>
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
         >
-          {mappedplacement}
-        </Select>
-      </FormControl>
-    </Box>
-  </>
+          <FormControl sx={{ minWidth: 300 }}>
+            <InputLabel htmlFor="exampleFormControlSelect2">
+              Placement
+            </InputLabel>
+            <Select
+              className="form-control"
+              name="Placement"
+              value={finish}
+              onChange={handleChange}
+            >
+              {mappedplacement}
+            </Select>
+          </FormControl>
+        </Box>
+    </CardContent>
+  </Card>
   )
 }
 
