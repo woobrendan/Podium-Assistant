0c5894ad0f587ea5636a2fe0f54d247ff70cace4
Brendan Woo <woo.brendan@gmail.com>
2022-05-23 09:47:24 -0400

    import placement component to see changes

--- podium-helper/src/App.js
+++ podium-helper/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Placement from './components/Placement';
 import Series from './components/Series';
 
 function App() {
   return (
     <div className="App">
       <Series />
+      <Placement />
     </div>
   );
 }
