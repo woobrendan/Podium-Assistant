fb09aa26fd55e0a3892cb774de10e5f8127c1b8d
Brendan Woo <woo.brendan@gmail.com>
2022-05-23 09:11:25 -0400

    remove driversearch component and move to series component

--- podium-helper/src/App.js
+++ podium-helper/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import DriverSearch from './components/DriverSearch';
 import Series from './components/Series';
 
 function App() {
   return (
     <div className="App">
       <Series />
-      <DriverSearch />
     </div>
   );
 }
