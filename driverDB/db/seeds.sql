6e30ee8fe22df251c49a51e8687e91382ac38ec4
Brendan Woo <woo.brendan@gmail.com>
2022-05-24 17:47:32 -0400

    add seeds for gt3 teams

--- driverDB/db/seeds.sql
+++ driverDB/db/seeds.sql
@@ -1,4 +1,21 @@
-INSERT INTO vehicles (manufacturer, model,  number, classification)
+INSERT INTO teams (name, country) VALUES
+('K-PAX', 'USA'),
+('Crowdstrike Racing with Riley Motorsports', 'USA'),
+('US Racetronics', 'USA'),
+('DXDT Racing', 'USA'),
+('TR3 Racing', 'USA'),
+('Ian Lacy Racing', 'USA'),
+('Triarsi Competizione', 'USA'),
+('RealTime Racing', 'USA'),
+('Wright Motorsports', 'USA'),
+('AF CORSE', 'USA'),
+('Zelus Motorsports', 'USA'),
+('Racers Edge Motorsports', 'USA'),
+('BimmerWorld', 'USA'),
+('Turner Motorsport', 'USA'),
+('ST Racing', 'Canada');
+
+INSERT INTO vehicles (manufacturer, model,  number, classification, team_id)
 VALUES
 ('Lamborghini', 'Huracan GT3', "1", 'Pro'),
 ('Lamborghini', 'Huracan GT3', "3", 'Pro'),
