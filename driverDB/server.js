e129219d7f08e0d13eba3dd8a4fb97345cffae0e
Brendan Woo <woo.brendan@gmail.com>
2022-05-24 17:15:22 -0400

    create folder for db and schema/seeds

--- driverDB/server.js
+++ driverDB/server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-const pool = require('')
+const pool = require('./lib/db');
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
