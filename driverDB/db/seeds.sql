c98a2c9850d452bd5cf566a3a37684dba5026bb2
Brendan Woo <woo.brendan@gmail.com>
2022-05-24 17:39:12 -0400

    set up initial vehicle seeds

---
+++ driverDB/db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO vehicles (manufacturer, model,  number, classification)
+VALUES
+('Lamborghini', 'Huracan GT3', "1", 'Pro'),
+('Lamborghini', 'Huracan GT3', "3", 'Pro'),
+('Mercedes', 'AMG GT3', "04", 'Pro-Am'),
+('Mercedes', 'AMG GT3', "6", 'Pro'),
+('Mercedes', 'AMG GT3', "08", 'Pro-Am'),
+('Lamborghini', 'Huracan GT3', "9", 'Pro-Am');
