039fe0c319d8e210ca85782a5b50bfb26ed0aac6
Brendan Woo <woo.brendan@gmail.com>
2022-05-30 14:46:06 -0400

    fix props calls, to props.entry

--- podium-helper/src/components/DriverDetails.js
+++ podium-helper/src/components/DriverDetails.js
@@ -27,29 +27,31 @@ function DriverDetails(props) {
       <CardMedia
         component="img"
         height="140"
-        image={props.carImage}
-        alt={props.car}
+        image={props.entry.carImage}
+        alt={props.entry.car}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {props.team} #{props.number}
+          {props.entry.team} - #{props.entry.number} - {props.entry.classification}
         </Typography>
         <table>
+          <tbody>
           <tr>
             <th>Driver</th>
             <th>Nationality</th>
             <th>Rating</th>
           </tr>
           <tr>
-            <td>{props.driver1.name}</td>
-            <td>{props.driver1.nationality}</td>
-            <td>{props.driver1.rating}</td>
+            <td>{props.entry.driver1.name}</td>
+            <td>{props.entry.driver1.nationality}</td>
+            <td>{props.entry.driver1.rating}</td>
           </tr>
           <tr>
-            <td>{props.driver2.name}</td>
-            <td>{props.driver2.nationality}</td>
-            <td>{props.driver2.rating}</td>
+            <td>{props.entry.driver2.name}</td>
+            <td>{props.entry.driver2.nationality}</td>
+            <td>{props.entry.driver2.rating}</td>
           </tr>
+          </tbody>
         </table>
       </CardContent>
     </Card>
