8b34071303527aded61ddcdbb79b4bfcec04308f
Brendan Woo <woo.brendan@gmail.com>
2022-05-30 14:33:48 -0400

    comment out the variations for series and change to only map through new driver sample data

--- podium-helper/src/components/DriverSearch.js
+++ podium-helper/src/components/DriverSearch.js
@@ -1,16 +1,34 @@
-import { GTWCADrivers, PGT4ADrivers } from '../drivers'
+import { GTWCADrivers, PGT4ADrivers, driverInfo } from '../drivers'
+
+// {
+//   team: "K-PAX",
+//   driver1: {
+//     name: 'Michele Beretta',
+//     nationality: 'Italy',
+//     rating: 'Silver'
+//     },
+//   driver2: {
+//     name: 'Andrea Calderlli',
+//     nationality: 'Italy',
+//     rating: 'Platinum'
+//     },
+//   car: "Lamborghini Huracan GT3",
+//   classification: "Pro",
+//   number: "6"
+// },
 
 function DriverSearch(props) {
-  const seriesDrivers = (choice) => {
-    switch (choice) {
-      case 'GT World Challenge America':
-        return GTWCADrivers;
-      case 'Pirelli GT4 America':
-        return PGT4ADrivers;
-    }
-  }
-  const mappedDrivers = seriesDrivers(props.seriesChoice).map((driver,index) => (
-        <li key={index}>{driver}</li>
+  // const seriesDrivers = (choice) => {
+  //   switch (choice) {
+  //     case 'GT World Challenge America':
+  //       return driverInfo;
+  //     case 'Pirelli GT4 America':
+  //       return PGT4ADrivers;
+  //   }
+  // }
+  // const mappedDrivers = seriesDrivers(props.seriesChoice).map((driver,index) => 
+  const mappedDrivers = driverInfo.map((entry,index) =>(
+        <li key={index}>{entry.driver1.name}</li>
       ))
   
   return (
