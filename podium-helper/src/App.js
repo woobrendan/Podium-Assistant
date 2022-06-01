2b0a546bad83880b12256973ca2fd7c8cd737d45
Brendan Woo <woo.brendan@gmail.com>
2022-05-31 20:04:42 -0400

    add route option for podium, import component

--- podium-helper/src/App.js
+++ podium-helper/src/App.js
@@ -3,6 +3,7 @@ import DriverSearch from './components/DriverSearch';
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Placement from './components/Placement';
 import Series from './components/Series';
+import Podium from './components/Podium';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Series />} />
           <Route path="/competitors" element={<DriverSearch />} />
+          <Route path='/podium' element={<Podium />} />
         </Routes>
       </div>
     </Router>
