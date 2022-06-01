195757090ee7db3c9ac2f24c34c9c5da4b245ee8
Brendan Woo <woo.brendan@gmail.com>
2022-06-01 17:10:56 -0400

    add classname to dive, change button stlying and coloring. import style sheet

--- podium-helper/src/components/Podium.js
+++ podium-helper/src/components/Podium.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import WinnerTop3 from './WinnerTop3';
+import { Button } from '@mui/material';
+import '../Styling/podium.scss'
 
 //make a function that grabs the usestate results from each race on submit
 function Podium() {
   return (
-    <div>
+    <div className="race-results-container">
       <WinnerTop3 />
       <WinnerTop3 />
       <WinnerTop3 />
+      <Button variant="contained" color="success">Submit</Button>
     </div>
   )
 }
