1cb7695c53894633c6cd50a2d5c0f653933edf0e
Brendan Woo <woo.brendan@gmail.com>
2022-06-02 14:35:51 -0400

    create file for fast lap section

---
+++ podium-helper/src/components/FastLap.js
@@ -0,0 +1,15 @@
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import MenuItem from '@mui/material/MenuItem';
+import { FormControl, InputLabel, Select } from '@mui/material';
+function FastLap() {
+  return (
+    <div className="results-container">
+      <Card sx={{ minWidth: 450 }}>
+
+      </Card>
+    </div>
+  )
+}
+
+export default FastLap
