274076de6981f3da90e8bcb391ed7b3cbe08e518
Brendan Woo <woo.brendan@gmail.com>
2022-06-02 16:27:41 -0400

    add results component that will house the final submission

---
+++ podium-helper/src/components/Results.js
@@ -0,0 +1,19 @@
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+
+function Results() {
+  return (
+    <div>
+      <Typography variant="h2" gutterBottom component="div">
+        Race Results
+      </Typography>
+      <Card sx={{ minWidth: 450 }}>
+        results from pro
+      </Card>
+      
+    </div>
+  )
+}
+
+export default Results
+
