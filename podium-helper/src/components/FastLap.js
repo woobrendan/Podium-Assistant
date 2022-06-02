ffec3cc84e990d80fd45cfd580277b293061fc1f
Brendan Woo <woo.brendan@gmail.com>
2022-06-02 14:49:21 -0400

    add box and textfield for fast lap time. create usestate to manage  value

--- podium-helper/src/components/FastLap.js
+++ podium-helper/src/components/FastLap.js
@@ -1,12 +1,34 @@
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import MenuItem from '@mui/material/MenuItem';
-import { FormControl, InputLabel, Select } from '@mui/material';
+import { FormControl, InputLabel, Select, TextField } from '@mui/material';
+import {useState} from 'react'
+
+
+
 function FastLap() {
+  const [fastTime, setFastTime] = useState('');
+  const handleChange = (event) => {
+    setFastTime(event.target.value)
+  }
   return (
     <div className="results-container">
       <Card sx={{ minWidth: 450 }}>
-
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+          id="outlined"
+          label="Fast Lap"
+          value={fastTime}
+          onChange={handleChange}
+        />
+        </Box>
       </Card>
     </div>
   )
