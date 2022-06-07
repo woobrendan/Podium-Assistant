afc4aaf6676786c0e3835b29804d18cefb653f99
Brendan Woo <woo.brendan@gmail.com>
2022-06-07 14:29:14 -0400

    change vehicle table from manuf and model to car

--- driverDB/db/schema.sql
+++ driverDB/db/schema.sql
@@ -17,8 +17,7 @@ CREATE TABLE teams  (
 
 CREATE TABLE vehicles(
   id SERIAL PRIMARY KEY NOT NULL,
-  manufacturer VARCHAR(255) NOT NULL,
-  model VARCHAR(255) NOT NULL,
+  car VARCHAR(255) NOT NULL,
   number VARCHAR(6),
   classification VARCHAR(255),
   team_id INTEGER REFERENCES teams(id) ON DELETE CASCADE,
