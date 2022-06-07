d0f8b72c4aa5dd8e9f5940da7c3a5348324d6e38
Brendan Woo <woo.brendan@gmail.com>
2022-06-07 14:29:32 -0400

    adjust vehicle values to be car instead of two columns for make n model

--- driverDB/db/seeds.sql
+++ driverDB/db/seeds.sql
@@ -21,27 +21,27 @@ INSERT INTO teams (name, country) VALUES
 ('Turner Motorsport', 'USA'),
 ('ST Racing', 'Canada');
 
-INSERT INTO vehicles (manufacturer, model,  number, classification, team_id, series_id)
+INSERT INTO vehicles (car,  number, classification, team_id, series_id)
 VALUES
-('Lamborghini', 'Huracan GT3', '1', 'Pro', 1, 1),
-('Lamborghini', 'Huracan GT3', '3', 'Pro', 1, 1),
-('Mercedes', 'AMG GT3', '04', 'Pro-Am', 2, 1),
-('Mercedes', 'AMG GT3', '6', 'Pro', 3, 1),
-('Mercedes', 'AMG GT3', '08', 'Pro-Am', 4, 1),
-('Lamborghini', 'Huracan GT3', '9', 'Pro-Am', 5, 1),
-('Aston Martin', 'Vantage AMR GT3', '12', 'Pro-Am', 6, 1),
-('Ferrari', '488 GT3', '13', 'Pro-Am', 7, 1),
-('Ferrari', '488 GT3', '23', 'Am', 7, 1),
-('Acura', 'NSX GT3', '43', 'Pro-Am', 8, 1),
-('Porsche', '911 GT3-R (991.ii)', '45', 'Pro-Am', 9, 1),
-('Ferrari', '488 GT3', '61', 'Am', 10, 1),
-('Mercedes', 'AMG GT3', '63', 'Pro-Am', 4, 1),
-('Lamborghini', 'Huracan GT3', '88', 'Pro-Am', 11, 1),
-('Lamborghini', 'Huracan GT3', '91', 'Pro-Am', 11, 1),
-('Acura', 'NSX GT3', '93', 'Pro-Am', 12, 1),
-('BMW', 'M4 GT3', '94', 'Pro-Am', 13, 1),
-('BMW', 'M4 GT3', '96', 'Pro', 14, 1),
-('BMW', 'M4 GT3', '28', 'Pro', 15, 1);
+('Lamborghini Huracan GT3', '1', 'Pro', 1, 1),
+('Lamborghini Huracan GT3', '3', 'Pro', 1, 1),
+('Mercedes-AMG GT3', '04', 'Pro-Am', 2, 1),
+('Mercedes-AMG GT3', '6', 'Pro', 3, 1),
+('Mercedes-AMG GT3', '08', 'Pro-Am', 4, 1),
+('Lamborghini Huracan GT3', '9', 'Pro-Am', 5, 1),
+('Aston Martin Vantage AMR GT3', '12', 'Pro-Am', 6, 1),
+('Ferrari 488 GT3', '13', 'Pro-Am', 7, 1),
+('Ferrari 488 GT3', '23', 'Am', 7, 1),
+('Acura NSX GT3', '43', 'Pro-Am', 8, 1),
+('Porsche 911 GT3-R (991.ii)', '45', 'Pro-Am', 9, 1),
+('Ferrari 488 GT3', '61', 'Am', 10, 1),
+('Mercedes-AMG GT3', '63', 'Pro-Am', 4, 1),
+('Lamborghini Huracan GT3', '88', 'Pro-Am', 11, 1),
+('Lamborghini Huracan GT3', '91', 'Pro-Am', 11, 1),
+('Acura NSX GT3', '93', 'Pro-Am', 12, 1),
+('BMW M4 GT3', '94', 'Pro-Am', 13, 1),
+('BMW M4 GT3', '96', 'Pro', 14, 1),
+('BMW M4 GT3', '28', 'Pro', 15, 1);
 
 INSERT INTO drivers (name, rating, nationality, team_id, vehicle_id) VALUES
 ('Michele Beretta', 'Silver', 'Italy', 1, 1),
