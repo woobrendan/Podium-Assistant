fa11e84e9831878d292711a54c49805dd96caaa9
Brendan Woo <woo.brendan@gmail.com>
2022-06-07 14:25:53 -0400

    add folder for routes

---
+++ driverDB/routes/drivers.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+  //route /api/drivers
+  router.get('/', (req, res) => {
+    const queryString = `
+    SELECT drivers.id as id, drivers.name AS driver, rating, nationality, teams.name AS team, vehicles.* AS vehicle 
+    FROM drivers
+    JOIN teams ON teams.id = team_id
+    JOIN vehicles ON vehicles.id = vehicle_id;
+    `;
+    return db
+      .query(queryString)
+      .then(response => {
+        return res.status(200)
+          .json(response.rows)
+      })
+      .catch(err => console.log(err.message))
+  })
+  return router;
+}
