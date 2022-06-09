44aea2a99d380ca782a3257a8a8f9762cc688d1b
Brendan Woo <woo.brendan@gmail.com>
2022-06-09 16:40:44 -0400

    wrap everything in a div competitor card, import elevation

--- podium-helper/src/components/DriverDetails.js
+++ podium-helper/src/components/DriverDetails.js
@@ -3,26 +3,35 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import DriverTable from './DriverTable';
+import Elevation from './elevationPaper';
 
 function DriverDetails(props) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="180"
-        image={props.entry.carImage}
-        alt={props.entry.car}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props.entry.team} #{props.entry.number} - {props.entry.classification}
-        </Typography>
-        <Typography gutterBottom variant="h6" component="div">
-          {props.entry.car}
-        </Typography>
-        <DriverTable  drivers={props.entry} />
-      </CardContent>
-    </Card>
+    <div className="competitor-card">
+      <Card sx={{ 
+        maxWidth: 345, 
+        minHeight: 661,
+        minWidth: 345
+      }}>
+        <CardMedia
+          component="img"
+          height="180"
+          image={props.entry.carImage}
+          alt={props.entry.car}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.entry.team} #{props.entry.number} 
+          </Typography>
+          <Elevation 
+            series={props.entry.series}
+            car={props.entry.car}
+            class={props.entry.classification}
+          />
+          <DriverTable  drivers={props.entry} />
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
