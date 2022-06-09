0ec3aefa2158a963e584571baac7c7b60107fe88
Brendan Woo <woo.brendan@gmail.com>
2022-06-09 12:27:39 -0400

    remove commented out function and unused imports

--- podium-helper/src/components/DriverSearch.js
+++ podium-helper/src/components/DriverSearch.js
@@ -1,17 +1,8 @@
-import { GTWCADrivers, PGT4ADrivers, driverInfo } from '../drivers'
+import { driverInfo } from '../drivers'
 import DriverDetails from './DriverDetails';
 import '../Styling/competitors.scss'
 
 function DriverSearch(props) {
-  // const seriesDrivers = (choice) => {
-  //   switch (choice) {
-  //     case 'GT World Challenge America':
-  //       return driverInfo;
-  //     case 'Pirelli GT4 America':
-  //       return PGT4ADrivers;
-  //   }
-  // }
-  // const mappedDrivers = seriesDrivers(props.seriesChoice).map((driver,index) => 
   const mappedDrivers = driverInfo.map((entry,index) =>(
        <DriverDetails entry={entry} key={index}/>
       ))
