591d8f6191b834c535a88b41ef6d511b29fa9ed7
Brendan Woo <woo.brendan@gmail.com>
2022-06-15 11:20:38 -0400

    create functionality for search by driver

--- podium-helper/src/components/DriverSearch.js
+++ podium-helper/src/components/DriverSearch.js
@@ -1,12 +1,24 @@
 import { driverInfo } from '../drivers'
 import DriverDetails from './DriverDetails';
 import '../Styling/competitors.scss'
+import {useState} from 'react';
 
 function DriverSearch(props) {
-  const mappedDrivers = driverInfo.map((entry,index) =>(
+  const [drivers, setDrivers] = useState(driverInfo);
+  const [driverSearch, setDriverSearch] = useState('');
+  
+  const filteredDrivers = drivers.filter(val => {
+    if (driverSearch === "") {
+      return val;
+    } else if (val.driver1.name.toLowerCase().includes(driverSearch.toLowerCase)
+    || val.driver2.name.toLowerCase().includes(driverSearch.toLowerCase)){
+      return val;
+    }
+  });
+  const mappedDrivers = filteredDrivers.map((entry,index) =>(
        <DriverDetails entry={entry} key={index}/>
       ))
-  
+
   return (
     <div className="entry_cards">
       {mappedDrivers}
