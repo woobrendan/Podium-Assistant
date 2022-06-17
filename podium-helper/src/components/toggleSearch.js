15f7debe6b266640699d40f9cce0572392497ece
Brendan Woo <woo.brendan@gmail.com>
2022-06-17 17:49:25 -0400

    set up inital search toggle component

---
+++ podium-helper/src/components/toggleSearch.js
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+
+
+function toggleSearch() {
+  const [searchOption, setSearchOption] = useState({
+    driver: '',
+    vehicle: '',
+    country: '',
+    team: '',
+    rating: ''
+  });
+
+  const handleToggle = (event) => {
+
+  }
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default toggleSearch
