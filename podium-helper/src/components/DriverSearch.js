10584750381b0b031e4cca9922fc598fb32e8e60
Brendan Woo <woo.brendan@gmail.com>
2022-06-16 23:15:43 -0400

    complete logic and fix function of search by driver name

--- podium-helper/src/components/DriverSearch.js
+++ podium-helper/src/components/DriverSearch.js
@@ -9,13 +9,23 @@ function DriverSearch() {
   const [driverSearch, setDriverSearch] = useState('');
   
   const filteredDrivers = drivers.filter(val => {
-    if (driverSearch === "") {
-      return val;
-    } else if (val.driver1.name.toLowerCase().includes(driverSearch.toLowerCase)
-    || val.driver2.name.toLowerCase().includes(driverSearch.toLowerCase)){
-      return val;
+    if (!val.driver2) {
+      if (driverSearch === "") {
+        return val;
+      } else if (val.driver1.name.toLowerCase().includes(driverSearch.toLowerCase())) {
+        return val;
+      }
+    } else {
+      if (driverSearch === "") {
+        return val;
+      } else if (val.driver1.name.toLowerCase().includes(driverSearch.toLowerCase())
+        || val.driver2.name.toLowerCase().includes(driverSearch.toLowerCase())){
+        return val;
+      }
     }
   });
+
+  
   const mappedDrivers = filteredDrivers.map((entry,index) =>(
        <DriverDetails entry={entry} key={index}/>
       ))
