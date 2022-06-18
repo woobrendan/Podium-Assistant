e4fee4597012d1f687f15179c7445b965267e35e
Brendan Woo <woo.brendan@gmail.com>
2022-06-17 22:16:07 -0400

    change searchoption use state to empty string instead of object. create function to render options for searches

--- podium-helper/src/components/toggleSearch.js
+++ podium-helper/src/components/toggleSearch.js
@@ -1,21 +1,46 @@
 import {useState} from 'react';
+import Box from '@mui/material/Box';
+import MenuItem from '@mui/material/MenuItem';
+import { FormControl, InputLabel, Select } from '@mui/material';
 
 
 function toggleSearch() {
-  const [searchOption, setSearchOption] = useState({
-    driver: '',
-    vehicle: '',
-    country: '',
-    team: '',
-    rating: ''
-  });
+  const [searchOption, setSearchOption] = useState('');
+
+  const options = ['driver', 'car', 'nationality', 'team', 'rating'];
 
   const handleToggle = (event) => {
-
+    setSearchOption(event.target.value);
   }
-  
+
+  const mappedOptions = options.map(option => (
+    <MenuItem key={opion} value={option}>{option}</MenuItem>
+  ))
+
   return (
     <div>
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <FormControl sx={{ minWidth: 300 }}>
+            <InputLabel htmlFor="exampleFormControlSelect2">
+              Search Options
+            </InputLabel>
+            <Select
+              className="form-control"
+              name="search-by"
+              value={searchOption}
+              onChange={handleToggle}
+            >
+              {mappedOptions}
+            </Select>
+          </FormControl>
+        </Box>
       
     </div>
   )
