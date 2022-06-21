1053352163241f206ca957c3e4779152fdd31954
Brendan Woo <woo.brendan@gmail.com>
2022-06-21 17:24:21 -0400

    fix style sheet import statement

--- podium-helper/src/components/Podium_Results/FastLapTable.js
+++ podium-helper/src/components/Podium_Results/FastLapTable.js
@@ -6,7 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import '../Styling/result.scss';
+import '../../Styling/result.scss';
 
 function createFastLap(driver, lapTime) {
   return { driver, lapTime };
