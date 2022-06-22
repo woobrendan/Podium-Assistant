7c22af8a32dda7007b92b4a1fc35dd15aaf193dc
Brendan Woo <woo.brendan@gmail.com>
2022-06-22 15:02:05 -0400

    update driver query to better label columns

--- driverDB/routes/drivers.js
+++ driverDB/routes/drivers.js
@@ -5,10 +5,12 @@ module.exports = (db) => {
   //route /api/drivers
   router.get('/', (req, res) => {
     const queryString = `
-    SELECT drivers.id as id, drivers.name AS driver, rating, nationality, teams.name AS team, vehicles.* AS vehicle 
+    SELECT drivers.id as id, drivers.name AS driver, rating, nationality, teams.name AS team, vehicles.car AS vehicle, vehicles.number AS number, vehicles.classification AS classification, series.name AS series 
     FROM drivers
     JOIN teams ON teams.id = team_id
-    JOIN vehicles ON vehicles.id = vehicle_id;
+    JOIN vehicles ON vehicles.id = vehicle_id
+    JOIN series ON series.id = series_id
+    ORDER BY drivers.id;
     `;
     return db
       .query(queryString)
