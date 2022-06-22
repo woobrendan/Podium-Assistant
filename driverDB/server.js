0d4c8b83e74dbde36f390863bd53cce4b5a8b8bc
Brendan Woo <woo.brendan@gmail.com>
2022-06-22 15:43:18 -0400

    add vehicles route middleware

--- driverDB/server.js
+++ driverDB/server.js
@@ -7,11 +7,13 @@ app.use(express.static('public'));
 const pool = require('./lib/db');
 
 const apiDriverRoute = require('./routes/drivers');
-const apiTeamsRoute = require('./routes/teams')
+const apiTeamsRoute = require('./routes/teams');
+const apiVehiclesRoute = require('./routes/vehicles');
 
 
 
 app.use('/api/drivers', apiDriverRoute(pool));
 app.use('/api/teams', apiTeamsRoute(pool));
+app.use('/api/vehicles', apiVehiclesRoute(pool));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
