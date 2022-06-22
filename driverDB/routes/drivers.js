1960f511974c30d528d6859e846b0593279d439e
Brendan Woo <woo.brendan@gmail.com>
2022-06-22 15:11:59 -0400

    setup driver/vehicles route for info on driver and their vehicle

--- driverDB/routes/drivers.js
+++ driverDB/routes/drivers.js
@@ -19,13 +19,24 @@ module.exports = (db) => {
       .catch(err => console.log(err.message))
   });
 
+  router.get('/vehicles', (req, res) => {
+    const queryString = `
+    SELECT drivers.id as id, drivers.name AS driver, rating, nationality, teams.name AS team, vehicles.car AS vehicle, vehicles.number AS number, vehicles.classification AS classification, series.name AS series 
+    FROM drivers
+    JOIN teams ON teams.id = team_id
+    JOIN vehicles ON vehicles.id = vehicle_id
+    JOIN series ON series.id = series_id
+    ORDER BY drivers.id;
+    `;
+    return db
+      .query(queryString)
+      .then(response => {
+        return res.status(200)
+          .json(response.rows)
+      })
+      .catch(err => console.log(err.message))
+  })
   return router;
 }
 
 //driver + vehicle info
-// SELECT drivers.id as id, drivers.name AS driver, rating, nationality, teams.name AS team, vehicles.car AS vehicle, vehicles.number AS number, vehicles.classification AS classification, series.name AS series 
-//     FROM drivers
-//     JOIN teams ON teams.id = team_id
-//     JOIN vehicles ON vehicles.id = vehicle_id
-//     JOIN series ON series.id = series_id
-//     ORDER BY drivers.id;
