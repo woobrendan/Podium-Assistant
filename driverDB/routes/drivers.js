0f813ceb663a8e484c843a85167e7d31039ca127
Brendan Woo <woo.brendan@gmail.com>
2022-06-22 15:10:01 -0400

    adjust drivers route to be just driver information

--- driverDB/routes/drivers.js
+++ driverDB/routes/drivers.js
@@ -5,12 +5,10 @@ module.exports = (db) => {
   //route /api/drivers
   router.get('/', (req, res) => {
     const queryString = `
-    SELECT drivers.id as id, drivers.name AS driver, rating, nationality, teams.name AS team, vehicles.car AS vehicle, vehicles.number AS number, vehicles.classification AS classification, series.name AS series 
+    SELECT drivers.id AS id, drivers.name AS name, drivers.rating AS rating, drivers.nationality AS nationality, teams.name AS team, vehicles.car AS vehicle
     FROM drivers
-    JOIN teams ON teams.id = team_id
-    JOIN vehicles ON vehicles.id = vehicle_id
-    JOIN series ON series.id = series_id
-    ORDER BY drivers.id;
+    JOIN teams ON teams.id = team_id 
+    JOIN vehicles ON vehicle_id = vehicles.id;
     `;
     return db
       .query(queryString)
@@ -19,6 +17,15 @@ module.exports = (db) => {
           .json(response.rows)
       })
       .catch(err => console.log(err.message))
-  })
+  });
+
   return router;
 }
+
+//driver + vehicle info
+// SELECT drivers.id as id, drivers.name AS driver, rating, nationality, teams.name AS team, vehicles.car AS vehicle, vehicles.number AS number, vehicles.classification AS classification, series.name AS series 
+//     FROM drivers
+//     JOIN teams ON teams.id = team_id
+//     JOIN vehicles ON vehicles.id = vehicle_id
+//     JOIN series ON series.id = series_id
+//     ORDER BY drivers.id;
