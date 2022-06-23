905ccc768b8643f455243c0b13a6ac252a0dfe79
Brendan Woo <woo.brendan@gmail.com>
2022-06-23 16:49:37 -0400

    create file for results route

---
+++ driverDB/routes/results.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+  //route /api/results
+  router.get('/', (req, res) => {
+
+  });
+  return router;
+}
+
