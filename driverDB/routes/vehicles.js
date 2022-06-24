28482ec06a9e2e7f8607fecb4f40a1d28538b877
Brendan Woo <woo.brendan@gmail.com>
2022-06-24 15:37:48 -0400

    adjust query to pull driver infor and number

--- driverDB/routes/vehicles.js
+++ driverDB/routes/vehicles.js
@@ -5,10 +5,11 @@ module.exports = (db) => {
   //route /api/vehicles
   router.get('/', (req, res) => {
     const queryString = `
-    SELECT vehicles.*, teams.name AS team, series.name AS series 
+    SELECT vehicles.id AS id, vehicles.car AS vehicle, vehicles.number AS number, vehicles.classification AS classification, teams.name AS team, series.name AS series, drivers.name AS driver_name, drivers.rating AS rating, drivers.nationality AS nationality
     FROM vehicles
     JOIN teams ON teams.id = team_id
     JOIN series ON series.id = series_id
+    JOIN drivers ON vehicles.id = vehicle_id
     ORDER BY vehicles.id;
     `;
     return db
@@ -21,11 +22,15 @@ module.exports = (db) => {
   });
 
   router.get('/:id', (req, res) => {
-    const queryString = `SELECT vehicles.*, teams.name AS team, series.name AS series 
-    FROM vehicles 
+    const queryString = `
+    SELECT vehicles.id AS id, vehicles.car AS vehicle, vehicles.number AS number, vehicles.classification AS classification, teams.name AS team, series.name AS series, drivers.name AS driver_name, drivers.rating AS rating, drivers.nationality AS nationality
+    FROM vehicles
     JOIN teams ON teams.id = team_id
     JOIN series ON series.id = series_id
-    WHERE vehicles.id=$1;`
+    JOIN drivers ON vehicles.id = vehicle_id
+    WHERE vehicles.id = $1
+    ORDER BY vehicles.id;
+    ` 
     return db
       .query(queryString, [req.params.id])
       .then(response => {
