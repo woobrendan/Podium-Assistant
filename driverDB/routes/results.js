3f225c47dac18b9b9bab63fceba4af5a88938706
Brendan Woo <woo.brendan@gmail.com>
2022-06-24 14:41:35 -0400

    reset results home to podiums.* query, add query to get result by id

--- driverDB/routes/results.js
+++ driverDB/routes/results.js
@@ -4,7 +4,48 @@ const router = express.Router();
 module.exports = (db) => {
   //route /api/results
   router.get('/', (req, res) => {
+    const queryString = 
+    // `SELECT results.id AS result_id, events.name AS event, series.name AS series, class.name AS class
+    // FROM results
+    // JOIN events ON events.id = event_id
+    // JOIN series ON series.id = series_id
+    // JOIN podiums ON results.id = result_id
+    // JOIN class ON class_id = class.id
+    // WHERE results.id = 1
+    // ;`;
+    `SELECT results.id AS result_id, events.name AS event, series.name AS series, podiums.*
+    FROM results
+    JOIN events ON events.id = event_id
+    JOIN series ON series.id = series_id
+    JOIN podiums ON results.id = result_id
+    WHERE results.id = 1
+    ;`;
+    return db
+      .query(queryString)
+      .then(response => {
+        return res.status(200)
+          .json(response.rows)
+      })
+      .catch(err => console.log(err.message))
+  });
 
+  router.get('/:id', (req, res) => {
+    const queryString = `
+    SELECT results.id AS result_id, events.name AS event, series.name AS series, podiums.* 
+    FROM results
+    JOIN events ON events.id = event_id
+    JOIN series ON series.id = series_id
+    JOIN podiums ON results.id = result_id
+    WHERE results.id = $1
+    ;
+    `;
+    return db
+      .query(queryString, [req.params.id])
+      .then(response => {
+        return res.status(200)
+          .json(response.rows)
+      })
+      .catch(err => console.log(err.message))
   });
   return router;
 }
