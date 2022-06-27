6ddd1c36853db36c36e2f064b7cb2a1cbe4d9332
Brendan Woo <woo.brendan@gmail.com>
2022-06-27 14:43:39 -0400

    remove driver info from the vehicle info, change class to class name

--- driverDB/routes/vehicles.js
+++ driverDB/routes/vehicles.js
@@ -5,11 +5,11 @@ module.exports = (db) => {
   //route /api/vehicles
   router.get('/', (req, res) => {
     const queryString = `
-    SELECT vehicles.id AS id, vehicles.car AS vehicle, vehicles.number AS number, vehicles.classification AS classification, teams.name AS team, series.name AS series, drivers.name AS driver_name, drivers.rating AS rating, drivers.nationality AS nationality
+    SELECT vehicles.id AS id, vehicles.car AS vehicle, vehicles.number AS number, class.name AS class, teams.name AS team, series.name AS series 
     FROM vehicles
     JOIN teams ON teams.id = team_id
     JOIN series ON series.id = series_id
-    JOIN drivers ON vehicles.id = vehicle_id
+    JOIN class ON class.id = class_id
     ORDER BY vehicles.id;
     `;
     return db
