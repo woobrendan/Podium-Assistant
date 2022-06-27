bbb48e6bb2435eb0bc43a2ec3af58938d961ba27
Brendan Woo <woo.brendan@gmail.com>
2022-06-27 14:00:43 -0400

    create route for result history component

--- podium-helper/src/App.js
+++ podium-helper/src/App.js
@@ -3,6 +3,7 @@ import DriverSearch from './components/DriverSearch';
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Podium from './components/Podium_Results/Podium';
 import NavBar from './components/NavBar';
+import Results from './components/Results/ResultsHistory';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Podium />} />
           <Route path="/competitors" element={<DriverSearch />} />
+          <Route path="/results" element={<Results />} />
         </Routes>
       </div>
     </Router>
