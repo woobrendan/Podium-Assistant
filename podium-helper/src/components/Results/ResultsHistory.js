792da970a6ded400bdfe8183ad6bfcea230322dc
Brendan Woo <woo.brendan@gmail.com>
2022-06-27 14:23:05 -0400

    set up axios call for results and usestate for driversr and results

--- podium-helper/src/components/Results/ResultsHistory.js
+++ podium-helper/src/components/Results/ResultsHistory.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function Results() {
+  const [drivers, setDrivers] = useState([]);
+  const [resultHistory, setResultHistory] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/results`)
+      .then((res) => {
+        setResultHistory(res.data);
+      })
+      .catch((err) => console.log("Error:", err));
+  }, [])
+
   return (
     <div className="result-container">
       <span>Hello World</span>
