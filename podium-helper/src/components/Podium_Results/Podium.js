cba3929f366fdb3c30b4a0bde95e31a933f8b77a
Brendan Woo <woo.brendan@gmail.com>
2022-06-30 13:31:51 -0400

    change podiumx to resultx to match backend data structure and result table usage

--- podium-helper/src/components/Podium_Results/Podium.js
+++ podium-helper/src/components/Podium_Results/Podium.js
@@ -14,17 +14,17 @@ function Podium() {
   const [results, setResults] = useState({
     series: '',
     event: '',
-    podium1: '',
-    podium2: '',
-    podium3: '',
+    result1: '',
+    result2: '',
+    result3: '',
     fastLap: ''
   });
 
   //toggle control for winnertop3 card
   const [showWinnerTable, setShowWinnerTable] = useState({
-    podium1: true,
-    podium2: true,
-    podium3: true,
+    result1: true,
+    result2: true,
+    result3: true,
     fastLap: true,
     misc: true,
     printPage: false
@@ -46,9 +46,9 @@ function Podium() {
 
   const handleRacePodiumSubmit = (value) => {
     const podiumNumber = () => {
-      if (!results.podium1) return 'podium1'
-      else if (!results.podium2) return 'podium2'
-      else return 'podium3'
+      if (!results.result1) return 'result1'
+      else if (!results.result2) return 'result2'
+      else return 'result3'
     }
     setResults((prev) => ({
       ...prev,
@@ -91,19 +91,19 @@ function Podium() {
     <div className="race-results-container">
       {showWinnerTable.misc && <EventSearch getEventName={getEventName}/>}
       {showWinnerTable.misc && <Series getSeries={getSeries}/>}
-      {showWinnerTable.podium1 && 
+      {showWinnerTable.result1 && 
         <WinnerTop3 
           series={results.series} 
           onClick={handleRacePodiumSubmit}
           results={results}
         />}
-      {showWinnerTable.podium2 && 
+      {showWinnerTable.result2 && 
         <WinnerTop3 
           series={results.series} 
           onClick={handleRacePodiumSubmit}
           results={results}
         />}
-      {showWinnerTable.podium3 && 
+      {showWinnerTable.result3 && 
         <WinnerTop3 
           series={results.series} 
           onClick={handleRacePodiumSubmit}
@@ -123,9 +123,9 @@ function Podium() {
           Print Results
         </Button>
       } */}
-      {!showWinnerTable.podium1 && <Results result={results.podium1} />}
-      {!showWinnerTable.podium2 && <Results result={results.podium2} />}
-      {!showWinnerTable.podium3 && <Results result={results.podium3} />}
+      {!showWinnerTable.result1 && <Results result={results.result1} />}
+      {!showWinnerTable.result2 && <Results result={results.result2} />}
+      {!showWinnerTable.result3 && <Results result={results.result3} />}
       {!showWinnerTable.fastLap && <FastLapTable fastLap={results.fastLap} />}
       {showWinnerTable.printPage && <Button variant="contained" color="success" onClick={printPage}>Print Page</Button>}
     </div>
