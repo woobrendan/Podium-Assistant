65d94fb55696fa5c95d45d6f7223d09fe3d17c57
Brendan Woo <woo.brendan@gmail.com>
2022-06-30 12:55:44 -0400

    add route to get event by id

--- driverDB/routes/events.js
+++ driverDB/routes/events.js
@@ -15,6 +15,21 @@ module.exports = (db) => {
       .catch(err => console.log(err.message))
   });
 
+  // api/events/"id"
+  router.get('/:id', (req, res) => {
+    const queryString = 
+    `SELECT * FROM events
+    WHERE id=$1;`;
+    return db
+      .query(queryString,[req.params.id])
+      .then(response => {
+        return res.status(200)
+          .json(response.rows)
+        })
+        .catch(err => console.log(err.message))
+    });
+      
+
   return router;
 }
 
