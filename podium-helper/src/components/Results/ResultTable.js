5e5b17ea1aac6c4fb4595265db263e8d13e253c7
Brendan Woo <woo.brendan@gmail.com>
2022-06-30 13:05:33 -0400

    remove unused imports and remove data structure templatee

--- podium-helper/src/components/Results/ResultTable.js
+++ podium-helper/src/components/Results/ResultTable.js
@@ -1,33 +1,8 @@
-import React from 'react';
-import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import '../../Styling/result.scss';
-
-const result = {
-  result1: {
-    class: "Pro",
-    event: "VIR",
-    first_place:{
-      team: "K-Pax",
-      vehicle: "Lambo",
-      number: "1",
-      driver1: {
-        name: "Andrea",
-      },
-      driver2: {
-        name: "Misha"
-      }
-    },
-    second_place:{},
-    third_place: {}
-  },
-  result2: {}
-}
 
 function createDataTwoDriver(place, number, driver1, driver2, team, car) {
   return { place, number, driver1, driver2, team, car };
