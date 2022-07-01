1e0fc0d33b267d9e4324eccee50669d7e59ffd82
Brendan Woo <woo.brendan@gmail.com>
2022-07-01 13:15:37 -0400

    add axios call to fast laps, create new usestate for fast laps. map through fast laps to add driver name to fast lap info. Add to grouped results to add fast lap info to podium result obj

--- podium-helper/src/components/Results/ResultsHistory.js
+++ podium-helper/src/components/Results/ResultsHistory.js
@@ -5,17 +5,20 @@ import ResultTableHeader from './ResultTableHeader';
 function Results() {
   const [drivers, setDrivers] = useState([]);
   const [resultHistory, setResultHistory] = useState([]);
-  const [vehicles, setVehicles] = useState([])
+  const [vehicles, setVehicles] = useState([]);
+  const [fastLaps, setFastLaps] = useState([]);
 
   useEffect(() => {
     const promise1 = axios.get(`http://localhost:8080/api/results`);
     const promise2 = axios.get(`http://localhost:8080/api/drivers`);
     const promise3 = axios.get(`http://localhost:8080/api/vehicles`);
-    Promise.all([promise1, promise2, promise3])
+    const promise4 = axios.get(`http://localhost:8080/api/fastlaps`);
+    Promise.all([promise1, promise2, promise3, promise4])
       .then((all) => {
         setResultHistory(all[0].data);
         setDrivers(all[1].data);
-        setVehicles(all[2].data)
+        setVehicles(all[2].data);
+        setFastLaps(all[3].data);
       })
       .catch((err) => console.log("Error:", err));
   }, [])
@@ -32,6 +35,15 @@ function Results() {
     }
   });
 
+  fastLaps.map((lap) => {
+    for (const driver of drivers) {
+      if (driver.id === lap.driver_id) {
+        lap.driver = driver.name;
+      }
+    }
+  });
+
+
   const groupedResults = (resultArr) => {
     const arrayOfGroupedResults = [];
     for (let i = 0; i < resultArr.length; i += 3) {
@@ -44,6 +56,11 @@ function Results() {
         result2: resultArr[y],
         result3: resultArr[z]
       })
+    }
+    for (let x = 0; x < fastLaps.length; x++) {
+      if (fastLaps[x].id === arrayOfGroupedResults[x].result1.fast_lap) {
+        arrayOfGroupedResults[x].fastLap = fastLaps[x]
+      }
     }
     return arrayOfGroupedResults
   };
@@ -63,6 +80,8 @@ function Results() {
     }
     return result;
   });
+
+  console.log('group', groupedResults(entryResults))
 
   const allResults = groupedResults(entryResults).map((result) => (
     <div key={result.result1.result_id}>
