598ded7bc8ae3341cc52c30972be25fc2fd4f23d
Brendan Woo <woo.brendan@gmail.com>
2022-07-01 13:25:20 -0400

    create file to track the obj structures of backend and front end podiums

---
+++ podium-helper/src/components/dataStructures.js
@@ -0,0 +1,60 @@
+//From podium
+const podium = {
+  series: "GTWCA",
+  event: 'VIR',
+  result1: {
+    class: 'pro',
+    first_place: {
+      team: 'K-PAX',
+      car: "Lambo",
+      number: '1',
+      carImage: 'image',
+      series: 'GTWCA',
+      driver1: {
+        name: 'Michele',
+        nationality: 'Italy',
+        rating: 'Silver'
+      },
+      driver2: {
+        name: 'Andrea',
+        nationality: 'Italy',
+        rating: 'Platinum'
+      },
+    },
+    second: {},
+    third: {}
+  },
+  result2: {},
+  result3: {},
+  fastlap: {}
+}
+
+//from backend
+const result = {
+  event: "VIR",
+  series: "GTWCA",
+  fastLap: {
+    id: 2,
+    driver: "Giacomo",
+    laptimee: "1",
+    driver_id: "2"
+  },
+  result1: {
+    class: "Pro",
+    event: "VIR",
+    first_place:{
+      team: "K-Pax",
+      vehicle: "Lambo",
+      number: "1",
+      driver1: {
+        name: "Andrea",
+      },
+      driver2: {
+        name: "Misha"
+      }
+    },
+    second_place:{},
+    third_place: {}
+  },
+  result2: {}
+}
