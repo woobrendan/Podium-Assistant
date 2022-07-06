6400604b8987ae9a8a6d408d005f115ce8d3504b
Brendan Woo <woo.brendan@gmail.com>
2022-07-06 14:09:05 -0400

    add switch case logic for search results by event

--- podium-helper/src/components/Results/ResultsHistory.js
+++ podium-helper/src/components/Results/ResultsHistory.js
@@ -14,20 +14,114 @@ function Results() {
     groupResults
   } = useEntries()
 
+  console.log(groupResults)
   const [searchValue, setSearchValue] = useState('');
   const [option, setOption] = useState('');
 
-  const allResults = groupResults.map((result) => (
+  const getSearchOption = (option) => {
+    setOption(option);
+  };
+  const searchLabel = option ? `Search by ${option}`: "Search";
+
+  const filteredResults = groupResults.filter(val => {
+    switch(option){
+      case 'All':
+        return val;
+
+      case 'Event':
+        if (!searchValue) {
+          return val;
+        } else if (val.event.toLowerCase().includes(searchValue.toLowerCase())) {
+          return val;
+        }
+        break;
+
+      // case 'Driver':
+      //   if (!val.driver2) {
+      //     if (!searchValue) {
+      //       return val;
+      //     } else if (val.driver1.name.toLowerCase().includes(searchValue.toLowerCase())) {
+      //       return val;
+      //     }
+      //   } else {
+      //     if (!searchValue) {
+      //       return val;
+      //     } else if (val.driver1.name.toLowerCase().includes(searchValue.toLowerCase())
+      //       || val.driver2.name.toLowerCase().includes(searchValue.toLowerCase())){
+      //       return val;
+      //     }
+      //   }
+      //   break;
+
+
+      // case 'Team':
+      //   if (!searchValue) {
+      //     return val;
+      //   } else if (val.team.toLowerCase().includes(searchValue.toLowerCase())) {
+      //     return val;
+      //   }
+      //   break;
+
+      // case 'Nationality':
+      //   if (!val.driver2) {
+      //     if (!searchValue) {
+      //       return val;
+      //     } else if (val.driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())) {
+      //       return val;
+      //     }
+      //   } else {
+      //     if (!searchValue) {
+      //       return val;
+      //     } else if (val.driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())
+      //       || val.driver2.nationality.toLowerCase().includes(searchValue.toLowerCase())){
+      //       return val;
+      //     }
+      //   }
+      //   break;
+
+      //   case 'Rating':
+      //   if (!val.driver2) {
+      //     if (!searchValue) {
+      //       return val;
+      //     } else if (val.driver1.rating.toLowerCase().includes(searchValue.toLowerCase())) {
+      //       return val;
+      //     }
+      //   } else {
+      //     if (!searchValue) {
+      //       return val;
+      //     } else if (val.driver1.rating.toLowerCase().includes(searchValue.toLowerCase())
+      //       || val.driver2.rating.toLowerCase().includes(searchValue.toLowerCase())){
+      //       return val;
+      //     }
+      //   }
+      //   break;
+
+      //   case 'Series':
+      //   if (!searchValue) {
+      //     return val;
+      //   } else if (val.series.toLowerCase().includes(searchValue.toLowerCase())) {
+      //     return val;
+      //   }
+      //   break;
+
+      //   case 'Number':
+      //   if (!searchValue) {
+      //     return val;
+      //   } else if (val.number.includes(searchValue)) {
+      //     return val;
+      //   }
+      //   break;
+
+      default:
+        return val; 
+    }
+  });
+
+  const allResults = filteredResults.map((result) => (
     <div key={result.result1.result_id}>
       <ResultTableHeader results={result}/>
     </div>
   ));
-
-  const getSearchOption = (option) => {
-    setOption(option);
-  }
-
-  const searchLabel = option ? `Search by ${option}`: "Search"
 
   return (
     <div className="result-container">
