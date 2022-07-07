ebc3144e04dc05a118315de97f9ab9e3b63fb0ce
Brendan Woo <woo.brendan@gmail.com>
2022-07-07 16:28:53 -0400

    create file to hold function for switch case search options

---
+++ podium-helper/src/resultOptions.js
@@ -0,0 +1,96 @@
+
+export default function filteredResultOptions(list, option, searchValue) {
+  const filtered = list.filter(val => {
+    switch(option){
+      case 'All':
+        return val;
+
+      case 'Driver':
+        if (!val.driver2) {
+          if (!searchValue) {
+            return val;
+          } else if (val.driver1.name.toLowerCase().includes(searchValue.toLowerCase())) {
+            return val;
+          }
+        } else {
+          if (!searchValue) {
+            return val;
+          } else if (val.driver1.name.toLowerCase().includes(searchValue.toLowerCase())
+            || val.driver2.name.toLowerCase().includes(searchValue.toLowerCase())){
+            return val;
+          }
+        }
+        break;
+
+      case 'Car':
+        if (!searchValue) {
+          return val;
+        } else if (val.vehicle.toLowerCase().includes(searchValue.toLowerCase())) {
+          return val;
+        }
+        break;
+
+      case 'Team':
+        if (!searchValue) {
+          return val;
+        } else if (val.team.toLowerCase().includes(searchValue.toLowerCase())) {
+          return val;
+        }
+        break;
+
+      case 'Nationality':
+        if (!val.driver2) {
+          if (!searchValue) {
+            return val;
+          } else if (val.driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())) {
+            return val;
+          }
+        } else {
+          if (!searchValue) {
+            return val;
+          } else if (val.driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())
+            || val.driver2.nationality.toLowerCase().includes(searchValue.toLowerCase())){
+            return val;
+          }
+        }
+        break;
+
+      case 'Rating':
+      if (!val.driver2) {
+        if (!searchValue) {
+          return val;
+        } else if (val.driver1.rating.toLowerCase().includes(searchValue.toLowerCase())) {
+          return val;
+        }
+      } else {
+        if (!searchValue) {
+          return val;
+        } else if (val.driver1.rating.toLowerCase().includes(searchValue.toLowerCase())
+          || val.driver2.rating.toLowerCase().includes(searchValue.toLowerCase())){
+          return val;
+        }
+      }
+      break;
+
+      case 'Series':
+      if (!searchValue) {
+        return val;
+      } else if (val.series.toLowerCase().includes(searchValue.toLowerCase())) {
+        return val;
+      }
+      break;
+
+      case 'Number':
+      if (!searchValue) {
+        return val;
+      } else if (val.number.includes(searchValue)) {
+        return val;
+      }
+      break;
+
+      default:
+        return val; 
+    }
+  });
+  return filtered;
+}
