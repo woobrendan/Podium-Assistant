3e9cc36fd240c11c215b18a935392545cb18720d
Brendan Woo <woo.brendan@gmail.com>
2022-07-07 14:38:56 -0400

    import searchOption and use instead of filteredDrivers

--- podium-helper/src/components/DriverSearch.js
+++ podium-helper/src/components/DriverSearch.js
@@ -5,6 +5,7 @@ import TextField from '@mui/material/TextField';
 import {useState} from 'react';
 import ToggleSearch from './toggleSearch';
 import BackToTopButton from './BackToTopButton';
+import filteredOptions from '../searchOptions';
 
 function DriverSearch() {
   const [drivers, setDrivers] = useState(driverInfo);
@@ -15,101 +16,101 @@ function DriverSearch() {
     setOption(option);
   }
   
-  const filteredDrivers = drivers.filter(val => {
-    switch(option){
-      case 'All':
-        return val;
+  // const filteredDrivers = drivers.filter(val => {
+  //   switch(option){
+  //     case 'All':
+  //       return val;
 
-      case 'Driver':
-        if (!val.driver2) {
-          if (!searchValue) {
-            return val;
-          } else if (val.driver1.name.toLowerCase().includes(searchValue.toLowerCase())) {
-            return val;
-          }
-        } else {
-          if (!searchValue) {
-            return val;
-          } else if (val.driver1.name.toLowerCase().includes(searchValue.toLowerCase())
-            || val.driver2.name.toLowerCase().includes(searchValue.toLowerCase())){
-            return val;
-          }
-        }
-        break;
+  //     case 'Driver':
+  //       if (!val.driver2) {
+  //         if (!searchValue) {
+  //           return val;
+  //         } else if (val.driver1.name.toLowerCase().includes(searchValue.toLowerCase())) {
+  //           return val;
+  //         }
+  //       } else {
+  //         if (!searchValue) {
+  //           return val;
+  //         } else if (val.driver1.name.toLowerCase().includes(searchValue.toLowerCase())
+  //           || val.driver2.name.toLowerCase().includes(searchValue.toLowerCase())){
+  //           return val;
+  //         }
+  //       }
+  //       break;
 
-      case 'Car':
-        if (!searchValue) {
-          return val;
-        } else if (val.car.toLowerCase().includes(searchValue.toLowerCase())) {
-          return val;
-        }
-        break;
+  //     case 'Car':
+  //       if (!searchValue) {
+  //         return val;
+  //       } else if (val.car.toLowerCase().includes(searchValue.toLowerCase())) {
+  //         return val;
+  //       }
+  //       break;
 
-      case 'Team':
-        if (!searchValue) {
-          return val;
-        } else if (val.team.toLowerCase().includes(searchValue.toLowerCase())) {
-          return val;
-        }
-        break;
+  //     case 'Team':
+  //       if (!searchValue) {
+  //         return val;
+  //       } else if (val.team.toLowerCase().includes(searchValue.toLowerCase())) {
+  //         return val;
+  //       }
+  //       break;
 
-      case 'Nationality':
-        if (!val.driver2) {
-          if (!searchValue) {
-            return val;
-          } else if (val.driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())) {
-            return val;
-          }
-        } else {
-          if (!searchValue) {
-            return val;
-          } else if (val.driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())
-            || val.driver2.nationality.toLowerCase().includes(searchValue.toLowerCase())){
-            return val;
-          }
-        }
-        break;
+  //     case 'Nationality':
+  //       if (!val.driver2) {
+  //         if (!searchValue) {
+  //           return val;
+  //         } else if (val.driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())) {
+  //           return val;
+  //         }
+  //       } else {
+  //         if (!searchValue) {
+  //           return val;
+  //         } else if (val.driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())
+  //           || val.driver2.nationality.toLowerCase().includes(searchValue.toLowerCase())){
+  //           return val;
+  //         }
+  //       }
+  //       break;
 
-        case 'Rating':
-        if (!val.driver2) {
-          if (!searchValue) {
-            return val;
-          } else if (val.driver1.rating.toLowerCase().includes(searchValue.toLowerCase())) {
-            return val;
-          }
-        } else {
-          if (!searchValue) {
-            return val;
-          } else if (val.driver1.rating.toLowerCase().includes(searchValue.toLowerCase())
-            || val.driver2.rating.toLowerCase().includes(searchValue.toLowerCase())){
-            return val;
-          }
-        }
-        break;
+  //       case 'Rating':
+  //       if (!val.driver2) {
+  //         if (!searchValue) {
+  //           return val;
+  //         } else if (val.driver1.rating.toLowerCase().includes(searchValue.toLowerCase())) {
+  //           return val;
+  //         }
+  //       } else {
+  //         if (!searchValue) {
+  //           return val;
+  //         } else if (val.driver1.rating.toLowerCase().includes(searchValue.toLowerCase())
+  //           || val.driver2.rating.toLowerCase().includes(searchValue.toLowerCase())){
+  //           return val;
+  //         }
+  //       }
+  //       break;
 
-        case 'Series':
-        if (!searchValue) {
-          return val;
-        } else if (val.series.toLowerCase().includes(searchValue.toLowerCase())) {
-          return val;
-        }
-        break;
+  //       case 'Series':
+  //       if (!searchValue) {
+  //         return val;
+  //       } else if (val.series.toLowerCase().includes(searchValue.toLowerCase())) {
+  //         return val;
+  //       }
+  //       break;
 
-        case 'Number':
-        if (!searchValue) {
-          return val;
-        } else if (val.number.includes(searchValue)) {
-          return val;
-        }
-        break;
+  //       case 'Number':
+  //       if (!searchValue) {
+  //         return val;
+  //       } else if (val.number.includes(searchValue)) {
+  //         return val;
+  //       }
+  //       break;
 
-      default:
-        return val; 
-    }
-  });
+  //     default:
+  //       return val; 
+  //   }
+  // });
 
   
-  const mappedDrivers = filteredDrivers.map((entry,index) =>(
+  const mappedDrivers = filteredOptions(drivers).map((entry,index) =>(
        <DriverDetails entry={entry} key={index}/>
       ))
 
