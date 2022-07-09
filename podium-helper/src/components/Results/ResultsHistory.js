9b41f3c7546f680b27e87df1abfdcab432079853
Brendan Woo <woo.brendan@gmail.com>
2022-07-08 22:20:15 -0400

    remove search options switch case, import filteredresultoption and pass to allresult

--- podium-helper/src/components/Results/ResultsHistory.js
+++ podium-helper/src/components/Results/ResultsHistory.js
@@ -3,9 +3,10 @@ import ResultTableHeader from './ResultTableHeader';
 import ToggleSearch from '../toggleSearch';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import useEntries from '../../useEntries.js'
+import useEntries from '../../useEntries.js';
+import filteredResultOptions from '../../resultOptions';
 
-function Results() {
+function ResultsHistory() {
   const {
     drivers,
     vehicles,
@@ -14,7 +15,7 @@ function Results() {
     groupResults
   } = useEntries()
 
-  console.log(groupResults)
+  // console.log(groupResults)
   const [searchValue, setSearchValue] = useState('');
   const [option, setOption] = useState('');
 
@@ -23,157 +24,8 @@ function Results() {
   };
   const searchLabel = option ? `Search by ${option}`: "Search";
 
-  const filteredResults = groupResults.filter(val => {
-    const allResults = [val.result1, val.result2, val.result3]
-    const allValDrivers = []
-    for (const result of allResults) {
-      if (!result.first_place.driver2) {
-
-        allValDrivers.push(result.first_place.driver1.name)
-        if (result.second_place) {
-          allValDrivers.push(result.second_place.driver1.name)
-        }
-        if (result.third_place) {
-          allValDrivers.push(result.third_place.driver1.name)
-        }
-      } else {
-        allValDrivers.push(
-          result.first_place.driver1.name,
-          result.first_place.driver2.name
-        )
-        if (result.second_place) {
-          allValDrivers.push(
-            result.second_place.driver1.name,
-            result.second_place.driver2.name
-          )
-        }
-        if (result.third_place) {
-          allValDrivers.push(
-            result.third_place.driver1.name,
-            result.third_place.driver2.name
-          )
-        } 
-        console.log('alldrivers', allValDrivers)
-      }
-    }
-    switch(option){
-      case 'All':
-        return val;
-
-      case 'Event':
-        if (!searchValue) {
-          return val;
-        } else if (val.event.toLowerCase().includes(searchValue.toLowerCase())) {
-          return val;
-        }
-        break;
-
-      // case 'Driver':
-        // const allResults = [val.result1, val.result2, val.result3]
-        // const allValDrivers = []
-        // for (const result of allResults) {
-        //   if (!result.first_place.driver2) {
-        //     allValDrivers.push(
-        //       result.first_place.driver1.name, 
-        //       result.second_place.driver1.name,
-        //       result.third_place.driver1.name
-        //     )
-        //   } else {
-        //     allValDrivers.push(
-        //       result.first_place.driver1.name,
-        //       result.first_place.driver2.name,  
-        //       result.second_place.driver1.name,
-        //       result.second_place.driver2.name,
-        //       result.third_place.driver1.name,
-        //       result.third_place.driver2.name
-        //     )
-        //   }
-        // }
-    
-        // console.log('alldrivers', allValDrivers)
-        // if (!val.driver2) {
-        //   if (!searchValue) {
-        //     return val;
-        //   } else if (val.driver1.name.toLowerCase().includes(searchValue.toLowerCase())) {
-        //     return val;
-        //   }
-        // } else {
-        //   if (!searchValue) {
-        //     return val;
-        //   } else if (val.driver1.name.toLowerCase().includes(searchValue.toLowerCase())
-        //     || val.driver2.name.toLowerCase().includes(searchValue.toLowerCase())){
-        //     return val;
-        //   }
-        // }
-        // console.log('hello')
-        // break;
-
-
-      // case 'Team':
-      //   if (!searchValue) {
-      //     return val;
-      //   } else if (val.team.toLowerCase().includes(searchValue.toLowerCase())) {
-      //     return val;
-      //   }
-      //   break;
-
-      // case 'Nationality':
-      //   if (!val.driver2) {
-      //     if (!searchValue) {
-      //       return val;
-      //     } else if (val.driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())) {
-      //       return val;
-      //     }
-      //   } else {
-      //     if (!searchValue) {
-      //       return val;
-      //     } else if (val.driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())
-      //       || val.driver2.nationality.toLowerCase().includes(searchValue.toLowerCase())){
-      //       return val;
-      //     }
-      //   }
-      //   break;
-
-      //   case 'Rating':
-      //   if (!val.driver2) {
-      //     if (!searchValue) {
-      //       return val;
-      //     } else if (val.driver1.rating.toLowerCase().includes(searchValue.toLowerCase())) {
-      //       return val;
-      //     }
-      //   } else {
-      //     if (!searchValue) {
-      //       return val;
-      //     } else if (val.driver1.rating.toLowerCase().includes(searchValue.toLowerCase())
-      //       || val.driver2.rating.toLowerCase().includes(searchValue.toLowerCase())){
-      //       return val;
-      //     }
-      //   }
-      //   break;
-
-      //   case 'Series':
-      //   if (!searchValue) {
-      //     return val;
-      //   } else if (val.series.toLowerCase().includes(searchValue.toLowerCase())) {
-      //     return val;
-      //   }
-      //   break;
-
-      //   case 'Number':
-      //   if (!searchValue) {
-      //     return val;
-      //   } else if (val.number.includes(searchValue)) {
-      //     return val;
-      //   }
-      //   break;
-
-      default:
-        return val; 
-    }
-  });
-
-  const allResults = groupResults.map((result) => (
-    <div key={result.result1.result_id}>
+  const allResults = filteredResultOptions(groupResults, option, searchValue).map((result, index) => (
+    <div key={index}>
       <ResultTableHeader results={result}/>
     </div>
   ));
@@ -196,4 +48,4 @@ function Results() {
   )
 }
 
-export default Results;
+export default ResultsHistory;
