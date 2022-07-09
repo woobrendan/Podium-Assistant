dd879c5181e9bf820f2e67670dbecac3237c5375
Brendan Woo <woo.brendan@gmail.com>
2022-07-08 22:19:45 -0400

    fix car import of x.car to x.vehicle

--- podium-helper/src/components/Results/ResultTable.js
+++ podium-helper/src/components/Results/ResultTable.js
@@ -19,24 +19,24 @@ function ResultTable(props) {
   const second = podium.second_place;
   const third = podium.third_place;
 
-  const checkPodium = (podium) => {
+  const checkPodium = () => {
     const allResults = [];
     if(!first.driver2) {
-      allResults.push(createDataSingleDriver('1st', first.number, first.driver1.name, first.team, first.car));
+      allResults.push(createDataSingleDriver('1st', first.number, first.driver1.name, first.team, first.vehicle));
       if (second) {
-        allResults.push(createDataSingleDriver('2nd', second.number, second.driver1.name, second.team, second.car));
+        allResults.push(createDataSingleDriver('2nd', second.number, second.driver1.name, second.team, second.vehicle));
       } 
       if (third) {
-        allResults.push(createDataSingleDriver('3rd', third.number, third.driver1.name, third.team, third.car));
+        allResults.push(createDataSingleDriver('3rd', third.number, third.driver1.name, third.team, third.vehicle));
       }
       return allResults; 
     } else {
-      allResults.push(createDataTwoDriver('1st', first.number, first.driver1.name, first.driver2.name, first.team, first.car));
+      allResults.push(createDataTwoDriver('1st', first.number, first.driver1.name, first.driver2.name, first.team, first.vehicle));
       if (second) {
-        allResults.push(createDataTwoDriver('2nd', second.number, second.driver1.name, second.driver2.name, second.team, second.car));
+        allResults.push(createDataTwoDriver('2nd', second.number, second.driver1.name, second.driver2.name, second.team, second.vehicle));
       } 
       if (third) {
-        allResults.push(createDataTwoDriver('3rd', third.number, third.driver1.name, third.driver2.name, third.team, third.car));
+        allResults.push(createDataTwoDriver('3rd', third.number, third.driver1.name, third.driver2.name, third.team, third.vehicle));
       }
       return allResults;
     }
@@ -49,7 +49,7 @@ function ResultTable(props) {
         </TableRow>
       </TableHead>
       <TableBody>
-        {checkPodium(podium).map((row) => (
+        {checkPodium().map((row) => (
           <TableRow
             key={row.number}
             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
