946275ec5be7dea7537b0e16732ab0a763c9da89
Brendan Woo <woo.brendan@gmail.com>
2022-07-14 15:49:15 -0400

    add body parser and use middleware

--- driverDB/server.js
+++ driverDB/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 app.use(cors());
