b2c126a50879f36d183a7692396c4fa6db84a31a
Brendan Woo <woo.brendan@gmail.com>
2022-07-15 13:05:22 -0400

    create route file for results

---
+++ driverDB/routes/results.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+  //results
+  router.post('/', (req, res) => {
+    const fastLap = req.body;
+    console.log('req.body', fastLap)
+    const queryString = 
+    `INSERT INTO fastlaps (driver_id, laptime) VALUES ($1, $2) RETURNING *;`;
+    return db
+      .query(queryString, [fastLap.driver, fastLap.lapTime])
+      .then(result => {
+        console.log('result rows:', result.rows[0])
+        return result.rows[0].id
+      })
+      .catch(err => console.log(err.message))
+  });
+  return router;
+}
+
