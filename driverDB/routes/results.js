177e23211d59c18c5d6cb5bfab9b941ebd5e6d7e
Brendan Woo <woo.brendan@gmail.com>
2022-07-18 15:13:19 -0400

    change newResultString to return all from query. add then statement to query the db for new result, add then to return the new result id

--- driverDB/routes/results.js
+++ driverDB/routes/results.js
@@ -6,22 +6,26 @@ module.exports = (db) => {
   router.post('/new', (req, res) => {
     const fastLap = req.body.results.fastLap;
     const result = req.body.results;
+    console.log('req.body', req.body)
     const queryString = 
     `INSERT INTO fastlaps (driver_id, laptime) VALUES ($1, $2) RETURNING *;`;
 
     const newResultString = `
-    INSERT INTO results (date, event_id, series_id, fastlap_id) VALUES ($1, $2, $3, $4);`
+    INSERT INTO results (date, event_id, series_id, fastlap_id) VALUES ($1, $2, $3, $4) RETURNING *;`
 
     // Create new fast lap entry
     return db
       .query(queryString, [fastLap.id, fastLap.laptime])
       .then(result => {
-        console.log('result rows:', result.rows[0])
         return result.rows[0].id
       })
-      // .then(fastId  => {
-      //   return db.query(newResultString, [result.date, result])
-      // })
+      .then(fastId  => {
+        return db.query(newResultString, [result.date, result.event, result.series, fastId])
+      })
+      .then(val => {
+        const resultId = val.rows[0].id
+        console.log('result Id:', resultId)
+      })
       .catch(err => console.log(err.message))
   });
   return router;
