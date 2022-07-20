ec500da24862b2662bbfbc4347f2a01e82515b61
Brendan Woo <woo.brendan@gmail.com>
2022-07-19 23:39:10 -0400

    move getToday function into helper func and export

--- podium-helper/src/helperFunc.js
+++ podium-helper/src/helperFunc.js
@@ -7,6 +7,15 @@ const getIdFromArray = (name, arr) => {
   }
 }
 
+const getToday = () => {
+  let today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const year = today.getFullYear();
+  return `${mm}-${dd}-${year}`;
+}
+
 export {
-  getIdFromArray
+  getIdFromArray,
+  getToday
 }
