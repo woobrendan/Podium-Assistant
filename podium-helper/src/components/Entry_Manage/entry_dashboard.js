923f63c7acd5049310a945f040b14c4c14c07630
Brendan Woo <woo.brendan@gmail.com>
2022-07-21 11:58:26 -0400

    add single and double driver data creation for table

--- podium-helper/src/components/Entry_Manage/entry_dashboard.js
+++ podium-helper/src/components/Entry_Manage/entry_dashboard.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import useEntries from '../../useEntries'
+import useEntries from '../../useEntries';
+
+
+function createSingleDriverData(carNum, teamName, driver1, vehicle) {
+  return { carNum, teamName, driver1, vehicle };
+}
+
+function createSingleDriverData(carNum, teamName, driver1, driver2, vehicle) {
+  return { carNum, teamName, driver1, driver2, vehicle };
+}
 
 function entry_dashboard() {
   const {vehicles} = useEntries();
-  //create component to map through each vehicle entry and display options
+  //create component to map through each vehicle entry and display optionss
   return (
     <div>
       
