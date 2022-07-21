ca14cfe84f32a45b2c813f06fe6353841cfe88fd
Brendan Woo <woo.brendan@gmail.com>
2022-07-21 12:07:43 -0400

    change data creation for proper inputs. add table template frm drivertable

--- podium-helper/src/components/Entry_Manage/entry_dashboard.js
+++ podium-helper/src/components/Entry_Manage/entry_dashboard.js
@@ -6,17 +6,55 @@ function createSingleDriverData(carNum, teamName, driver1, vehicle) {
   return { carNum, teamName, driver1, vehicle };
 }
 
-function createSingleDriverData(carNum, teamName, driver1, driver2, vehicle) {
+function createDoubleDriverData(carNum, teamName, driver1, driver2, vehicle) {
   return { carNum, teamName, driver1, driver2, vehicle };
 }
 
 function entry_dashboard() {
   const {vehicles} = useEntries();
-  //create component to map through each vehicle entry and display optionss
+
+  const tableRows = (entry) => {
+    if (!entry.driver2) {
+      return [
+        createSingleDriverData(entry.number, entry.team, entry.driver1.name, entry.vehicle)
+      ]
+    } else {
+      return [
+        createDoubleDriverData(entry.number, entry.team, entry.driver1.name, entry.driver2.name, entry.vehicle)
+      ]
+    }
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+    <TableContainer component={Paper} className="entry-dashboard-table">
+      <Table sx={{ minWidth: 300 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Number</TableCell>
+            <TableCell align="right">Team Name</TableCell>
+            <TableCell align="right">Driver 1</TableCell>
+            <TableCell align="right">Driver 2</TableCell>
+            <TableCell align="right">Vehicle</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tableRows(props.entry).map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.carNum}
+              </TableCell>
+              <TableCell align="right">{row.nationality}</TableCell>
+              <TableCell align="right">{row.rating}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </>
   )
 }
 
