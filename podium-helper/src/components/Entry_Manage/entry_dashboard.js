c0dfd77635c9e928743b93b3f24d48342d7f450c
Brendan Woo <woo.brendan@gmail.com>
2022-07-21 14:16:46 -0400

    comment out map for each row of table. adjust tableRows function to loop for each entry of array vechiles

--- podium-helper/src/components/Entry_Manage/entry_dashboard.js
+++ podium-helper/src/components/Entry_Manage/entry_dashboard.js
@@ -1,5 +1,13 @@
-import React from 'react'
+import {useEffect, useState} from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import useEntries from '../../useEntries';
+import axios from 'axios'
 
 
 function createSingleDriverData(carNum, teamName, driver1, vehicle) {
@@ -10,23 +18,27 @@ function createDoubleDriverData(carNum, teamName, driver1, driver2, vehicle) {
   return { carNum, teamName, driver1, driver2, vehicle };
 }
 
-function entry_dashboard() {
+function EntryDashboard() {
+
   const {vehicles} = useEntries();
 
-  const tableRows = (entry) => {
-    if (!entry.driver2) {
-      return [
-        createSingleDriverData(entry.number, entry.team, entry.driver1.name, entry.vehicle)
-      ]
-    } else {
-      return [
-        createDoubleDriverData(entry.number, entry.team, entry.driver1.name, entry.driver2.name, entry.vehicle)
-      ]
+  console.log('vehicles', vehicles)
+
+  const tableRows = (entries) => {
+    const allEntries = []
+    for (const entry of entries) {
+      if (!entry.driver2) {
+        allEntries.push(createSingleDriverData(entry.number, entry.team, entry.driver1.name, entry.vehicle))
+      } else {
+        allEntries.push(createDoubleDriverData(entry.number, entry.team, entry.driver1.name, entry.driver2.name, entry.vehicle))
+      }
     }
+    return allEntries;
   }
 
   return (
-    <>
+    <div>
+
     <TableContainer component={Paper} className="entry-dashboard-table">
       <Table sx={{ minWidth: 300 }} aria-label="simple table">
         <TableHead>
@@ -39,25 +51,23 @@ function entry_dashboard() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {tableRows(vehicles).map((entry, index) => (
+          {/* {tableRows(vehicles).map((entry, index) => (
             <TableRow
               key={index}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {entry.carNum}
-              </TableCell>
+              <TableCell component="th" scope="row">{entry.carNum}</TableCell>
               <TableCell align="right">{entry.teamName}</TableCell>
               <TableCell colSpan={entry.driver2 ? 1 : 2} align="right">{entry.driver1}</TableCell>
               {entry.driver2 && <TableCell align="right">{entry.driver2}</TableCell>}
               <TableCell align="right">{entry.vehicle}</TableCell>
             </TableRow>
-          ))}
+          ))} */}
         </TableBody>
       </Table>
     </TableContainer>
-    </>
+    </div>
   )
 }
 
-export default entry_dashboard
+export default EntryDashboard
