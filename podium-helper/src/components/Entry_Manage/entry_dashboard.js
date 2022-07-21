99bb31503c2788550b83b10a66cebd11b33bddde
Brendan Woo <woo.brendan@gmail.com>
2022-07-20 22:53:27 -0400

    import useEntries, and import vehicle entries to future map to dashboard

--- podium-helper/src/components/Entry_Manage/entry_dashboard.js
+++ podium-helper/src/components/Entry_Manage/entry_dashboard.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import useEntries from '../../useEntries'
 
 function entry_dashboard() {
+  const {vehicles} = useEntries();
+  //create component to map through each vehicle entry and display options
   return (
     <div>
       
