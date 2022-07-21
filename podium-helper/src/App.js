a4dc477647405881ef41ba87d17fb25a038a51d1
Brendan Woo <woo.brendan@gmail.com>
2022-07-21 14:09:32 -0400

    add route for entry dashboard

--- podium-helper/src/App.js
+++ podium-helper/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Podium from './components/Podium_Results/Podium';
 import NavBar from './components/NavBar';
 import Results from './components/Results/ResultsHistory';
+import EntryDashboard from './components/Entry_Manage/entry_dashboard';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Podium />} />
           <Route path="/competitors" element={<DriverSearch />} />
           <Route path="/results" element={<Results />} />
+          <Route path="/entry-dashboard" element={<EntryDashboard />} />
         </Routes>
       </div>
     </Router>
