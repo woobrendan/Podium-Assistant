e23414c3d04d2cbb107784462391154b400abd5f
Brendan Woo <woo.brendan@gmail.com>
2022-07-22 11:49:46 -0400

    add useState  edit,  dynamically render button given edit state

--- podium-helper/src/components/Entry_Manage/editButton.js
+++ podium-helper/src/components/Entry_Manage/editButton.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import {useState} from 'react'
 import { Button } from '@mui/material'
 
-function EditButton() {
+function EditButton(props) {
+  const [edit, setEdit] = useState(false);
+  const onClick = () => {
+    !edit ? setEdit(true) : setEdit(false)
+  }
+
   return (
-    <Button  variant="outlined">
-      Edit
-    </Button>
+    !edit 
+    ? <Button variant="outlined"  onClick={onClick}>Edit</Button>  
+    : <Button variant="contained" onClick={onClick}>Confirm</Button>
   )
 }
 
