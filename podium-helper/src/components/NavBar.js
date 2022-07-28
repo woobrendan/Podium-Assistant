dec2ae84b683fb47afc20393956110d5da4a50c2
Brendan Woo <woo.brendan@gmail.com>
2022-07-28 15:51:09 -0400

    change import for MUI package to descnstruct, in attempt to fix docker modules missing

--- podium-helper/src/components/NavBar.js
+++ podium-helper/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import AppBar from '@mui/material/AppBar';
+import {AppBar} from '@mui/material';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
