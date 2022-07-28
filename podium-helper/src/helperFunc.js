bd4ab3fee5c67b52174e906fc5a6b6b594c5a659
Brendan Woo <woo.brendan@gmail.com>
2022-07-28 13:47:59 -0400

    move compare function into helperFun to be used elsewhere

--- podium-helper/src/helperFunc.js
+++ podium-helper/src/helperFunc.js
@@ -27,9 +27,20 @@ const printPage = () => {
   window.print()
 }
 
+const compare = (a, b) => {
+  if (a.date < b.date) {
+    return -1
+  }
+  if (a.date > b.date) {
+    return 1
+  }
+  return 0;
+}  
+
 export {
   getIdFromArray,
   getToday,
   printPage,
-  getVehicleId
+  getVehicleId,
+  compare
 }
