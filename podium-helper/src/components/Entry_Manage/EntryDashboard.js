01e220f3e5dbd87f1f932078ab1153f1e8ef7a8e
Brendan Woo <woo.brendan@gmail.com>
2022-07-29 12:36:20 -0400

    change MUI imports to deconstructed object from @mui/material. attempting to fix module errors in container

--- podium-helper/src/components/Entry_Manage/EntryDashboard.js
+++ podium-helper/src/components/Entry_Manage/EntryDashboard.js
@@ -1,10 +1,4 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@mui/material'
 import useEntries from '../../useEntries';
 import {useState, useEffect} from 'react'
 import EditButton from './EditButton';
