bf5e352173167963712f01acec4361c3b3cc12fb
Brendan Woo <woo.brendan@gmail.com>
2022-07-29 12:34:12 -0400

    change MUI imports to deconstructed object from @mui/material. attempting to fix module errors in container

--- podium-helper/src/components/elevationPaper.js
+++ podium-helper/src/components/elevationPaper.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
+import {Grid, Box, Paper} from '@mui/material'
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
 const Item = styled(Paper)(({ theme }) => ({
