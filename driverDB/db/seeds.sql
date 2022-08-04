ebf7aecb7c0778d0eaacac3e1d48ef8ba6ff05f6
Brendan Woo <woo.brendan@gmail.com>
2022-08-03 23:09:02 -0500

    add VIR TC race 2 results to fastlap and reuslts table

--- driverDB/db/seeds.sql
+++ driverDB/db/seeds.sql
@@ -485,7 +485,8 @@ INSERT INTO fastlaps (driver_id, laptime) VALUES
 (168, '1:46.414'),
 (44, '1:54.727'),
 (114, '1:53.632'),
-(115, '2:00.641');
+(115, '2:00.641'),
+(117, '1:59.608');
 
 INSERT INTO results (date, event_id, series_id, fastlap_id) VALUES
 ('2022-06-18', 4, 1, 1),
@@ -511,7 +512,8 @@ INSERT INTO results (date, event_id, series_id, fastlap_id) VALUES
 ('2022-06-19', 4, 3, 21),
 ('2022-06-18', 4, 2, 22),
 ('2022-06-19', 4, 2, 23),
-('2022-06-18', 4, 4, 24);
+('2022-06-18', 4, 4, 24),
+('2022-06-19', 4, 4, 25);
 
 INSERT INTO podiums (class_id, first_place, second_place, third_place, result_id) VALUES
 (1, 1, 19, 2, 1),
