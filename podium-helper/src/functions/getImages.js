dc02676a3452eb65e867c2e2cb3e8e3cdb057a99
Brendan Woo <woo.brendan@gmail.com>
2022-08-26 13:20:45 -0700

    fix import of image to go up one folder to images

--- podium-helper/src/functions/getImages.js
+++ podium-helper/src/functions/getImages.js
@@ -4,7 +4,7 @@ import mercedesLogo from '../images/mercedes.png'
 import acuraLogo from '../images/acura.png';
 import astonMartin from '../images/aston_martin.png';
 import ferrari from '../images/ferrari.png';
-import ginetta from './images/ginetta.png';
+import ginetta from '../images/ginetta.png';
 import honda from './images/honda.png';
 import lamborghini from './images/lamborghini.png';
 import MINI from './images/MINI.png';
