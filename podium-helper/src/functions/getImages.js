4ad832112f0b5370ce226507cc494a9c0891d1d5
Brendan Woo <woo.brendan@gmail.com>
2022-08-29 11:09:13 -0700

    fix import for MINI  img going up one folder

--- podium-helper/src/functions/getImages.js
+++ podium-helper/src/functions/getImages.js
@@ -7,7 +7,7 @@ import ferrari from '../images/ferrari.png';
 import ginetta from '../images/ginetta.png';
 import honda from '../images/honda.png';
 import lamborghini from '../images/lamborghini.png';
-import MINI from './images/MINI.png';
+import MINI from '../images/MINI.png';
 import toyota from './images/toyota.png';
 import audi from './images/audi.png';
 import corvette from './images/corvette.png'
