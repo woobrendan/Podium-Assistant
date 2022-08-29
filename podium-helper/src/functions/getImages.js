713795ac98a40aafa7676a0aff578cdb2c84d012
Brendan Woo <woo.brendan@gmail.com>
2022-08-29 11:09:29 -0700

    fix import for toyota img going up one folder

--- podium-helper/src/functions/getImages.js
+++ podium-helper/src/functions/getImages.js
@@ -8,7 +8,7 @@ import ginetta from '../images/ginetta.png';
 import honda from '../images/honda.png';
 import lamborghini from '../images/lamborghini.png';
 import MINI from '../images/MINI.png';
-import toyota from './images/toyota.png';
+import toyota from '../images/toyota.png';
 import audi from './images/audi.png';
 import corvette from './images/corvette.png'
 import hyundai from './images/hyundai.png';
