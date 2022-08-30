ed47547126e7a6e98b605e22ec7d90822378e4c9
Brendan Woo <woo.brendan@gmail.com>
2022-08-30 15:16:27 -0700

    add condition for if car is GT2, add constant for GT2

--- podium-helper/src/functions/sortFuncs.js
+++ podium-helper/src/functions/sortFuncs.js
@@ -4,11 +4,14 @@ const sortBySeries = (listOfEntries) => {
   const gtam = "GT America";
   const gt4a = "Pirelli GT4 America";
   const gt3 = [];
+  const gt2 = [];
   const gt4 = [];
   const tc = [];
   listOfEntries.forEach((entry) => {
     if (entry.vehicle.includes('GT3')) {
       gt3.push(entry)
+    } else if (entry.vehicle.includes('GT2')) {
+      gt2.push(entry)
     } else if (entry.vehicle.includes('GT4')) {
       gt4.push(entry)
     } else {
