6c17b9bab35519c7abb0d09278b1d71f029ea164
Brendan Woo <woo.brendan@gmail.com>
2022-08-30 12:02:24 -0700

    change name of func from sortFunc to sortBySeries. Add logic to return gt3, gt4 and tc cars

--- podium-helper/src/functions/sortFuncs.js
+++ podium-helper/src/functions/sortFuncs.js
@@ -1,7 +1,23 @@
-const sortFuncs = (listOfEntries) => {
-
+const sortBySeries = (listOfEntries) => {
+  const gtwca = "GT World Challenge America";
+  const tcam = "TC America";
+  const gtam = "GT America";
+  const gt4a = "Pirelli GT4 America";
+  const gt3 = [];
+  const gt4 = [];
+  const tc = [];
+  listOfEntries.forEach((entry) => {
+    if (entry.vehicle.includes('GT3')) {
+      gt3.push(entry)
+    } else if (entry.vehicle.includes('GT4')) {
+      gt4.push(entry)
+    } else {
+      tc.push(entry)
+    }
+  })
+  return [...gt3, ...gt4, ...tc]
 }
 
 export {
-  sortFuncs
+  sortBySeries
 }
