b5564ed2b022d6a414847fbc5d97e08934a3cb56
Brendan Woo <woo.brendan@gmail.com>
2022-08-31 13:00:00 -0700

    clean up forEach loop, conditionals to remove brackets and make cleaner

--- podium-helper/src/functions/sortFuncs.js
+++ podium-helper/src/functions/sortFuncs.js
@@ -4,21 +4,16 @@ const sortByVehicleType = (listOfEntries) => {
   const gt4 = [];
   const tc = [];
   listOfEntries.forEach((entry) => {
-    if (entry.vehicle.includes('GT3')) {
-      gt3.push(entry)
-    } else if (entry.vehicle.includes('GT2')) {
-      gt2.push(entry)
-    } else if (entry.vehicle.includes('GT4')) {
-      gt4.push(entry)
-    } else {
-      tc.push(entry)
-    }
+    if (entry.vehicle.includes('GT3')) gt3.push(entry)
+    else if (entry.vehicle.includes('GT2')) gt2.push(entry)
+    else if (entry.vehicle.includes('GT4')) gt4.push(entry)
+    else tc.push(entry)
   })
   return [...gt3, ...gt2, ...gt4, ...tc]
 }
 
 const sortBySeries = () => {
-  
+
 }
 
 export {
