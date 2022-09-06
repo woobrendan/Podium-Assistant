3a8b8b64aaafab529327c62bdf02ccaa4f35cca8
Brendan Woo <woo.brendan@gmail.com>
2022-09-06 10:56:23 -0700

    remove searchOption component in place of searchAll, remove import for filteredOptions and mappedDrivers using same func

--- podium-helper/src/components/Competitor/DriverSearch.js
+++ podium-helper/src/components/Competitor/DriverSearch.js
@@ -3,7 +3,6 @@ import '../../Styling/competitors.scss'
 import {TextField} from '@mui/material'
 import {useState} from 'react';
 import BackToTopButton from './BackToTopButton';
-import filteredOptions from '../../functions/searchOptions';
 import useEntries from '../../functions/useEntries';
 import ToggleSort from './toggleSort';
 import searchAll from './searchAll';
@@ -41,10 +40,6 @@ function DriverSearch() {
         return sortBySeries(entryArray)
     }
   }
-
-  // const mappedDrivers = setSortOption(option, filteredOptions(vehicles, option, searchValue)).map((entry,index) =>(
-  //   <DriverDetails entry={entry} key={index} index={index}/>
-  // ))
 
   const searchLabel = option ? `Search by ${option}`: "Search"
   return (
