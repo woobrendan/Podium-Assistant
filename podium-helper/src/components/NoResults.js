dba0c0c50f8e69710da3d0fb4e8f322f7df51bbd
Brendan Woo <woo.brendan@gmail.com>
2022-09-07 11:37:41 -0700

    add text for noresults found

--- podium-helper/src/components/NoResults.js
+++ podium-helper/src/components/NoResults.js
@@ -3,6 +3,7 @@ export default function NoResults() {
   return (
     <div className="No-Result">
       <h1>No Results Found</h1>
+      <p>Could not find what you were searching for. Try searching again</p>
     </div>
   )
 }
