9b06e51b428e9faf7f092b57c8cc30f7e07fdc4b
Brendan Woo <woo.brendan@gmail.com>
2022-09-13 12:00:57 -0700

    route for api/drivers and use route

--- Mongo_DB/server.js
+++ Mongo_DB/server.js
@@ -3,5 +3,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+// const pool = require('./lib/db');
+
+const apiDriverRoute = require('./routes/api/drivers');
+
+app.use('/api/drivers', apiDriverRoute());
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`))
