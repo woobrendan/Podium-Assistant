6a02eca99e09d46fe54e68ac0ca5b9476fa349ac
Brendan Woo <woo.brendan@gmail.com>
2022-09-13 12:02:09 -0700

    add route folder, with api folder and drivers folder

---
+++ Mongo_DB/routes/api/drivers.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = (db) => {
+  router.get('/', (req, res) => {
+    res.send('Hello World')
+  })
+
+  return router
+}
