5a36637f10764d7c47956986a09aa93d91f0bd1f
Brendan Woo <woo.brendan@gmail.com>
2022-09-13 11:45:00 -0700

    update server file with const app, usee express public and listen

--- Mongo_DB/server.js
+++ Mongo_DB/server.js
@@ -1,1 +1,7 @@
-const express = require('express')
+const PORT = process.env.PORT || 2020;
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`))
