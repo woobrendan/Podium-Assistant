88a63bbd4c5aee3382e95db2f7316a6532ed6eda
Brendan Woo <woo.brendan@gmail.com>
2022-09-14 11:56:26 -0700

    complete connection file, with module export

--- Mongo_DB/db/conn.js
+++ Mongo_DB/db/conn.js
@@ -1,2 +1,27 @@
 const {MongoClient} = require('mongodb')
 const connectionString = process.env.ATLAS_URI;
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifedTopology: true
+});
+
+let dbConnection;
+
+module.exports = {
+  connectToServer: function(callback) {
+    client.connect(function (err, db) {
+      if (err || !db) {
+        return callback(err)
+      }
+    
+      dbConnection = db.db('sro_db');
+      console.log('Successfully connected to MongoDB.')
+
+      return callback();
+    });
+  },
+
+  getDb: function() {
+    return dbConnection;
+  }
+}
