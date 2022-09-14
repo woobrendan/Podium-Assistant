106e614e74fa384527973f16f1ef4be14a0f5fc5
Brendan Woo <woo.brendan@gmail.com>
2022-09-14 14:56:17 -0700

    use body parser, set up mongoclient outside of mongoose then comment out

--- Mongo_DB/routes/api/drivers.js
+++ Mongo_DB/routes/api/drivers.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+// const dbo = require('../../db/conn')
+const Entry = require('../../models/entries_schema')
 
+  //route = /api/drivers
+router.get('/', aysnc (req, res) => {
+  try {
+    const entries = await Entry.find()
+    res.json(entries)
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
+  // const dbConnect = dbo.getDb();
 
-module.exports = (db) => {
-  router.get('/', (req, res) => {
-    res.send('Hello World')
-  })
+  // db
+  //   .collection('entries')
+  //   .find()
+  //   .toArray((err, result) => {
+  //     if (err) {
+  //       res.status(400).send('Error fetching entries');
+  //     } else {
+  //       res.json(result)
+  //     }
+  //   })
+})
 
-  return router
-}
+module.exports = router
