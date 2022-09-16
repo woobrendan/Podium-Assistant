46b607634044230b95bdb03835289c5dea6e578f
Brendan Woo <woo.brendan@gmail.com>
2022-09-16 12:20:08 -0700

    update mongoose.connect to take process.env.COMPASS_URL. move express.json to top and comment out MongoClient connection

--- Mongo_DB/server.js
+++ Mongo_DB/server.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 2020;
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const app = express();
-const MongoClient = require('mongodb').MongoClient
+// const MongoClient = require('mongodb').MongoClient
 
 app.use(express.static('public'));
+app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,12 +20,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // })
 // .catch(err => console.error(err))
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewURLParser: true })
+mongoose.connect(process.env.COMPASS_URL, { useNewURLParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
-app.use(express.json())
 
 const apiDriverRoute = require('./routes/api/drivers');
 
