9ce25049ae5ec9148b748c8c4e03ebc8e86d4b79
Brendan Woo <woo.brendan@gmail.com>
2022-09-19 23:49:39 -0400

    create file results to house seeding file for results collection

---
+++ Mongo_DB/seeds/results.js
@@ -0,0 +1,48 @@
+[
+  {
+    "series": "GT America",
+    "date": "09-19-2022",
+    "event": "St. Petersburg",
+    "fastLap": {
+      "driver": "Marko Radisic",
+      "vehicle": "BMW M4 GT4",
+      "time": "1:11.111"
+    },
+    "result1": {
+      "class": "SRO3",
+      "firstPlace": {
+        //driver info
+      },
+      "secondPlace": {
+        //driver info
+      },
+      "thirdPlace": {
+        //driver info
+      }
+    },
+    "result2": {
+      "class": "GT4",
+      "firstPlace": {
+        //driver info
+      },
+      "secondPlace": {
+        //driver info
+      },
+      "thirdPlace": {
+        //driver info
+      }
+    },
+    "result3": {
+      "class": "GT2",
+      "firstPlace": {
+        //driver info
+      }
+    },
+    "result4": {
+      "class": "Masters",
+      "firstPlace": {
+        //driver info
+      }
+    }
+  }
+]
