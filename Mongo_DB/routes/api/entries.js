a65cea812dddb54ed8b41b20ab899c3391ce1c80
Brendan Woo <woo.brendan@gmail.com>
2022-09-21 23:03:41 -0400

    remove commented out connection, after confirming db connection, remove dbo connection statement

--- Mongo_DB/routes/api/entries.js
+++ Mongo_DB/routes/api/entries.js
@@ -1,29 +1,15 @@
 const express = require('express');
 const router = express.Router();
-// const dbo = require('../../db/conn')
 const Entry = require('../../models/entries_schema')
 
   //route = /api/drivers
 router.get('/', async (req, res) => {
   try {
     const entries =  await Entry.find()
-    // console.log('entries', entries)
     res.json(entries)
   } catch (err) {
     res.status(500).json({message: err.message})
   }
-  // const dbConnect = dbo.getDb();
-
-  // db
-  //   .collection('entries')
-  //   .find()
-  //   .toArray((err, result) => {
-  //     if (err) {
-  //       res.status(400).send('Error fetching entries');
-  //     } else {
-  //       res.json(result)
-  //     }
-  //   })
 })
 
 router.get('/:id', (req, res) => {
