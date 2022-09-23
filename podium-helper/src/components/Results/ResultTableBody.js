0754435bb5863c25189158ef7208ff970bc8c633
Brendan Woo <woo.brendan@gmail.com>
2022-09-22 23:33:01 -0400

    change references to first_place to firstPlace, same for second and third, to match mongodb data

--- podium-helper/src/components/Results/ResultTableBody.js
+++ podium-helper/src/components/Results/ResultTableBody.js
@@ -12,9 +12,9 @@ function createDataSingleDriver(place, number, driver1, team, car) {
 export default function ResultTableBbody(props) {
 
   const podium = props.results;
-  const first = podium.first_place;
-  const second = podium.second_place;
-  const third = podium.third_place;
+  const first = podium.firstPlace;
+  const second = podium.secondPlace;
+  const third = podium.thirdPlace;
 
   const checkPodium = () => {
     const allResults = [];
