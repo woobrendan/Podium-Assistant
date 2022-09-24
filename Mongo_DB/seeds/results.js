4024048c7197c0b579d33ac10c80de8d73f39ef8
Brendan Woo <woo.brendan@gmail.com>
2022-09-23 22:57:26 -0400

    add GTWCA sonoma race 1 result1

--- Mongo_DB/seeds/results.js
+++ Mongo_DB/seeds/results.js
@@ -122,5 +122,70 @@
         "team": "TR3 Racing"
       }
     }
+  },
+  {
+    "series": "GT World Challenge America",
+    "date": "04-16-2022",
+    "event": "Sonoma Raceway",
+    "fastLap": {
+      "driver": "Jordan Pepper",
+      "vehicle": "Lamborghini Huracan GT3",
+      "time": "1:37.310"
+    },
+    "result1": {
+      "class": "Pro",
+      "firstPlace": {
+        "driver1": "Jordan Pepper",
+        "driver2": "Andrea Caldarelli",
+        "vehicle": "Lamborghini Huracan GT3",
+        "team": "K-PAX Racing"
+      },
+      "secondPlace": {
+        "driver1": "Steven Aghakhani",
+        "driver2": "Loris Spinelli",
+        "vehicle": "Mercedes-AMG GT3",
+        "team": "US Racetronics"
+      },
+      "thirdPlace": {
+        "driver1": "Misha Goikhberg",
+        "driver2": "Giacomo Altoe",
+        "vehicle": "Lamborghini Huracan GT3",
+        "team": "K-PAX Racing"
+      }
+    },
+    "result2": {
+      "class": "GT4",
+      "firstPlace": {
+        "driver": "Jason Bell",
+        "vehicle": "Aston Martin Vantage GT4",
+        "team": "GMG Racing"
+      },
+      "secondPlace": {
+        "driver": "Chris Gumprecht",
+        "vehicle": "Mercedes-AMG GT4",
+        "team": "RENNtech Motorsports"
+      },
+      "thirdPlace": {
+        "driver": "Moisey Uretsky",
+        "vehicle": "Aston Martin Vantage GT4",
+        "team": "Accelerating Peformance"
+      }
+    },
+    "result3": {
+      "class": "GT2",
+      "firstPlace": {
+        "driver": "CJ Moses",
+        "vehicle": "Audi R8 LMS GT2",
+        "team": "GMG Racing"
+      }
+    },
+    "result4": {
+      "class": "Masters",
+      "firstPlace": {
+        "driver": "Ziad Ghandour",
+        "vehicle": "Lamborghini Huracan GT3",
+        "team": "TR3 Racing"
+      }
+    }
   }
 ]
