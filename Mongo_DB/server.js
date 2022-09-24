af964d46a7e385c9aca2a95deca320b7383eef4c
Brendan Woo <woo.brendan@gmail.com>
2022-09-23 23:23:30 -0400

    require cors and use cors

--- Mongo_DB/server.js
+++ Mongo_DB/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.static('public'));
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(cors());
 
 mongoose.connect(process.env.COMPASS_URL, { useNewURLParser: true })
 const db = mongoose.connection
