5af91cfa4e2e20a339dfa08ac7871cd935b56abd
Brendan Woo <woo.brendan@gmail.com>
2022-09-23 22:50:37 -0400

    import results schema and implement get route

--- Mongo_DB/routes/api/results.js
+++ Mongo_DB/routes/api/results.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Results = require('../../models/result_schema')
+
+router.get('/', async (req, res) => {
+  try {
+    const results =  await Results.find()
+    res.json(results)
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
+})
 
 module.exports = router;
