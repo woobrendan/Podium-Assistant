3193e885910f67c57b659e6b28c90e08edfa31e5
Brendan Woo <woo.brendan@gmail.com>
2022-09-26 15:48:24 -0700

    add GTWCA nola race 2 results for proam and am

--- Mongo_DB/seeds/results.js
+++ Mongo_DB/seeds/results.js
@@ -830,25 +830,25 @@
     "result2": {
       "class": "Pro/Am",
       "firstPlace": {
-        "driver1": "George Kurtz",
-        "driver2": "Colin Braun",
-        "number": "04",
-        "vehicle": "Mercedes-AMG GT3",
-        "team": "CrowdStrike Racing by Riley Motorsports"
-      },
-      "secondPlace": {
         "driver1": "Ashton Harrison",
         "driver2": "Mario Farnbacher",
         "number": "93",
         "vehicle": "Acura NSX GT3",
         "team": "Racers Edge Motorsports"
       },
-      "thirdPlace": {
+      "secondPlace": {
         "driver1": "Chandler Hull",
         "driver2": "Bill Auberlen",
         "number": "94",
         "vehicle": "BMW M4 GT3",
         "team": "BimmerWorld"
+      },
+      "thirdPlace": {
+        "driver1": "George Kurtz",
+        "driver2": "Colin Braun",
+        "number": "04",
+        "vehicle": "Mercedes-AMG GT3",
+        "team": "CrowdStrike Racing by Riley Motorsports"
       }
     },
     "result3": {
