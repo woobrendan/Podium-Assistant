8a2950346529b84bc02f32db44185738faf5eca5
Brendan Woo <woo.brendan@gmail.com>
2022-09-28 11:00:34 -0700

    add PGT4A VIR race 1 results for proam

--- Mongo_DB/seeds/results.js
+++ Mongo_DB/seeds/results.js
@@ -1491,25 +1491,25 @@
     "result2": {
       "class": "Pro/Am",
       "firstPlace": {
-        "driver1": "Bryan Putt",
-        "driver2": "Kenton Koch",
-        "number": "15",
-        "vehicle": "Aston Martin Vantage GT4",
-        "team": "BSport Racing"
+        "driver1": "Tom Capizzi",
+        "driver2": "John Capestro Dubets",
+        "number": "52",
+        "vehicle": "BMW M4 GT4",
+        "team": "AutoTechnic Racing"
       },
       "secondPlace": {
-        "driver1": "Scott Noble",
-        "driver2": "Jason Hart",
-        "number": "47",
-        "vehicle": "Porsche 718 Cayman GT4 RS CS",
-        "team": "NOLAsport"
+        "driver1": "Edgar Lau",
+        "driver2": "Mike Skeen",
+        "number": "124",
+        "vehicle": "Ford Mustang GT4",
+        "team": "ARG / Rotek Racing"
       },
       "thirdPlace": {
-        "driver1": "Elias Sabo",
-        "driver2": "Andy Lee",
-        "number": "8",
-        "vehicle": "Aston Martin Vantage GT4",
-        "team": "Flying Lizard Motorsports"
+        "driver1": "Adam Adelson",
+        "driver2": "Elliot Skeer",
+        "number": "120",
+        "vehicle": "Porsche 718 Cayman GT4 RS CS",
+        "team": "Premier Racing"
       }
     },
     "result3": {
