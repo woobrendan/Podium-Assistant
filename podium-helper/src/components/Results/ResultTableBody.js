a056aed125433a2e5403cab93347fa8bcc452ddc
Brendan Woo <woo.brendan@gmail.com>
2022-09-29 11:53:18 -0700

    create driver3 data function. implement that option. add column to table

--- podium-helper/src/components/Results/ResultTableBody.js
+++ podium-helper/src/components/Results/ResultTableBody.js
@@ -9,6 +9,10 @@ function createDataSingleDriver(place, number, driver1, team, car) {
   return { place, number, driver1, team, car };
 }
 
+const createDataThreeDriver = (place, number, driver1, driver2, team, car) => {
+  return { place, number, driver1, driver2, driver3, team, car };
+}
+
 export default function ResultTableBbody(props) {
 
   const podium = props.results;
@@ -19,16 +23,21 @@ export default function ResultTableBbody(props) {
   const checkPodium = () => {
     const allResults = [];
     if(!first.driver2) {
+
       allResults.push(createDataSingleDriver('1st', first.number, first.driver1.name, first.team, first.vehicle))
+
       if (second) {
-        allResults.push(createDataSingleDriver('2nd', second.number, second.driver1.name, second.team, second.vehicle));
+        allResults.push(createDataSingleDriver('2nd', second.number, second.driver1.name, second.team, second.vehicle))
       } 
       if (third) {
         allResults.push(createDataSingleDriver('3rd', third.number, third.driver1.name, third.team, third.vehicle));
       }
       return allResults; 
-    } else {
+    } 
+      else if (first.driver2 && !first.driver3) {
+
       allResults.push(createDataTwoDriver('1st', first.number, first.driver1.name, first.driver2.name, first.team, first.vehicle));
+
       if (second) {
         allResults.push(createDataTwoDriver('2nd', second.number, second.driver1.name, second.driver2.name, second.team, second.vehicle));
       } 
@@ -37,8 +46,19 @@ export default function ResultTableBbody(props) {
       }
       return allResults;
     }
+      else {
+
+        allResults.push(createDataThreeDriver('1st', first.number, first.driver1.name, first.driver2.name, first.driver3.name, first.team, first.vehicle));
+
+      if (second) {
+        allResults.push(createDataThreeDriver('2nd', second.number, second.driver1.name, second.driver2.name, second.driver3.name, second.team, second.vehicle));
+      } 
+      if (third) {
+        allResults.push(createDataThreeDriver('3rd', third.number, third.driver1.name, third.driver2.name, third.driver3.name, third.team, third.vehicle));
+      }
+    }
   }
-  // console.log('checkPod', checkPodium())
+
   return (
     <>
       <TableHead>
@@ -58,6 +78,7 @@ export default function ResultTableBbody(props) {
             <TableCell align="right">{row.number}</TableCell>
             <TableCell align="right">{row.driver1}</TableCell>
             {row.driver2 && <TableCell align="right">{row.driver2}</TableCell>}
+            {row.driver3 && <TableCell align="right">{row.driver3}</TableCell>}
             <TableCell align="right">{row.team}</TableCell>
             <TableCell align="right">{row.car}</TableCell>
           </TableRow>
