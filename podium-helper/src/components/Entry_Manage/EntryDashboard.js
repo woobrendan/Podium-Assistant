1240371ee41d0ce7815b12b74a036724ac33d5b0
Brendan Woo <woo.brendan@gmail.com>
2022-10-06 17:38:24 -0400

    import toggleSort and textfield. return in component, wrap in fragment. add usestate values forr searchValue and option

--- podium-helper/src/components/Entry_Manage/EntryDashboard.js
+++ podium-helper/src/components/Entry_Manage/EntryDashboard.js
@@ -1,10 +1,31 @@
 import EntryRows from './EntryRows';
+import ToggleSort from '../Competitor/toggleSort';
 import '../../Styling/entryDash.scss'
+import {useState} from 'react';
+import { TextField } from '@mui/material'
 
 function EntryDashboard() {
 
+  const [searchValue, setSearchValue] = useState('');
+  const [option, setOption] = useState('');
+  
+  const getSortOption = (option) => setOption(option);
+
   return (
+    <>
+    <div className="search-sort-options">
+        <ToggleSort getOption={getSortOption} />
+        <TextField 
+          className="form-control"
+          label="Search" 
+          variant="outlined" 
+          color="error"
+          value={searchValue}
+          onChange={e => {setSearchValue(e.target.value)}}
+        />
+      </div>
     <EntryRows />
+    </>
   )
 }
 
