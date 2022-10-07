ba43754cf21aceff4ca37ccd81790ac6b0467c4f
Brendan Woo <woo.brendan@gmail.com>
2022-10-07 11:12:41 -0400

    change fragment into div and give className entrry-dhasboard to target with css

--- podium-helper/src/components/Entry_Manage/EntryDashboard.js
+++ podium-helper/src/components/Entry_Manage/EntryDashboard.js
@@ -37,20 +37,20 @@ function EntryDashboard() {
   //pass those remaining entries down to entryrows
 
   return (
-    <>
-    <div className="search-sort-options">
-        <ToggleSort getOption={getSortOption} />
-        <TextField 
-          className="form-control"
-          label="Search" 
-          variant="outlined" 
-          color="error"
-          value={searchValue}
-          onChange={e => {setSearchValue(e.target.value)}}
-        />
-      </div>
-    {sortedEntries}
-    </>
+    <div className="entry-dashboard">
+      <div className="search-sort-options">
+          <ToggleSort getOption={getSortOption} />
+          <TextField 
+            className="form-control"
+            label="Search" 
+            variant="outlined" 
+            color="error"
+            value={searchValue}
+            onChange={e => {setSearchValue(e.target.value)}}
+          />
+        </div>
+      {sortedEntries}
+    </div>
   )
 }
 
