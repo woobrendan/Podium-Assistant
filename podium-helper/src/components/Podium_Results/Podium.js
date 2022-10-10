fb462d53f9bb274c902ed11b4500a0ad4926aa6a
Brendan Woo <woo.brendan@gmail.com>
2022-10-10 15:42:22 -0700

    remove constants for results. remove all sql podium formation logic into formSQLPodium file

--- podium-helper/src/components/Podium_Results/Podium.js
+++ podium-helper/src/components/Podium_Results/Podium.js
@@ -38,12 +38,6 @@ function Podium() {
   })
 
   const handleFastLapSumbit = (value) => {
-    const result1 = results.result1;
-    const result2 = results.result2;
-    const result3 = results.result3;
-    const result4 = results.result4;
-    
-
     setResults((prev) => ({
       ...prev,
       fastLap: {...value, id: getIdFromArray(value.driver, drivers)}
@@ -58,45 +52,6 @@ function Podium() {
       result3: false,
       result4: false
     }));
-
-    // create copy of results useState to get proper values to pass. change values of drivers into their id number for backend
-    const copyResults = () => {
-      const copy = {
-        ...results,
-        result1: {
-          class: getIdFromArray(result1.class, classCategory),
-          firstPlace: getVehicleId(result1.firstPlace.number, vehicles),
-          secondPlace: (getVehicleId(result1.secondPlace.number, vehicles) || null),
-          thirdPlace: (getVehicleId(result1.thirdPlace.number, vehicles) || null)
-        },
-        result2: {
-          class: getIdFromArray(result2.class, classCategory),
-          firstPlace: getVehicleId(result2.firstPlace.number, vehicles),
-          secondPlace: (getVehicleId(result2.secondPlace.number, vehicles) || null),
-          thirdPlace: (getVehicleId(result2.thirdPlace.number, vehicles) || null)
-        },
-        event: getIdFromArray(results.event, events),
-        series: getIdFromArray(results.series, series),
-        fastLap: {...value, id: getIdFromArray(value.driver, drivers)}
-      }
-      if (result3) {
-        copy['result3'] = {
-          class: getIdFromArray(result3.class, classCategory),
-          firstPlace: getVehicleId(result3.firstPlace.number, vehicles),
-          secondPlace: (getVehicleId(result3.secondPlace.number, vehicles) || null),
-          thirdPlace: (getVehicleId(result3.thirdPlace.number, vehicles) || null)
-        }
-      }
-      if (result4) {
-        copy['result4'] = {
-          class: getIdFromArray(result4.class, classCategory),
-          firstPlace: getVehicleId(result4.firstPlace.number, vehicles),
-          secondPlace: (getVehicleId(result4.secondPlace.number, vehicles) || null),
-          thirdPlace: (getVehicleId(result4.thirdPlace.number, vehicles) || null)
-        }
-      }
-      return copy;
-    }
 
     axios.post(`http://localhost:2020/api/results/new`, {
       results: mongoResult(results, value)
