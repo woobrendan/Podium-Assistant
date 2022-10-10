cf032e20850ff426f568e4b8a06b5a1b6c945050
Brendan Woo <woo.brendan@gmail.com>
2022-10-10 10:24:06 -0400

    import props, create constant for entry and pass key props.index

--- podium-helper/src/components/Entry_Manage/EntryRow.js
+++ podium-helper/src/components/Entry_Manage/EntryRow.js
@@ -1,7 +1,10 @@
 function EntryRow(props) {
+
+  const entry = props.entry;
+  
   return (
     <TableRow
-      key={index}
+      key={props.index}
       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
     >
       <TableCell component="th" scope="row">{entry.carNum}</TableCell>
