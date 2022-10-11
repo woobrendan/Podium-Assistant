ee59474d0fbabf5424e79603eef4c81bcadfd77e
Brendan Woo <woo.brendan@gmail.com>
2022-10-10 17:37:48 -0700

    import props editStatus and apply to useState edit

--- podium-helper/src/components/Entry_Manage/editButton.js
+++ podium-helper/src/components/Entry_Manage/editButton.js
@@ -2,15 +2,17 @@ import {useState} from 'react'
 import { Button } from '@mui/material'
 
 function EditButton(props) {
-  const [edit, setEdit] = useState(false);
+  const [edit, setEdit] = useState(props.editStatus);
   const onClick = () => {
-    !edit ? setEdit(true) : setEdit(false)
+    !edit 
+    ? setEdit(true) 
+    : setEdit(false)
   }
 
   return (
-    !edit 
-    ? <Button variant="outlined"  onClick={onClick}>Edit</Button>  
-    : <Button variant="contained" onClick={onClick}>Confirm</Button>
+    edit 
+    ? <Button variant="contained" onClick={onClick}>Confirm</Button>
+    : <Button variant="outlined"  onClick={onClick}>Edit</Button>  
   )
 }
 
