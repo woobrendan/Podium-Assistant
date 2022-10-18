76ba2d370b8983c2d5932fdd8f2e9a4ecc39e6ec
Brendan Woo <woo.brendan@gmail.com>
2022-10-18 12:53:01 -0700

    change return value to be fragment and conditionally render TableRow

--- podium-helper/src/components/Entry_Manage/EntryRow.js
+++ podium-helper/src/components/Entry_Manage/EntryRow.js
@@ -1,6 +1,7 @@
 import { TableCell, TableRow } from '@mui/material';
 import EditButton from './EditButton';
 import {useState} from 'react'
+import EditEntryRow from './editEntryRow';
 
 function EntryRow(props) {
   const [editStatus, setEditStatus] = useState(false)
@@ -10,27 +11,29 @@ function EntryRow(props) {
   }
 
   return (
-    <TableRow
-      key={props.index}
-      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-    >
-      <TableCell component="th" scope="row">{entry.carNum}</TableCell>
-      <TableCell align="right">{entry.series}</TableCell>
-      <TableCell align="right">{entry.teamName}</TableCell>
-      <TableCell align="right">{entry.driver1}</TableCell>
-      {entry.driver2 
-        ? <TableCell align="right">{entry.driver2}</TableCell>
-        : <TableCell align="right">-</TableCell>
-      }
-      {entry.driver3 
-        ? <TableCell align="right">{entry.driver3}</TableCell>
-        : <TableCell align="right">-</TableCell>
-      }
-      <TableCell align="right">{entry.vehicle}</TableCell>
-      <TableCell align="right">
-        <EditButton getEditStatus={getEditStatus} editStatus={editStatus}/>
-      </TableCell>
-    </TableRow>
+    <>
+      {!editStatus && <TableRow
+        key={props.index}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        <TableCell component="th" scope="row">{entry.carNum}</TableCell>
+        <TableCell align="right">{entry.series}</TableCell>
+        <TableCell align="right">{entry.teamName}</TableCell>
+        <TableCell align="right">{entry.driver1}</TableCell>
+        {entry.driver2 
+          ? <TableCell align="right">{entry.driver2}</TableCell>
+          : <TableCell align="right">-</TableCell>
+        }
+        {entry.driver3 
+          ? <TableCell align="right">{entry.driver3}</TableCell>
+          : <TableCell align="right">-</TableCell>
+        }
+        <TableCell align="right">{entry.vehicle}</TableCell>
+        <TableCell align="right">
+          <EditButton getEditStatus={getEditStatus} editStatus={editStatus}/>
+        </TableCell>
+      </TableRow>}
+    </>
   )
 }
 
