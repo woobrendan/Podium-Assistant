d114190b89250f3e3a6409f5de58f13889530e57
Brendan Woo <woo.brendan@gmail.com>
2022-10-18 20:01:11 -0700

    add div for DataGrid

--- podium-helper/src/components/Entry_Manage/AllEditTable.js
+++ podium-helper/src/components/Entry_Manage/AllEditTable.js
@@ -1,6 +1,12 @@
 export default function AllEditTable() {
   return (
-    <>
-    </>
+    <div style={{ height: 300, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </div>
   )
 }
+
