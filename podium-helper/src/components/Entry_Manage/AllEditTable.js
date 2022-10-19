ef24e27e2cc34bfa3306e788292e9a20b0507ed2
Brendan Woo <woo.brendan@gmail.com>
2022-10-18 20:44:25 -0700

    adjsut field key values to match data coming in from rows, move columns outside of component. add className to wrapper div

--- podium-helper/src/components/Entry_Manage/AllEditTable.js
+++ podium-helper/src/components/Entry_Manage/AllEditTable.js
@@ -5,20 +5,11 @@ export default function AllEditTable(props) {
   const idMappedEntries = props.allEntries.map((entry, index) => {
     return {...entry, id: index}
   })
-  
+
   const rows = idMappedEntries
-  const columns = [
-    { field: 'number', headerName: 'Number', type: 'number', width: 180, editable: true },
-    { field: 'series', headerName: 'Series', editable: false },
-    { field: 'team', headerName: 'Team Name', editable: false },
-    { field: 'driver1', headerName: 'Driver 1', width: 180, editable: true },
-    { field: 'driver2', headerName: 'Driver 2', width: 180, editable: true },
-    { field: 'driver3', headerName: 'Driver 3', width: 180, editable: true },
-    { field: 'vehicle', headerName: 'Vehicle', width: 180, editable: true },
-  ];
 
   return (
-    <div style={{ height: 300, width: '100%' }}>
+    <div className="Edit-Table" style={{ height: 750, width: '100%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
@@ -28,3 +19,12 @@ export default function AllEditTable(props) {
   )
 }
 
+const columns = [
+  { field: 'carNum', headerName: 'Number', type: 'number', width: 180, editable: true },
+  { field: 'series', headerName: 'Series', editable: false },
+  { field: 'teamName', headerName: 'Team Name', editable: false },
+  { field: 'driver1', headerName: 'Driver 1', width: 180, editable: true },
+  { field: 'driver2', headerName: 'Driver 2', width: 180, editable: true },
+  { field: 'driver3', headerName: 'Driver 3', width: 180, editable: true },
+  { field: 'vehicle', headerName: 'Vehicle', width: 180, editable: true },
+];
