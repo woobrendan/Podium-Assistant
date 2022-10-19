e4dbd3987c7737dc2dcabc0306f112bcbdeec3f5
Brendan Woo <woo.brendan@gmail.com>
2022-10-18 20:08:44 -0700

    add constant columns, edit information to match needed headers

--- podium-helper/src/components/Entry_Manage/AllEditTable.js
+++ podium-helper/src/components/Entry_Manage/AllEditTable.js
@@ -10,3 +10,13 @@ export default function AllEditTable() {
   )
 }
 
+const columns = [
+  { field: 'number', headerName: 'Number', type: 'number', width: 180, editable: true },
+  { field: 'series', headerName: 'Series', editable: false },
+  { field: 'team', headerName: 'Team Name', editable: false },
+  { field: 'driver1', headerName: 'Driver 1', width: 180, editable: true },
+  { field: 'driver2', headerName: 'Driver 2', width: 180, editable: true },
+  { field: 'driver3', headerName: 'Driver 3', width: 180, editable: true },
+  { field: 'vehicle', headerName: 'Vehicle', width: 180, editable: true },
+  
+];
