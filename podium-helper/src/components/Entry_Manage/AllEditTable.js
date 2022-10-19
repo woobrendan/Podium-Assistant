3a620486d142266e3c5006930258dbd4c3a3433e
Brendan Woo <woo.brendan@gmail.com>
2022-10-19 16:58:39 -0700

    change vehicle column to be aligned center. add width to team name

--- podium-helper/src/components/Entry_Manage/AllEditTable.js
+++ podium-helper/src/components/Entry_Manage/AllEditTable.js
@@ -30,9 +30,9 @@ export default function AllEditTable(props) {
 const columns = [
   { field: 'carNum', headerName: 'Number', type: 'number', width: 80, editable: true, align: 'center' },
   { field: 'series', headerName: 'Series', width: 180, editable: false, align: 'center' },
-  { field: 'teamName', headerName: 'Team Name', editable: false, align: 'center' },
+  { field: 'teamName', headerName: 'Team Name', width: 180, editable: false, align: 'center' },
   { field: 'driver1', headerName: 'Driver 1', width: 180, editable: true, align: 'center' },
   { field: 'driver2', headerName: 'Driver 2', width: 180, editable: true, align: 'center' },
   { field: 'driver3', headerName: 'Driver 3', width: 180, editable: true, align: 'center' },
-  { field: 'vehicle', headerName: 'Vehicle', width: 180, editable: true, align: 'right' },
+  { field: 'vehicle', headerName: 'Vehicle', width: 180, editable: true, align: 'center' },
 ];
