551871b34835f8929bafe05586dd4216650567c4
Brendan Woo <woo.brendan@gmail.com>
2022-10-18 20:15:05 -0700

    import props. pass rows as props.AllEntries

--- podium-helper/src/components/Entry_Manage/AllEditTable.js
+++ podium-helper/src/components/Entry_Manage/AllEditTable.js
@@ -1,4 +1,15 @@
-export default function AllEditTable() {
+export default function AllEditTable(props) {
+  const rows = props.allEntries
+  const columns = [
+    { field: 'number', headerName: 'Number', type: 'number', width: 180, editable: true },
+    { field: 'series', headerName: 'Series', editable: false },
+    { field: 'team', headerName: 'Team Name', editable: false },
+    { field: 'driver1', headerName: 'Driver 1', width: 180, editable: true },
+    { field: 'driver2', headerName: 'Driver 2', width: 180, editable: true },
+    { field: 'driver3', headerName: 'Driver 3', width: 180, editable: true },
+    { field: 'vehicle', headerName: 'Vehicle', width: 180, editable: true },
+  ];
+
   return (
     <div style={{ height: 300, width: '100%' }}>
       <DataGrid
@@ -10,13 +21,3 @@ export default function AllEditTable() {
   )
 }
 
-const columns = [
-  { field: 'number', headerName: 'Number', type: 'number', width: 180, editable: true },
-  { field: 'series', headerName: 'Series', editable: false },
-  { field: 'team', headerName: 'Team Name', editable: false },
-  { field: 'driver1', headerName: 'Driver 1', width: 180, editable: true },
-  { field: 'driver2', headerName: 'Driver 2', width: 180, editable: true },
-  { field: 'driver3', headerName: 'Driver 3', width: 180, editable: true },
-  { field: 'vehicle', headerName: 'Vehicle', width: 180, editable: true },
-  
-];
