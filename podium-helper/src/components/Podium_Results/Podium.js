08b2649ba127105d9ce98430bea8267029054a54
Brendan Woo <woo.brendan@gmail.com>
2022-10-21 18:12:34 -0700

    comment out func mongoResult to test in seperate file

--- podium-helper/src/components/Podium_Results/Podium.js
+++ podium-helper/src/components/Podium_Results/Podium.js
@@ -97,76 +97,77 @@ function Podium() {
       return copy;
     }
 
-    const mongoResult = () => {
+  //   const mongoResult = () => {
       
-      const copy = {
-        ...results,
-        result1: {
-          ...results.result1,
-          firstPlace: {
-            driver1: result1.firstPlace.driver1.name,
-            driver2: result1.firstPlace.driver2.name,
-            number: result1.firstPlace.number,
-            vehicle: result1.firstPlace.vehicle,
-            team: result1.firstPlace.team
-          }
-        }
+  //     const copy = {
+  //       ...results,
+  //       result1: {
+  //         ...results.result1,
+  //         firstPlace: {
+  //           driver1: result1.firstPlace.driver1.name,
+  //           driver2: result1.firstPlace.driver2.name,
+  //           number: result1.firstPlace.number,
+  //           vehicle: result1.firstPlace.vehicle,
+  //           team: result1.firstPlace.team
+  //         }
+  //       }
+  //     }
 
-      if (result1.secondPlace) {
-        copy.result1.secondPlace = {
-          driver1: result1.secondPlace.driver1.name,
-          driver2: result1.secondPlace.driver2.name,
-          number: result1.secondPlace.number,
-          vehicle: result1.secondPlace.vehicle,
-          team: result1.secondPlace.team
-        }
-      }
-      if (result2) {
-        copy['result2'] = {
-          ...results.result2,
-          firstPlace: {
-            driver1: result2.firstPlace.driver1.name,
-            driver2: result2.firstPlace.driver2.name,
-            number: result2.firstPlace.number,
-            vehicle: result2.firstPlace.vehicle,
-            team: result2.firstPlace.team
-          }
-        }
-      }
-      if (result3) {
-        copy['result3'] = {
-          ...results.result3,
-          firstPlace: {
-            driver1: result3.firstPlace.driver1.name,
-            driver2: result3.firstPlace.driver2.name,
-            number: result3.firstPlace.number,
-            vehicle: result3.firstPlace.vehicle,
-            team: result3.firstPlace.team
-          }
-        }
-      }
+  //     if (result1.secondPlace) {
+  //       copy.result1.secondPlace = {
+  //         driver1: result1.secondPlace.driver1.name,
+  //         driver2: result1.secondPlace.driver2.name,
+  //         number: result1.secondPlace.number,
+  //         vehicle: result1.secondPlace.vehicle,
+  //         team: result1.secondPlace.team
+  //       }
+  //     }
+  //     if (result2) {
+  //       copy['result2'] = {
+  //         ...results.result2,
+  //         firstPlace: {
+  //           driver1: result2.firstPlace.driver1.name,
+  //           driver2: result2.firstPlace.driver2.name,
+  //           number: result2.firstPlace.number,
+  //           vehicle: result2.firstPlace.vehicle,
+  //           team: result2.firstPlace.team
+  //         }
+  //       }
+  //     }
+  //     if (result3) {
+  //       copy['result3'] = {
+  //         ...results.result3,
+  //         firstPlace: {
+  //           driver1: result3.firstPlace.driver1.name,
+  //           driver2: result3.firstPlace.driver2.name,
+  //           number: result3.firstPlace.number,
+  //           vehicle: result3.firstPlace.vehicle,
+  //           team: result3.firstPlace.team
+  //         }
+  //       }
+  //     }
 
-      if (result4) {
-        copy['result4'] = {
-          ...results.result4,
-          firstPlace: {
-            driver1: result4.firstPlace.driver1.name,
-            driver2: result4.firstPlace.driver2.name,
-            number: result4.firstPlace.number,
-            vehicle: result4.firstPlace.vehicle,
-            team: result4.firstPlace.team
-          }
-        }
-      }
-    return copy
-  }
-    console.log('mongo', mongoResult)
+  //     if (result4) {
+  //       copy['result4'] = {
+  //         ...results.result4,
+  //         firstPlace: {
+  //           driver1: result4.firstPlace.driver1.name,
+  //           driver2: result4.firstPlace.driver2.name,
+  //           number: result4.firstPlace.number,
+  //           vehicle: result4.firstPlace.vehicle,
+  //           team: result4.firstPlace.team
+  //         }
+  //       }
+  //     }
+  //   return copy
+  // }
+  //   console.log('mongo', mongoResult)
 
     const promise1 = axios.post(`http://localhost:8080/results/new`, {
       results: copyResults()
     });
     const promise2 = axios.post(`http://localhost:2020/api/results/new`, {
-      results: mongoResult()
+      // results: mongoResult()
     });
     // axios
     //   .post('http://localhost:8080/results/new', {
