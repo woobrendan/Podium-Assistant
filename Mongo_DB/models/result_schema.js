8753c64d8702a9daad262725ebb50497f0a93916
Brendan Woo <woo.brendan@gmail.com>
2022-10-24 15:04:23 -0700

    remove required restraints on most of objects

--- Mongo_DB/models/result_schema.js
+++ Mongo_DB/models/result_schema.js
@@ -16,15 +16,12 @@ const resultSchema = new mongoose.Schema({
     fastLap: {
       driver: {
         type: String,
-        required: true
       },
       vehicle: {
         type: String,
-        required: true
       },
       time: {
         type: String,
-        required: true
       }
     },
     result1: {
@@ -35,91 +32,72 @@ const resultSchema = new mongoose.Schema({
       firstPlace: {
         driver: {
           type: String,
-          required: true
         },
         vehicle: {
           type: String,
-          required: true
         },
         team: {
           type: String,
-          required: true
         }
       },
       secondPlace: {
         driver: {
           type: String,
-          required: true
         },
         vehicle: {
           type: String,
-          required: true
         },
         team: {
           type: String,
-          required: true
         }
       },
       thirdPlace: {
         driver: {
           type: String,
-          required: true
         },
         vehicle: {
           type: String,
-          required: true
         },
         team: {
           type: String,
-          required: true
         }
       }
     },
     result2: {
       class: {
         type: String,
-        required: true
       },
       firstPlace: {
         driver: {
           type: String,
-          required: true
         },
         vehicle: {
           type: String,
-          required: true
         },
         team: {
           type: String,
-          required: true
         }
       },
       secondPlace: {
         driver: {
           type: String,
-          required: true
         },
         vehicle: {
           type: String,
-          required: true
         },
         team: {
           type: String,
-          required: true
         }
       },
       thirdPlace: {
         driver: {
           type: String,
-          required: true
         },
         vehicle: {
           type: String,
-          required: true
         },
         team: {
           type: String,
-          required: true
         }
       }
     }
