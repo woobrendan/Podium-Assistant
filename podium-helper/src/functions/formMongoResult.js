5dfbe603b5ad7d6a9a6e19467dad9b573c74129a
Brendan Woo <woo.brendan@gmail.com>
2022-10-24 12:26:33 -0700

    fix thirdplace formation of data to not instantly plut driver2 into copy

--- podium-helper/src/functions/formMongoResult.js
+++ podium-helper/src/functions/formMongoResult.js
@@ -25,7 +25,6 @@ const mongoResult = (results, fastLap) => {
       }
     }
   }
-  console.log('copy', copy)
 
   //checking result 1 for second or third drivers
   if (result1.firstPlace.driver2) {
@@ -35,12 +34,11 @@ const mongoResult = (results, fastLap) => {
     copy.result1.firstPlace.driver3 = result1.firstPlace.driver3.name
   }
 
-  //begin checking for result2
+  //begin checking for second place result
 
   if (result1.secondPlace) {
     copy.result1.secondPlace = {
       driver1: result1.secondPlace.driver1.name,
-      // driver2: result1.secondPlace.driver2.name,
       number: result1.secondPlace.number,
       vehicle: result1.secondPlace.vehicle,
       team: result1.secondPlace.team
@@ -57,50 +55,56 @@ const mongoResult = (results, fastLap) => {
   if (result1.thirdPlace) {
     copy.result1.thirdPlace = {
       driver1: result1.thirdPlace.driver1.name,
-      driver2: result1.thirdPlace.driver2.name,
       number: result1.thirdPlace.number,
       vehicle: result1.thirdPlace.vehicle,
       team: result1.thirdPlace.team
     }
   }
 
-  if (result2) {
-    copy['result2'] = {
-      ...results.result2,
-      firstPlace: {
-        driver1: result2.firstPlace.driver1.name,
-        driver2: result2.firstPlace.driver2.name,
-        number: result2.firstPlace.number,
-        vehicle: result2.firstPlace.vehicle,
-        team: result2.firstPlace.team
-      }
-    }
+  if (result1.thirdPlace.driver2) {
+    copy.result1.hirdPlace.driver2 = result1.thirdPlace.driver2.name
   }
-  if (result3) {
-    copy['result3'] = {
-      ...results.result3,
-      firstPlace: {
-        driver1: result3.firstPlace.driver1.name,
-        driver2: result3.firstPlace.driver2.name,
-        number: result3.firstPlace.number,
-        vehicle: result3.firstPlace.vehicle,
-        team: result3.firstPlace.team
-      }
-    }
+  if (result1.thirdPlace.driver3) {
+    copy.result1.thirdPlace.driver3 = result1.thirdPlace.driver3.name
   }
 
-  if (result4) {
-    copy['result4'] = {
-      ...results.result4,
-      firstPlace: {
-        driver1: result4.firstPlace.driver1.name,
-        driver2: result4.firstPlace.driver2.name,
-        number: result4.firstPlace.number,
-        vehicle: result4.firstPlace.vehicle,
-        team: result4.firstPlace.team
-      }
-    }
-  }
+  // if (result2) {
+  //   copy['result2'] = {
+  //     ...results.result2,
+  //     firstPlace: {
+  //       driver1: result2.firstPlace.driver1.name,
+  //       driver2: result2.firstPlace.driver2.name,
+  //       number: result2.firstPlace.number,
+  //       vehicle: result2.firstPlace.vehicle,
+  //       team: result2.firstPlace.team
+  //     }
+  //   }
+  // }
+  // if (result3) {
+  //   copy['result3'] = {
+  //     ...results.result3,
+  //     firstPlace: {
+  //       driver1: result3.firstPlace.driver1.name,
+  //       driver2: result3.firstPlace.driver2.name,
+  //       number: result3.firstPlace.number,
+  //       vehicle: result3.firstPlace.vehicle,
+  //       team: result3.firstPlace.team
+  //     }
+  //   }
+  // }
+
+  // if (result4) {
+  //   copy['result4'] = {
+  //     ...results.result4,
+  //     firstPlace: {
+  //       driver1: result4.firstPlace.driver1.name,
+  //       driver2: result4.firstPlace.driver2.name,
+  //       number: result4.firstPlace.number,
+  //       vehicle: result4.firstPlace.vehicle,
+  //       team: result4.firstPlace.team
+  //     }
+  //   }
+  // }
 
   return copy
 }
