23d243664d27ed15e16b5e728583786875858a9f
Brendan Woo <woo.brendan@gmail.com>
2022-10-25 11:33:24 -0700

    import event schema, change find to search events. add module export statement

--- Mongo_DB/routes/api/events.js
+++ Mongo_DB/routes/api/events.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Events = require('../../models/events_schema');
 
 router.get('/', async (req, res) => {
   try {
-    const results =  await Results.find()
-    res.json(results)
+    const events =  await Events.find()
+    res.json(events)
   } catch (err) {
     res.status(500).json({message: err.message})
   }
 })
+
+module.exports = router;
