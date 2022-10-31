cac8c19b2927084b6093d73aeda99dd08234dc59
Brendan Woo <woo.brendan@gmail.com>
2022-10-31 13:49:05 -0700

    create fetch for recent result data, set to new state recentResult, change render from h1 to resulttableheader

--- podium-helper/src/components/Results/SingleRacePodium.js
+++ podium-helper/src/components/Results/SingleRacePodium.js
@@ -1,13 +1,29 @@
 import ResultTableHeader from "./ResultTableHeader";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 function SingleRacePodium() {
   const { results } = useParams()
+  const [recentResult, setRecentResult] = useState([])
+
+  axios.get('http://localhost:2020/api/results/recent')
+    .then(res => {
+      console.log('data', res.data)
+      setRecentResult([res.data])
+    })
+    .catch(err => {
+      console.error('Error:', err)
+    })
+    console.log('recent', recentResult)
+
+
+
   return (
-    // <ResultTableHeader results={results}/>
-    <>
-      <h1>Hello</h1>
-    </>
+    <ResultTableHeader results={recentResult}/>
+    // <>
+    //   <h1>Hello</h1>
+    // </>
   )
 }
 
