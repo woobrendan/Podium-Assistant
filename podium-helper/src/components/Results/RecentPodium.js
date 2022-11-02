8b84ade19775024122fb9dcdba884e6b3c34e04e
Brendan Woo <woo.brendan@gmail.com>
2022-11-02 11:41:26 -0700

    change name of file from recentPodium instead of single_result_podium

---
+++ podium-helper/src/components/Results/RecentPodium.js
@@ -0,0 +1,41 @@
+import ResultTableHeader from "./ResultTableHeader";
+import { useState, useEffect } from "react";
+import { printPage } from "../../functions/helperFunc";
+import axios from "axios";
+import { Button } from "@mui/material";
+import '../../Styling/result.scss'
+
+function RecentPodium() {
+
+  const [recentResult, setRecentResult] = useState({})
+  
+  useEffect(() => {
+    getAndSet()
+  }, [])
+
+  const getAndSet = async () => {
+    try {
+      const result = await axios.get('http://localhost:2020/api/results/recent')
+      setRecentResult({...result.data})
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+
+  return (
+    <div className="recent-podium-container">
+      <Button 
+        onClick={() => printPage()}
+        variant="contained" 
+        color="success"
+      >
+        Print Page
+      </Button>
+      {recentResult.result1 
+        && <ResultTableHeader results={recentResult}/>}
+    </div>
+  )
+}
+
+export default RecentPodium
