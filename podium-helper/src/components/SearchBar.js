39873e292c994d347787c6e7269ffc2534ab845e
Brendan Woo <woo.brendan@gmail.com>
2022-11-04 11:50:31 -0700

    - Add state searchValue, add onChangeHandler method. Pass method to textField

--- podium-helper/src/components/SearchBar.js
+++ podium-helper/src/components/SearchBar.js
@@ -1,13 +1,22 @@
 import { TextField } from "@mui/material"
 
 function SearchBar() {
+
+  const [searchValue, setSearchValue] = useState('')
+
+  const onChangeHandler = (event) => {
+    setSearchValue(event.target.value)
+    props.getSearchValue(event.target.value)
+  }
+
   return (
     <TextField 
       label='Search' 
       variant="outlined" 
       color="error"
       value={searchValue}
-      onChange={e => {setSearchValue(e.target.value)}}
+      // onChange={e => {setSearchValue(e.target.value)}}
+      onChange={onChangeHandler}
     />
   )
 }
