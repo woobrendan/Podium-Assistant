9fe2141c121f488f3990ab3da88b2b5e410442ef
Brendan Woo <woo.brendan@gmail.com>
2022-12-15 13:53:13 -0800

    import useDispatch and useSelector. get state for results via useEffect ti dispatch fetchData

--- podium-helper/src/components/Results/ResultsHistory.js
+++ podium-helper/src/components/Results/ResultsHistory.js
@@ -1,58 +1,67 @@
-import { useState, useEffect } from 'react';
-import { Typography, TextField } from '@mui/material'
-import ResultTableHeader from './ResultTableHeader';
-import useEntries from '../../functions/useEntries.js';
-import SearchAllResults from './SearchAllResults';
-import BackToTopButton from '../BackToTopButton';
-import EventSearch from '../EventsSearch';
+import { useState, useEffect } from "react";
+import { Typography, TextField } from "@mui/material";
+import ResultTableHeader from "./ResultTableHeader";
+import useEntries from "../../functions/useEntries.js";
+import SearchAllResults from "./SearchAllResults";
+import BackToTopButton from "../BackToTopButton";
+import EventSearch from "../EventsSearch";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "../../store/podiumActions";
 
 function ResultsHistory() {
-  const { resultHistory } = useEntries()
-  const [searchValue, setSearchValue] = useState('');
-  const [event, setEvent] = useState('');
-  const [filteredResults, setFilteredResults] = useState([])
+  const { resultHistory } = useEntries();
+  const dispatch = useDispatch();
+  const [searchValue, setSearchValue] = useState("");
+  const [event, setEvent] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
 
-  const getValue = (name, value) => setEvent(value)
-  
+  const resultsRedux = useSelector((state) => state.podium.podiumsArray);
+
+  const getValue = (name, value) => setEvent(value);
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
   const filterByEvent = () => {
     if (event) {
       const filtered = resultHistory.filter((result) => {
-        if (event === result.event) return result
-      })
+        if (event === result.event) return result;
+      });
       return filtered;
     }
-  }
+  };
   useEffect(() => {
-    event ? setFilteredResults(filterByEvent()) 
-          : setFilteredResults(resultHistory)
-  }, [event, resultHistory])
+    event
+      ? setFilteredResults(filterByEvent())
+      : setFilteredResults(resultHistory);
+  }, [event, resultHistory]);
 
-
-  const allResults = SearchAllResults(filteredResults, searchValue).map((result, index) => (
-    <ResultTableHeader results={result} key={index} />
-  ));
+  const allResults = SearchAllResults(filteredResults, searchValue).map(
+    (result, index) => <ResultTableHeader results={result} key={index} />,
+  );
 
   return (
     <div className="result-container">
       <Typography gutterBottom variant="h3" component="div">
         Result History
       </Typography>
-      <div className="filter-details" >
-        <EventSearch getValue={getValue}/>
-        <TextField 
-          label='Search' 
-          variant="outlined" 
+      <div className="filter-details">
+        <EventSearch getValue={getValue} />
+        <TextField
+          label="Search"
+          variant="outlined"
           color="error"
           value={searchValue}
-          onChange={e => {setSearchValue(e.target.value)}}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+          }}
         />
       </div>
-      <div className="result-history-list">
-        {allResults}
-      </div>
+      <div className="result-history-list">{allResults}</div>
       <BackToTopButton />
     </div>
-  )
+  );
 }
 
 export default ResultsHistory;
