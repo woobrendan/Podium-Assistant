b79f3fe142f42503ad40274d3c53ab77af4d7c84
Brendan Woo <woo.brendan@gmail.com>
2022-12-16 11:11:10 -0800

    remove resultHistory import from useEntries. Use store redux state for results and replace all references to resultHistory with results

--- podium-helper/src/components/Results/ResultsHistory.js
+++ podium-helper/src/components/Results/ResultsHistory.js
@@ -1,21 +1,19 @@
 import { useState, useEffect } from "react";
 import { Typography, TextField } from "@mui/material";
 import ResultTableHeader from "./ResultTableHeader";
-import useEntries from "../../functions/useEntries.js";
 import SearchAllResults from "./SearchAllResults";
 import BackToTopButton from "../BackToTopButton";
 import EventSearch from "../EventsSearch";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchData } from "../../store/podiumActions";
+import { fetchData } from "../../store/resultsActions";
 
 function ResultsHistory() {
-  const { resultHistory } = useEntries();
   const dispatch = useDispatch();
   const [searchValue, setSearchValue] = useState("");
   const [event, setEvent] = useState("");
   const [filteredResults, setFilteredResults] = useState([]);
 
-  const resultsRedux = useSelector((state) => state.podium.podiumsArray);
+  const results = useSelector((state) => state.results.resultsArray);
 
   const getValue = (name, value) => setEvent(value);
 
@@ -25,17 +23,15 @@ function ResultsHistory() {
 
   const filterByEvent = () => {
     if (event) {
-      const filtered = resultHistory.filter((result) => {
+      const filtered = results.filter((result) => {
         if (event === result.event) return result;
       });
       return filtered;
     }
   };
   useEffect(() => {
-    event
-      ? setFilteredResults(filterByEvent())
-      : setFilteredResults(resultHistory);
-  }, [event, resultHistory]);
+    event ? setFilteredResults(filterByEvent()) : setFilteredResults(results);
+  }, [event, results]);
 
   const allResults = SearchAllResults(filteredResults, searchValue).map(
     (result, index) => <ResultTableHeader results={result} key={index} />,
