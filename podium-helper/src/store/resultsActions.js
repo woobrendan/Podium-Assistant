d9f8ac234d709f777edfc4a9581619fa241dc630
Brendan Woo <woo.brendan@gmail.com>
2022-12-16 11:12:39 -0800

    change file over to resultActions to better refelect data being used

---
+++ podium-helper/src/store/resultsActions.js
@@ -0,0 +1,18 @@
+import { resultsActions } from "./resultsSlice";
+
+export const fetchData = () => {
+  return async (dispatch) => {
+    const fetchHandler = async () => {
+      const res = await fetch("http://localhost:2020/api/results");
+      const data = await res.json();
+      return data;
+    };
+
+    try {
+      const results = await fetchHandler();
+      dispatch(resultsActions.setResultHistory(results));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
