6f3a4429be6775a77be0a4c7be83370da0e2e4e5
Brendan Woo <woo.brendan@gmail.com>
2022-12-16 14:15:57 -0800

    import useDispatch, and dispatch resultACtions addResults passing mongoResult

--- podium-helper/src/components/Podium_Results/Podium.js
+++ podium-helper/src/components/Podium_Results/Podium.js
@@ -1,78 +1,82 @@
-import { useState , useEffect } from 'react'
-import { Button } from '@mui/material';
-import '../../Styling/podium.scss'
-import FastLap from './FastLap';
-import Series from './Series';
-import axios from 'axios';
-import EventSearch from '../EventsSearch';
-import DatePicker from './DatePicker';
-import useEntries from '../../functions/useEntries';
-import { getToday } from '../../functions/helperFunc';
-import mongoResult from '../../functions/formMongoResult';
-import WinnerPodium from './WinnerPodium';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Button } from "@mui/material";
+import "../../Styling/podium.scss";
+import FastLap from "./FastLap";
+import Series from "./Series";
+import axios from "axios";
+import EventSearch from "../EventsSearch";
+import DatePicker from "./DatePicker";
+import useEntries from "../../functions/useEntries";
+import { getToday } from "../../functions/helperFunc";
+import mongoResult from "../../functions/formMongoResult";
+import WinnerPodium from "./WinnerPodium";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { resultsActions } from "../../store/resultsSlice";
 
 function Podium() {
-  const { currentEventName } = useEntries(); 
+  const { currentEventName } = useEntries();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const [results, setResults] = useState({
     date: getToday(),
-    series: '',
-    event: '',
-    fastLap: ''
+    series: "",
+    event: "",
+    fastLap: "",
   });
 
   useEffect(() => {
     setResults((prev) => ({
       ...prev,
-      event: currentEventName
-    }))
-  }, [currentEventName])
-  
+      event: currentEventName,
+    }));
+  }, [currentEventName]);
 
-  const handleFinalSubmit = async () => {
-    try {
-      await axios.post(`http://localhost:2020/api/results/new`, {
-        results: mongoResult(results, results.fastLap)
-      })
-      navigate('/recent')
-    } catch (err) {
-      console.error(err)
-    }
-  }
+  const handleFinalSubmit = () => {
+    // console.log("mongResult", mongoResult(results, results.fastLap));
+    // try {
+    //   await axios.post(`http://localhost:2020/api/results/new`, {
+    //     results: mongoResult(results, results.fastLap),
+    //   });
+    //   navigate("/recent");
+    // } catch (err) {
+    //   console.error(err);
+    // }
+    dispatch(resultsActions.addResults(mongoResult(results, results.fastLap)));
+  };
 
   //sent to WinnerPodium as onclick to get podium result X and set to results
   const handleRacePodiumSubmit = (value, resultNumber) => {
     setResults((prev) => ({
       ...prev,
-      [`result${resultNumber}`]: value
+      [`result${resultNumber}`]: value,
     }));
-  }
+  };
 
   //grab value and name (for key) from component and set result state
   const getValue = (name, value) => {
     setResults((prev) => ({
       ...prev,
-      [name]: value
-    }))
-  }
+      [name]: value,
+    }));
+  };
 
   //dynamically render appropriate amount of WinnerPodium components based on need per series class requirements
   const numOfPodiumDisplays = (series) => {
-    const classifications = series.class
-    const mappedSeries = classifications.map((classification, index) => 
+    const classifications = series.class;
+    const mappedSeries = classifications.map((classification, index) => (
       <WinnerPodium
         key={index}
-        seriesName={series.name} 
+        seriesName={series.name}
         onClick={handleRacePodiumSubmit}
         results={results}
         classification={classification}
-        resultNum={index+1}
+        resultNum={index + 1}
       />
-    )
+    ));
     return mappedSeries;
-  }
+  };
 
   return (
     <div className="race-results-container">
@@ -84,18 +88,16 @@ function Podium() {
       <div className="podium_results_container">
         {results.series && numOfPodiumDisplays(results.series)}
       </div>
-      {results.series && <FastLap getValue={getValue} series={results.series} />}
-      {results.series && 
-        <Button 
-          variant="contained" 
-          color="success"
-          onClick={handleFinalSubmit}
-        >
+      {results.series && (
+        <FastLap getValue={getValue} series={results.series} />
+      )}
+      {results.series && (
+        <Button variant="contained" color="success" onClick={handleFinalSubmit}>
           Submit All
         </Button>
-      }
+      )}
     </div>
-  )
+  );
 }
 
-export default Podium
+export default Podium;
