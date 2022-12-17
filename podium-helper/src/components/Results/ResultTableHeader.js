7221068211a45adf0fed30bc66462e64df329dab
Brendan Woo <woo.brendan@gmail.com>
2022-12-16 19:35:35 -0800

    change props import to be descontructed results. update references and remove podiums variable

--- podium-helper/src/components/Results/ResultTableHeader.js
+++ podium-helper/src/components/Results/ResultTableHeader.js
@@ -1,28 +1,38 @@
-import React from 'react';
-import {Table, TableCell, TableContainer, TableHead, TableRow, Paper} from '@mui/material'
-import ResultTableBody from './ResultTableBody';
-import FastLapTable from '../Podium_Results/FastLapTable';
-import '../../Styling/result.scss';
-import { dateToString } from '../../functions/helperFunc';
+import React from "react";
+import {
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import ResultTableBody from "./ResultTableBody";
+import FastLapTable from "../Podium_Results/FastLapTable";
+import "../../Styling/result.scss";
+import { dateToString } from "../../functions/helperFunc";
 
-function ResultTableHeader(props) {
-  const podiums = props.results;
-  const podium1 = podiums.result1;
+function ResultTableHeader({ results }) {
+  const podium1 = results.result1;
 
   return (
-    <div id='result-table-container'>
-      <TableContainer sx={{ minWidth: 600, maxWidth: 1000 }}component={Paper}>
+    <div id="result-table-container">
+      <TableContainer sx={{ minWidth: 600, maxWidth: 1000 }} component={Paper}>
         <Table aria-label="simple table">
           <TableHead className="result-table-head">
             <TableRow>
-              <TableCell 
-                colSpan={podium1.firstPlace.driver3 ? 4 : 3} 
-                align='center'
+              <TableCell
+                colSpan={podium1.firstPlace.driver3 ? 4 : 3}
+                align="center"
               >
-                {podiums.event}
+                {results.event}
               </TableCell>
-              <TableCell colSpan={1} align='right'>{podiums.series}</TableCell>
-              <TableCell colSpan={2} align='right'>{dateToString(podiums.date)}</TableCell>
+              <TableCell colSpan={1} align="right">
+                {results.series}
+              </TableCell>
+              <TableCell colSpan={2} align="right">
+                {dateToString(results.date)}
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableHead>
@@ -30,22 +40,25 @@ function ResultTableHeader(props) {
               <TableCell>Place</TableCell>
               <TableCell align="right">#</TableCell>
               <TableCell align="right">Driver 1</TableCell>
-              {podium1.firstPlace.driver2 && <TableCell align="right">Driver 2</TableCell>}
-              {podium1.firstPlace.driver3 && <TableCell align="right">Driver 3</TableCell>}
+              {podium1.firstPlace.driver2 && (
+                <TableCell align="right">Driver 2</TableCell>
+              )}
+              {podium1.firstPlace.driver3 && (
+                <TableCell align="right">Driver 3</TableCell>
+              )}
               <TableCell align="right">Team</TableCell>
               <TableCell align="right">Vehicle</TableCell>
             </TableRow>
           </TableHead>
-          <ResultTableBody results={podiums.result1}/>
-          {podiums.result2 && <ResultTableBody results={podiums.result2}/>}
-          {podiums.result3 && <ResultTableBody results={podiums.result3}/>}
-          {podiums.result4 && <ResultTableBody results={podiums.result4}/>}
-          <FastLapTable fastLap={podiums.fastLap}/>
+          <ResultTableBody results={results.result1} />
+          {results.result2 && <ResultTableBody results={results.result2} />}
+          {results.result3 && <ResultTableBody results={results.result3} />}
+          {results.result4 && <ResultTableBody results={results.result4} />}
+          <FastLapTable fastLap={results.fastLap} />
         </Table>
       </TableContainer>
     </div>
-  )
+  );
 }
 
 export default ResultTableHeader;
-
