08d60bd92bad510596ff54b031dcb7b091098626
Brendan Woo <woo.brendan@gmail.com>
2022-12-19 12:50:19 -0800

    wrap all in useEffect. move axios get into try block

--- podium-helper/src/functions/useEvents.js
+++ podium-helper/src/functions/useEvents.js
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useEvents = async () => {
+const useEvents = () => {
   const [events, setEvents] = useState([]);
 
+  useEffect(() => {
+    getEventList();
+  }, []);
+
   const getEventList = async () => {
-    const eventList = await axios.get("http://localhost:2020/api/events");
-    console.log("eventsList", eventList);
+    try {
+      const eventList = await axios.get("http://localhost:2020/api/events");
+      setEvents(eventList.data);
+    } catch (err) {
+      console.log("Error:", err);
+    }
   };
-  try {
-    await getEventList();
-  } catch (err) {
-    console.log("Error:", err);
-  }
 
   return { events };
 };
