9afb1d71aa4ae7d907dc0fc88a9590dda72081b7
Brendan Woo <woo.brendan@gmail.com>
2022-12-19 13:03:14 -0800

    move in logic for eventByDate, add to getEventList and add state for currentEventName

--- podium-helper/src/functions/useEvents.js
+++ podium-helper/src/functions/useEvents.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { getToday } from "./helperFunc";
 
 const useEvents = () => {
   const [events, setEvents] = useState([]);
+  const [currentEventName, setCurrentEventName] = useState("");
 
   useEffect(() => {
     getEventList();
@@ -12,12 +14,25 @@ const useEvents = () => {
     try {
       const eventList = await axios.get("http://localhost:2020/api/events");
       setEvents(eventList.data);
+      eventByDate(eventList.data);
     } catch (err) {
       console.log("Error:", err);
     }
   };
 
-  return { events };
+  const eventByDate = (events) => {
+    const month = Number(getToday().split("-")[0]);
+    if (month <= 2) setCurrentEventName(events[0].name);
+    else if (month <= 4 && month > 2) setCurrentEventName(events[1].name);
+    else if (month === 5) setCurrentEventName(events[2].name);
+    else if (month === 6) setCurrentEventName(events[3].name);
+    else if (month === 7) setCurrentEventName(events[4].name);
+    else if (month === 8) setCurrentEventName(events[6].name);
+    else if (month === 9) setCurrentEventName(events[7].name);
+    else return setCurrentEventName(events[8].name);
+  };
+
+  return { events, currentEventName };
 };
 
 export default useEvents;
