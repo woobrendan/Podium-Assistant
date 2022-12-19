8f47982b01ff4f7cf08e36ad67429a1aa2189465
Brendan Woo <woo.brendan@gmail.com>
2022-12-19 13:10:45 -0800

    change props to deconstruction for results. adjust constants as needed

--- podium-helper/src/components/Results/ResultTableBody.js
+++ podium-helper/src/components/Results/ResultTableBody.js
@@ -1,5 +1,5 @@
-import {TableBody, TableCell, TableRow, TableHead} from '@mui/material';
-import '../../Styling/result.scss';
+import { TableBody, TableCell, TableRow, TableHead } from "@mui/material";
+import "../../Styling/result.scss";
 
 function createDataTwoDriver(place, number, driver1, driver2, team, car) {
   return { place, number, driver1, driver2, team, car };
@@ -9,68 +9,153 @@ function createDataSingleDriver(place, number, driver1, team, car) {
   return { place, number, driver1, team, car };
 }
 
-const createDataThreeDriver = (place, number, driver1, driver2, driver3, team, car) => {
+const createDataThreeDriver = (
+  place,
+  number,
+  driver1,
+  driver2,
+  driver3,
+  team,
+  car,
+) => {
   return { place, number, driver1, driver2, driver3, team, car };
-}
+};
 
-export default function ResultTableBbody(props) {
-
-  const podium = props.results;
-  const first = podium.firstPlace;
-  const second = podium.secondPlace;
-  const third = podium.thirdPlace;
+export default function ResultTableBbody({ results }) {
+  const first = results.firstPlace;
+  const second = results.secondPlace;
+  const third = results.thirdPlace;
 
   const checkPodium = () => {
     const allResults = [];
-    if(!first.driver2) {
-
-      allResults.push(createDataSingleDriver('1st', first.number, first.driver1.name || first.driver1, first.team, first.vehicle))
+    if (!first.driver2) {
+      allResults.push(
+        createDataSingleDriver(
+          "1st",
+          first.number,
+          first.driver1.name || first.driver1,
+          first.team,
+          first.vehicle,
+        ),
+      );
 
       if (second) {
-        allResults.push(createDataSingleDriver('2nd', second.number, second.driver1.name || second.driver1, second.team, second.vehicle))
-      } 
-      if (third) {
-        allResults.push(createDataSingleDriver('3rd', third.number, third.driver1.name || third.driver1, third.team, third.vehicle));
+        allResults.push(
+          createDataSingleDriver(
+            "2nd",
+            second.number,
+            second.driver1.name || second.driver1,
+            second.team,
+            second.vehicle,
+          ),
+        );
       }
-      return allResults; 
-    } 
-      else if (first.driver2 && !first.driver3) {
-
-      allResults.push(createDataTwoDriver('1st', first.number, first.driver1.name || first.driver1, first.driver2.name || first.driver2, first.team, first.vehicle));
+      if (third) {
+        allResults.push(
+          createDataSingleDriver(
+            "3rd",
+            third.number,
+            third.driver1.name || third.driver1,
+            third.team,
+            third.vehicle,
+          ),
+        );
+      }
+      return allResults;
+    } else if (first.driver2 && !first.driver3) {
+      allResults.push(
+        createDataTwoDriver(
+          "1st",
+          first.number,
+          first.driver1.name || first.driver1,
+          first.driver2.name || first.driver2,
+          first.team,
+          first.vehicle,
+        ),
+      );
 
       if (second) {
-        allResults.push(createDataTwoDriver('2nd', second.number, second.driver1.name || second.driver1, second.driver2.name || second.driver2, second.team, second.vehicle));
-      } 
+        allResults.push(
+          createDataTwoDriver(
+            "2nd",
+            second.number,
+            second.driver1.name || second.driver1,
+            second.driver2.name || second.driver2,
+            second.team,
+            second.vehicle,
+          ),
+        );
+      }
       if (third) {
-        allResults.push(createDataTwoDriver('3rd', third.number, third.driver1.name || third.driver1, third.driver2.name || third.driver2, third.team, third.vehicle));
+        allResults.push(
+          createDataTwoDriver(
+            "3rd",
+            third.number,
+            third.driver1.name || third.driver1,
+            third.driver2.name || third.driver2,
+            third.team,
+            third.vehicle,
+          ),
+        );
+      }
+      return allResults;
+    } else {
+      allResults.push(
+        createDataThreeDriver(
+          "1st",
+          first.number,
+          first.driver1.name || first.driver1,
+          first.driver2.name || first.driver2,
+          first.driver3.name || first.driver3,
+          first.team,
+          first.vehicle,
+        ),
+      );
+
+      if (second) {
+        allResults.push(
+          createDataThreeDriver(
+            "2nd",
+            second.number,
+            second.driver1.name || second.driver1,
+            second.driver2.name || second.driver2,
+            second.driver3.name || second.driver3,
+            second.team,
+            second.vehicle,
+          ),
+        );
+      }
+      if (third) {
+        allResults.push(
+          createDataThreeDriver(
+            "3rd",
+            third.number,
+            third.driver1.name || third.driver1,
+            third.driver2.name || third.driver2,
+            third.driver3.name || third.driver3,
+            third.team,
+            third.vehicle,
+          ),
+        );
       }
       return allResults;
     }
-      else {
-        allResults.push(createDataThreeDriver('1st', first.number, first.driver1.name || first.driver1, first.driver2.name || first.driver2, first.driver3.name || first.driver3, first.team, first.vehicle));
-
-      if (second) {
-        allResults.push(createDataThreeDriver('2nd', second.number, second.driver1.name || second.driver1, second.driver2.name || second.driver2, second.driver3.name || second.driver3, second.team, second.vehicle));
-      } 
-      if (third) {
-        allResults.push(createDataThreeDriver('3rd', third.number, third.driver1.name || third.driver1, third.driver2.name || third.driver2, third.driver3.name || third.driver3, third.team, third.vehicle));
-      }
-      return allResults;
-    }
-  }
+  };
 
   return (
     <>
       <TableHead>
         <TableRow>
-          <TableCell colSpan={7} align='center'>{podium.class}</TableCell>
+          <TableCell colSpan={7} align="center">
+            {results.class}
+          </TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
         {checkPodium().map((row, index) => (
           <TableRow
             key={index}
-            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
           >
             <TableCell component="th" scope="row">
               {row.place}
@@ -85,6 +170,5 @@ export default function ResultTableBbody(props) {
         ))}
       </TableBody>
     </>
-  )
+  );
 }
-
