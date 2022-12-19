81507ff263ff261e7960027ec34bdbb6364e2c2c
Brendan Woo <woo.brendan@gmail.com>
2022-12-19 13:17:52 -0800

    change route for api/entries that was api/drivers

--- Mongo_DB/server.js
+++ Mongo_DB/server.js
@@ -1,33 +1,33 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const PORT = process.env.PORT || 2020;
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.json())
+app.use(express.static("public"));
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cors());
 
-mongoose.connect(process.env.COMPASS_URL, { useNewURLParser: true })
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+mongoose.connect(process.env.COMPASS_URL, { useNewURLParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 
-const apiEntriesRoute = require('./routes/api/entries');
-const apiResultsRoute = require('./routes/api/results');
-const apiEventsRoute = require('./routes/api/events');
-const apiSeriesRoute = require('./routes/api/series');
+const apiEntriesRoute = require("./routes/api/entries");
+const apiResultsRoute = require("./routes/api/results");
+const apiEventsRoute = require("./routes/api/events");
+const apiSeriesRoute = require("./routes/api/series");
 
-app.use('/api/drivers', apiEntriesRoute);
-app.use('/api/results', apiResultsRoute);
-app.use('/api/events', apiEventsRoute);
-app.use('/api/series', apiSeriesRoute);
+app.use("/api/entries", apiEntriesRoute);
+app.use("/api/results", apiResultsRoute);
+app.use("/api/events", apiEventsRoute);
+app.use("/api/series", apiSeriesRoute);
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
