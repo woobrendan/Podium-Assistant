44d9783e957064493900c6a1922ecd1c1189999c
Brendan Woo <woo.brendan@gmail.com>
2022-12-20 14:16:47 -0800

    change vehicles from useEntries to be entries from store entries, useSelector

--- podium-helper/src/components/Entry_Manage/EntryDashboard.js
+++ podium-helper/src/components/Entry_Manage/EntryDashboard.js
@@ -1,57 +1,74 @@
-import EntryRows from './EntryRows';
-import ToggleSort from '../Competitor/toggleSort';
-import searchAllEntries from '../Competitor/searchAllEntries';
-import '../../Styling/entryDash.scss'
-import {useState} from 'react';
-import { TextField } from '@mui/material'
-import useEntries from '../../functions/useEntries';
-import {sortBySeries, sortByVehicleType, sortByManufacturer, sortByClass} from '../../functions/sortFuncs';
-import NoResults from '../NoResults';
+import EntryRows from "./EntryRows";
+import ToggleSort from "../Competitor/toggleSort";
+import searchAllEntries from "../Competitor/searchAllEntries";
+import "../../Styling/entryDash.scss";
+import { useState, useEffect } from "react";
+import { TextField } from "@mui/material";
+import {
+  sortBySeries,
+  sortByVehicleType,
+  sortByManufacturer,
+  sortByClass,
+} from "../../functions/sortFuncs";
+import NoResults from "../NoResults";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchEntry } from "../../store/entryActions";
 
 function EntryDashboard() {
+  const [searchValue, setSearchValue] = useState("");
+  const [option, setOption] = useState("");
+  const dispatch = useDispatch();
 
-  const [searchValue, setSearchValue] = useState('');
-  const [option, setOption] = useState('');
+  useEffect(() => {
+    dispatch(fetchEntry());
+  }, [dispatch]);
 
-  const {vehicles} = useEntries();
-  
+  const entries = useSelector((state) => state.entry.entriesArray);
+
   const getSortOption = (option) => setOption(option);
-  
-  const searchResults = searchAllEntries(vehicles, searchValue);
+
+  const searchResults = searchAllEntries(entries, searchValue);
+
   const setSortOption = (sortOption, entryArray) => {
-    switch(sortOption) {
-      case 'Number': return entryArray
-      case 'Manufacturer': return sortByManufacturer(entryArray)
-      case 'Vehicle Type': return sortByVehicleType(entryArray)
-      case 'Class': return sortByClass(entryArray)
-      default: return sortBySeries(entryArray)
+    switch (sortOption) {
+      case "Number":
+        return entryArray;
+      case "Manufacturer":
+        return sortByManufacturer(entryArray);
+      case "Vehicle Type":
+        return sortByVehicleType(entryArray);
+      case "Class":
+        return sortByClass(entryArray);
+      default:
+        return sortBySeries(entryArray);
     }
-  }
+  };
 
-  const sortedEntries = searchResults.length > 0 
-    ? <EntryRows filteredEntries={setSortOption(option, searchResults)}/>
-    : <NoResults />
-
-  //take in vehicles from useEntries
-  //filter those by search
-  //pass those remaining entries down to entryrows
+  const sortedEntries =
+    searchResults.length > 0 ? (
+      <EntryRows filteredEntries={setSortOption(option, searchResults)} />
+    ) : (
+      <NoResults />
+    );
 
   return (
     <div className="entry-dashboard">
       <div className="search-sort-options">
-          <ToggleSort getOption={getSortOption} />
-          <TextField 
-            className="form-control"
-            label="Search" 
-            variant="outlined" 
-            color="error"
-            value={searchValue}
-            onChange={e => {setSearchValue(e.target.value)}}
-          />
-        </div>
+        <ToggleSort getOption={getSortOption} />
+        <TextField
+          className="form-control"
+          label="Search"
+          variant="outlined"
+          color="error"
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+          }}
+        />
+      </div>
       {sortedEntries}
     </div>
-  )
+  );
 }
 
-export default EntryDashboard
+export default EntryDashboard;
