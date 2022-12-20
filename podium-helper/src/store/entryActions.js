3e5d80e3758b0c8a085a74b42e78ee6b5e70858d
Brendan Woo <woo.brendan@gmail.com>
2022-12-20 13:58:48 -0800

    add await to variable entries, to wait for promise. change try block variables from results to entries

--- podium-helper/src/store/entryActions.js
+++ podium-helper/src/store/entryActions.js
@@ -4,13 +4,13 @@ import { entryActions } from "./entry_slice";
 export const fetchEntry = () => {
   return async (dispatch) => {
     const fetchHandler = async () => {
-      const entries = axios.get("http://localhost:2020/api/entries");
+      const entries = await axios.get("http://localhost:2020/api/entries");
       return entries.data;
     };
 
     try {
-      const results = await fetchHandler();
-      dispatch(entryActions.setEntries(results));
+      const entries = await fetchHandler();
+      dispatch(entryActions.setEntries(entries));
     } catch (err) {
       console.error(err);
     }
