1eda9ffa00d2fe57973fefbb5d14e8495efc6f6f
Brendan Woo <woo.brendan@gmail.com>
2022-12-21 13:26:01 -0800

    add useEffect and create method getandSetSeries to get the series data from backend and set to new series state vvariable created

--- podium-helper/src/components/Podium_Results/Series.js
+++ podium-helper/src/components/Podium_Results/Series.js
@@ -1,32 +1,51 @@
-import {useState} from 'react';
-import { FormControl, InputLabel, Select, Box, MenuItem } from '@mui/material';
-import useEntries from '../../functions/useEntries';
-
+import { useState, useEffect } from "react";
+import { FormControl, InputLabel, Select, Box, MenuItem } from "@mui/material";
+import useEntries from "../../functions/useEntries";
+import axios from "axios";
 
 function Series(props) {
-  const [seriesName, setSeriesName] = useState('');
-  const { series } = useEntries();
+  const [seriesName, setSeriesName] = useState("");
+  // const { series } = useEntries();
+  const [series, setSeries] = useState([]);
+
+  useEffect(() => {
+    const getAndSetSeries = async () => {
+      const getData = async () => {
+        const res = await axios.get("http://localhost:2020/api/series");
+        return res.data;
+      };
+      try {
+        const seriesList = await getData();
+        setSeries(seriesList);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getAndSetSeries();
+  }, []);
 
   const handleChange = (event) => {
     setSeriesName(event.target.value);
-    props.getValue(event.target.name, event.target.value)
+    props.getValue(event.target.name, event.target.value);
   };
 
-  const mappedSeries = series.map((serie, index) => <MenuItem key={index} value={serie}>{serie.name}</MenuItem>)
+  const mappedSeries = series.map((serie, index) => (
+    <MenuItem key={index} value={serie}>
+      {serie.name}
+    </MenuItem>
+  ));
 
   return (
     <Box
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
       }}
       noValidate
       autoComplete="off"
     >
       <FormControl sx={{ minWidth: 300 }}>
-        <InputLabel htmlFor="Series">
-          Series
-        </InputLabel>
+        <InputLabel htmlFor="Series">Series</InputLabel>
         <Select
           className="form-control"
           name="series"
@@ -38,7 +57,7 @@ function Series(props) {
         </Select>
       </FormControl>
     </Box>
-  )
+  );
 }
 
-export default Series
+export default Series;
