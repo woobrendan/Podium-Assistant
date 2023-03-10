22f87a0c3714dc06c9a316a943630de2bc090348
Brendan Woo <woo.brendan@gmail.com>
2023-03-10 12:47:32 -0800

    import numOfDriverMenuItems from podiumResultHlper file and remove same function from within file

--- podium-helper/src/components/Podium_Results/WinnerPodium.js
+++ podium-helper/src/components/Podium_Results/WinnerPodium.js
@@ -4,6 +4,7 @@ import "../../Styling/winnerTop3.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchEntry } from "../../store/entryActions";
 import PlacementInput from "./PlacementInput";
+import { numOfDriverMenuItem } from "../../functions/podiumResultHelpers";
 
 const WinnerPodium = ({
   seriesName,
@@ -43,30 +44,6 @@ const WinnerPodium = ({
       if (isError) setIsError(false);
       onClick(winners, resultNum);
       setIsSubmitted(true);
-    }
-  };
-
-  //determine if entry is single or two drivers and return corresponding menu item
-  const numOfDriverMenuItem = (entry) => {
-    if (!entry.driver2) {
-      return (
-        <MenuItem key={`${entry.number} ${entry.driver1.name}`} value={entry}>
-          #{entry.number} - {entry.driver1.name}
-        </MenuItem>
-      );
-    } else if (entry.driver2 && !entry.driver3) {
-      return (
-        <MenuItem key={`${entry.number} ${entry.driver1.name}`} value={entry}>
-          #{entry.number} - {entry.driver1.name} & {entry.driver2.name}
-        </MenuItem>
-      );
-    } else {
-      return (
-        <MenuItem key={`${entry.number} ${entry.driver1.name}`} value={entry}>
-          #{entry.number} - {entry.driver1.name} & {entry.driver2.name} &{" "}
-          {entry.driver3.name}
-        </MenuItem>
-      );
     }
   };
 
