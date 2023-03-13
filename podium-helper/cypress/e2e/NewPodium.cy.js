4b601562dcc80cc11eaafa6bc211d71b49d138b7
Brendan Woo <woo.brendan@gmail.com>
2023-03-13 14:42:09 -0700

    change series test to grab data-testid

--- podium-helper/cypress/e2e/NewPodium.cy.js
+++ podium-helper/cypress/e2e/NewPodium.cy.js
@@ -1,48 +1,42 @@
 /// <reference types="cypress" />
-import {getToday} from '../../src/helperFunc';
+import { getToday } from "../../src/functions/helperFunc";
 
-describe('Creating New Podiums', () => {
+describe("Creating New Podiums", () => {
   const formatDate = () => {
     const day = [];
-    const splitDay = getToday().split('-');
-    day.push(splitDay[2], splitDay[0], splitDay[1])
-    return day.join('-');
-  }
+    const splitDay = getToday().split("-");
+    day.push(splitDay[2], splitDay[0], splitDay[1]);
+    return day.join("-");
+  };
 
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
-  })
-
-  it('Should select event', () => {
-    cy.get('#mui-component-select-event')
-    .parent()
-    .click()
-    .get('ul > li[data-value="Sonoma Raceway"]')
-    .click()
+    cy.visit("http://localhost:3000/");
   });
 
-  it('Should select Series', () => {
-    cy.get('#mui-component-select-series')
-    .parent()
-    .click()
-    .get('ul > li[data-value="GT World Challenge America"]')
-    .click()
+  it("Should select event", () => {
+    cy.get("#mui-component-select-event")
+      .parent()
+      .click()
+      .get('ul > li[data-value="Sonoma Raceway"]')
+      .click();
   });
 
-  it('Should select Class', () => {
-    cy.get('#mui-component-select-series')
+  it("Should select Series", () => {
+    cy.get('[data-testid="series_dropdown"]').click();
+    cy.get('[data-testid="GT World Challenge America"]').click();
+  });
+
+  it("Should select Class", () => {
+    cy.get("#mui-component-select-series")
       .parent()
       .click()
       .get('ul > li[data-value="GT World Challenge America"]')
-      .click()
+      .click();
 
-    cy.get('.class-container')
+    cy.get(".class-container")
       .first()
       .click()
       .get('ul > li[data-value="Pro"]')
-      .click()
+      .click();
   });
-
-
-
-})
+});
