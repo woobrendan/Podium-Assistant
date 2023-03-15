1a4f25d9db46f52b213771dfe57201fe21f855e9
Brendan Woo <woo.brendan@gmail.com>
2023-03-15 13:39:02 -0700

    create route for entryManager and entryTable component

--- podium-helper/src/App.js
+++ podium-helper/src/App.js
@@ -1,11 +1,12 @@
-import './Styling/App.scss';
-import DriverSearch from './components/Competitor/DriverSearch';
+import "./Styling/App.scss";
+import DriverSearch from "./components/Competitor/DriverSearch";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
-import Podium from './components/Podium_Results/Podium';
-import NavBar from './components/NavBar';
-import Results from './components/Results/ResultsHistory';
-import EntryDashboard from './components/Entry_Manage/EntryDashboard';
-import RecentPodium from './components/Results/RecentPodium';
+import Podium from "./components/Podium_Results/Podium";
+import NavBar from "./components/NavBar";
+import Results from "./components/Results/ResultsHistory";
+import EntryDashboard from "./components/Entry_Manage/EntryDashboard";
+import RecentPodium from "./components/Results/RecentPodium";
+import EntryTable from "./components/EntryManager/EntryTable";
 
 function App() {
   return (
@@ -13,11 +14,12 @@ function App() {
       <NavBar />
       <div className="App">
         <Routes>
-          <Route path='/' element={<Podium />} />
+          <Route path="/" element={<Podium />} />
           <Route path="/competitors" element={<DriverSearch />} />
           <Route path="/results" element={<Results />} />
           <Route path="/entry-dashboard" element={<EntryDashboard />} />
           <Route path="/recent" element={<RecentPodium />} />
+          <Route path="/entryManager" element={<EntryTable />} />
         </Routes>
       </div>
     </Router>
