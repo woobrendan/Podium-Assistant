02d709515bfbc29d4b094167dd8c122fd2e1bf24
Brendan Woo <woo.brendan@gmail.com>
2023-03-16 11:49:57 -0700

    remove sortEntries, and change component to be fragment wrapper, remove tablebody and map through. change entries props to be entry

--- podium-helper/src/components/EntryManager/EntryBodyRows.js
+++ podium-helper/src/components/EntryManager/EntryBodyRows.js
@@ -1,60 +1,40 @@
-import { TableBody, TableRow, TableCell, Button } from "@mui/material";
+import { TableRow, TableCell, Button } from "@mui/material";
 import { useState } from "react";
 import { shortenName } from "../../functions/helperFunc";
+import EditModal from "./EditModal";
 
-const EntryBodyRows = ({ entries }) => {
+const EntryBodyRows = ({ entry }) => {
   const [showModal, setShowModal] = useState(false);
 
-  const sortEntries = (entries) => {
-    const entryObj = {};
-    const entryArr = [];
-    entries.forEach((entry) => {
-      const series = entry.series;
-      entryObj[series]
-        ? entryObj[series].push(entry)
-        : (entryObj[series] = [entry]);
-    });
-
-    for (const series in entryObj) {
-      const sorted = entryObj[series].sort((a, b) => a.number - b.number);
-      if (series !== "GT World Challenge America") entryArr.push(...sorted);
-    }
-    return entryObj["GT World Challenge America"]
-      ? [...entryObj["GT World Challenge America"], ...entryArr]
-      : entryArr;
-  };
-
   return (
-    <TableBody>
-      {sortEntries(entries).map((entry, index) => (
-        <TableRow
-          key={index}
-          sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-        >
-          <TableCell component="th" scope="row">
-            {shortenName(entry.series)}
-          </TableCell>
-          <TableCell align="right">{entry.number}</TableCell>
-          <TableCell align="right">{entry.team}</TableCell>
-          <TableCell align="right">{entry.driver1.name}</TableCell>
-          <TableCell align="right">
-            {entry.driver2 ? entry.driver2.name : ""}
-          </TableCell>
-          <TableCell align="right">
-            {entry.driver3 ? entry.driver3.name : ""}
-          </TableCell>
-          <TableCell align="right">{entry.vehicle}</TableCell>
-          <TableCell align="right">
-            <Button
-              variant="contained"
-              onClick={() => setShowModal(!showModal)}
-            >
-              Edit
-            </Button>
-          </TableCell>
-        </TableRow>
-      ))}
-    </TableBody>
+    <>
+      <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+        <TableCell component="th" scope="row">
+          {shortenName(entry.series)}
+        </TableCell>
+        <TableCell align="right">{entry.number}</TableCell>
+        <TableCell align="right">{entry.team}</TableCell>
+        <TableCell align="right">{entry.driver1.name}</TableCell>
+        <TableCell align="right">
+          {entry.driver2 ? entry.driver2.name : ""}
+        </TableCell>
+        <TableCell align="right">
+          {entry.driver3 ? entry.driver3.name : ""}
+        </TableCell>
+        <TableCell align="right">{entry.vehicle}</TableCell>
+        <TableCell align="right">
+          <Button variant="contained" onClick={() => setShowModal(!showModal)}>
+            Edit
+          </Button>
+        </TableCell>
+      </TableRow>
+      {showModal && (
+        <EditModal
+          handleToggle={() => setShowModal(!showModal)}
+          show={showModal}
+        />
+      )}
+    </>
   );
 };
 
