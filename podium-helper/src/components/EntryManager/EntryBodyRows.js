9ca57ca1594790e3270894417ee321c155f1c73e
Brendan Woo <woo.brendan@gmail.com>
2023-03-16 14:32:23 -0700

    move mappedEntry inside of component. create function to shorten series name and return that value

--- podium-helper/src/components/EntryManager/EntryBodyRows.js
+++ podium-helper/src/components/EntryManager/EntryBodyRows.js
@@ -1,28 +1,50 @@
 import { TableBody, TableRow, TableCell } from "@mui/material";
 
 const EntryBodyRows = ({ entries }) => {
-  const entryRows = entries.map((entry, index) => (
-    <TableRow
-      key={index}
-      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-    >
-      <TableCell component="th" scope="row">
-        {entry.series}
-      </TableCell>
-      <TableCell align="right">{entry.number}</TableCell>
-      <TableCell align="right">{entry.team}</TableCell>
-      <TableCell align="right">{entry.driver1.name}</TableCell>
-      <TableCell align="right">
-        {entry.driver2 ? entry.driver2.name : ""}
-      </TableCell>
-      <TableCell align="right">
-        {entry.driver3 ? entry.driver3.name : ""}
-      </TableCell>
-      <TableCell align="right">{entry.vehicle}</TableCell>
-      <TableCell align="right">Edit</TableCell>
-    </TableRow>
-  ));
-  return <TableBody>{entryRows}</TableBody>;
+  const shortenName = (series) => {
+    switch (series) {
+      case "GT World Challenge America":
+        return "GTWCA";
+      case "GT America":
+        return "GTA";
+      case "Pirelli GT4 America":
+        return "PGT4A";
+      case "TC America":
+        return "TCAM";
+      case "Intercontinental GT Challenge":
+        return "IGTC";
+      case "Toyota GR Cup":
+        return "GR Cup";
+      default:
+        return "SRO";
+    }
+  };
+
+  return (
+    <TableBody>
+      {entries.map((entry, index) => (
+        <TableRow
+          key={index}
+          sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+        >
+          <TableCell component="th" scope="row">
+            {shortenName(entry.series)}
+          </TableCell>
+          <TableCell align="right">{entry.number}</TableCell>
+          <TableCell align="right">{entry.team}</TableCell>
+          <TableCell align="right">{entry.driver1.name}</TableCell>
+          <TableCell align="right">
+            {entry.driver2 ? entry.driver2.name : ""}
+          </TableCell>
+          <TableCell align="right">
+            {entry.driver3 ? entry.driver3.name : ""}
+          </TableCell>
+          <TableCell align="right">{entry.vehicle}</TableCell>
+          <TableCell align="right">Edit</TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  );
 };
 
 export default EntryBodyRows;
