c6302ba5217179e8682a02e3264c82bec2ddbdd9
Brendan Woo <woo.brendan@gmail.com>
2023-03-20 12:33:49 -0700

    create schema for entry

---
+++ express_mongo/models/entry_schema.ts
@@ -0,0 +1,21 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { EntryInterface } from "./models";
+
+export interface EntryModel extends EntryInterface, Document {}
+
+const entrySchema: Schema = new Schema({
+  team: { type: String, required: true },
+  vehicle: { type: String, required: true },
+  classification: { type: String, required: true },
+  number: { type: String, required: true },
+  carImage: { type: String, required: false },
+  series: { type: String, required: true },
+  year: { type: Number, required: true },
+  driver1: {
+    name: { type: String, required: true },
+    rating: { type: String, required: false },
+    nationality: { type: String, required: true },
+  },
+});
+
+export default mongoose.model<EntryModel>("Entries", entrySchema);
