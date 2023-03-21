e45a135f6a962b979af1f01e3c5d3f9117565f70
Brendan Woo <woo.brendan@gmail.com>
2023-03-21 13:20:21 -0700

    create controller folder, create entry controller file

---
+++ express_mongo/controllers/entry_controller.ts
@@ -0,0 +1,4 @@
+import { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
+import Entries from "../models/entry_schema";
+import { EntryInterface } from "../models/models";
