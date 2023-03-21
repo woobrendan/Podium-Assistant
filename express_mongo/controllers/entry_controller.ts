6d33b00081cb33a0424bb5859c596a48a7b93153
Brendan Woo <woo.brendan@gmail.com>
2023-03-21 13:30:35 -0700

    create controller for creating new entries

--- express_mongo/controllers/entry_controller.ts
+++ express_mongo/controllers/entry_controller.ts
@@ -1,4 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import Entries from "../models/entry_schema";
-import { EntryInterface } from "../models/models";
+
+const createEntry = (req: Request, res: Response) => {
+  const entry = new Entries({
+    _id: new mongoose.Types.ObjectId(),
+    ...req.body,
+    driver1: {
+      ...req.body.driver1,
+    },
+    ...(req.body.driver2 ? { driver2: { ...req.body.driver2 } } : {}),
+    ...(req.body.driver3 ? { driver3: { ...req.body.driver3 } } : {}),
+  });
+
+  return entry
+    .save()
+    .then((entry) => res.status(201).json({ entry }))
+    .catch((error) => res.status(500).json({ error }));
+};
+
+export default {
+  createEntry,
+};
