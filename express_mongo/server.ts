938d6941f9f554d30aa30ee35799d12af23fc448
Brendan Woo <woo.brendan@gmail.com>
2023-03-21 10:47:50 -0700

    add in logic for server, import routes, error handling and mongoose connection

---
+++ express_mongo/server.ts
@@ -0,0 +1,69 @@
+import express from "express";
+import mongoose from "mongoose";
+import http from "http";
+import { config } from "./config/config";
+import entryRoute from "./routes/entriesRoutes";
+
+const morgan = require("morgan");
+const router = express();
+
+router.use(morgan("dev"));
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(config.mongo.url, { retryWrites: true, w: "majority" })
+  .then(() => {
+    console.log("Connected");
+    startServer();
+  })
+  .catch((error) => {
+    console.log("Error");
+  });
+
+//** Only Start Server if Mongo Connects */
+
+const startServer = () => {
+  router.use((req, res, next) => {
+    res.status(res.statusCode);
+    next();
+  });
+
+  router.use(express.urlencoded({ extended: true }));
+  router.use(express.json());
+
+  //** Rules of API */
+  router.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    );
+
+    if (req.method == "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, PATCH, DELETE, GET",
+      );
+      return res.status(200).json({});
+    }
+
+    next();
+  });
+
+  //** Routes */
+  router.use("/entries", entryRoute);
+
+  //** Error handling */
+  router.use((req, res, next) => {
+    const error = new Error("not found");
+    console.log("Error: ", error);
+    return res.status(404).json({ message: error.message });
+  });
+
+  http
+    .createServer(router)
+    .listen(config.server.port, () =>
+      console.log(`Server is running on port ${config.server.port}`),
+    );
+};
