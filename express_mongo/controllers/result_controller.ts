ae46f81f6475da6eb9722686933f1c3df4253eb9
Brendan Woo <woo.brendan@gmail.com>
2023-03-21 11:49:40 -0700

    create controller for get by id and delete by id, add to file export

--- express_mongo/controllers/result_controller.ts
+++ express_mongo/controllers/result_controller.ts
@@ -8,6 +8,31 @@ const getAllResults = async (req: Request, res: Response) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
+const getResultById = async (req: Request, res: Response) => {
+  const resultId = req.params.resultId;
+  try {
+    const result = await Result.findById(resultId);
+    return result
+      ? res.status(200).json({ result })
+      : res.status(400).json({ message: "Result Not Found" });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+const deleteResult = (req: Request, res: Response) => {
+  const resultId = req.params.resultId;
+  return Result.findByIdAndDelete(resultId)
+    .then((result) =>
+      result
+        ? res.status(201).json({ message: "Deleted" })
+        : res.status(404).json({ message: "Not Found" }),
+    )
+    .catch((error) => res.status(500).json({ error }));
+};
+
 export default {
   getAllResults,
+  getResultById,
+  deleteResult,
 };
