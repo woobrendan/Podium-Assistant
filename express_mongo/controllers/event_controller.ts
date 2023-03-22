32c1b502812eb3c2f6de85570f2635d8a3dd8b02
Brendan Woo <woo.brendan@gmail.com>
2023-03-22 10:00:53 -0700

    add controllers for get by id and update

--- express_mongo/controllers/event_controller.ts
+++ express_mongo/controllers/event_controller.ts
@@ -8,6 +8,37 @@ const getAllEvents = async (req: Request, res: Response) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
+const getEventById = async (req: Request, res: Response) => {
+  const eventId = req.params.EventId;
+  try {
+    const event = await Event.findById(eventId);
+    return Event
+      ? res.status(200).json({ event })
+      : res.status(400).json({ message: "Event Not Found" });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+const updateEvent = async (req: Request, res: Response) => {
+  const eventId = req.params.eventId;
+  return Event.findById(eventId)
+    .then((event) => {
+      if (event) {
+        event.set(req.body);
+        return event
+          .save()
+          .then((event) => res.status(201).json({ event }))
+          .catch((error) => res.status(500).json({ error }));
+      } else {
+        res.status(404).json({ message: "event not found" });
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
 export default {
   getAllEvents,
+  getEventById,
+  updateEvent,
 };
