8f369f03e298ee36eb0dcf7f4f4bb047888223a7
Brendan Woo <woo.brendan@gmail.com>
2023-03-23 12:06:15 -0700

    update url for entries to not have api in url

--- podium-helper/src/store/entryActions.js
+++ podium-helper/src/store/entryActions.js
@@ -4,7 +4,7 @@ import { entryActions } from "./entry_slice";
 export const fetchEntry = () => {
   return async (dispatch) => {
     try {
-      const entries = await axios.get("http://localhost:2020/api/entries");
+      const entries = await axios.get("http://localhost:2020/entries");
       dispatch(entryActions.setEntries(entries.data));
     } catch (err) {
       console.error(err);
