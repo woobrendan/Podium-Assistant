5971450bdb13fcfb8a1282ad9ecdf3a7aa8efde8
Brendan Woo <woo.brendan@gmail.com>
2023-03-23 12:07:24 -0700

    update route urls to be / remove words like get, delete, create or update

--- express_mongo/routes/resultRoutes.ts
+++ express_mongo/routes/resultRoutes.ts
@@ -3,10 +3,10 @@ import controller from "../controllers/result_controller";
 
 const router = express.Router();
 
-router.post("/new", controller.createResult);
-router.get("/get/:resultId", controller.getResultById);
-router.get("/get/", controller.getAllResults);
-router.patch("/update/:resultId", controller.updateResult);
-router.delete("/delete/:resultId", controller.deleteResult);
+router.post("/", controller.createResult);
+router.get("/", controller.getAllResults);
+router.get("/:resultId", controller.getResultById);
+router.patch("/:resultId", controller.updateResult);
+router.delete("/:resultId", controller.deleteResult);
 
 export = router;
