a20ded3ceaf3ecc63ac7b16016e3f05bc37e3440
Brendan Woo <woo.brendan@gmail.com>
2023-03-27 13:57:27 -0700

    comment out useselector for enties and sortentries. add in props for enties and replace the entries being mapped

--- podium-helper/src/components/EntryManager/EntryManager.js
+++ podium-helper/src/components/EntryManager/EntryManager.js
@@ -1,11 +1,38 @@
 import EntryTable from "./EntryTable";
+import { useDispatch, useSelector } from "react-redux";
 
 //take in entries from state, run a filter to change return of entries and pass down to table
-
 const EntryManager = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchEntry());
+  }, [dispatch]);
+
+  const entries = useSelector((state) => state.entry.entriesArray);
+
+  const sortEntries = (entries) => {
+    const entryObj = {};
+    const entryArr = [];
+    entries.forEach((entry) => {
+      const series = entry.series;
+      entryObj[series]
+        ? entryObj[series].push(entry)
+        : (entryObj[series] = [entry]);
+    });
+
+    for (const series in entryObj) {
+      const sorted = entryObj[series].sort((a, b) => a.number - b.number);
+      if (series !== "GT World Challenge America") entryArr.push(...sorted);
+    }
+    return entryObj["GT World Challenge America"]
+      ? [...entryObj["GT World Challenge America"], ...entryArr]
+      : entryArr;
+  };
+
   return (
     <>
-      <EntryTable />
+      <EntryTable entries={sortEntries(entries)} />
     </>
   );
 };
