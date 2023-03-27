8ccbc10ac188c81e95b729fc41fbad1ba93df00c
Brendan Woo <woo.brendan@gmail.com>
2023-03-27 13:49:03 -0700

    create folder to house entrytable component

---
+++ podium-helper/src/components/EntryManager/EntryManager.js
@@ -0,0 +1,13 @@
+import EntryTable from "./EntryTable";
+
+//take in entries from state, run a filter to change return of entries and pass down to table
+
+const EntryManager = () => {
+  return (
+    <>
+      <EntryTable />
+    </>
+  );
+};
+
+export default EntryManager;
