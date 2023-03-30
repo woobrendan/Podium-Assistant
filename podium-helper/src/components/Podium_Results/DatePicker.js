5b442f60230ef9e4ac1ae029b02beb6eaf632832
Brendan Woo <woo.brendan@gmail.com>
2023-03-30 11:32:00 -0700

    change component to be arrrow function and change props to be deconstructed

--- podium-helper/src/components/Podium_Results/DatePicker.js
+++ podium-helper/src/components/Podium_Results/DatePicker.js
@@ -1,18 +1,16 @@
-import * as React from 'react';
-import {Stack, TextField} from '@mui/material'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import * as React from "react";
+import { Stack, TextField } from "@mui/material";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
-
-export default function DatePicker(props) {
-
+const DatePicker = ({ getValue }) => {
   const [date, setDate] = React.useState(props.today);
 
   const handleChange = (newValue) => {
-    const newDate = newValue.toISOString().split('T')[0]
-    setDate(newDate)
-    props.getValue('date', newDate)
+    const newDate = newValue.toISOString().split("T")[0];
+    setDate(newDate);
+    getValue("date", newDate);
   };
 
   return (
@@ -22,7 +20,7 @@ export default function DatePicker(props) {
           <DesktopDatePicker
             label="Date"
             inputFormat="yyyy-MM-dd"
-            name='date'
+            name="date"
             value={date}
             onChange={handleChange}
             renderInput={(params) => <TextField {...params} />}
@@ -31,4 +29,6 @@ export default function DatePicker(props) {
       </LocalizationProvider>
     </div>
   );
-}
+};
+
+export default DatePicker;
