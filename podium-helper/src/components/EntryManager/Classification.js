7f3a32567ca298e54fdf054d45a14605333c752a
Brendan Woo <woo.brendan@gmail.com>
2023-03-29 22:58:29 -0700

    add getclasses to useEffect. create func getSeriesClasses and complete logic to get classs names reeturn the unique ones

--- podium-helper/src/components/EntryManager/Classification.js
+++ podium-helper/src/components/EntryManager/Classification.js
@@ -3,8 +3,24 @@ import { useEffect } from "react";
 
 const Classification = () => {
   useEffect(() => {
-    const getClasses = async () => {};
+    const getClasses = async () => {
+      const series = await axios.get("http://localhost:2020/api/series");
+    };
   }, []);
+
+  const getSeriesClasses = (series) => {
+    //get GTWCA and GTA to the top, and pgt4 and tc to the bottom
+    const seriesList = series.sort((a, b) => a.name - b.name);
+
+    //loop through each series and push all classes togeher, then filter out duplicates
+    const classList = [];
+    for (const series of seriesList) {
+      classList.push(...series.class);
+    }
+    return classList.filter(
+      (className, index) => classList.indexOf(className) === index,
+    );
+  };
 
   return <></>;
 };
