e84f9b9f57a0d34c55c27a0040b897ee83f53f4d
Brendan Woo <woo.brendan@gmail.com>
2023-03-29 22:43:15 -0700

    create component for classifications select instead of usisng input textfields

---
+++ podium-helper/src/components/EntryManager/Classification.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+const Classification = () => {
+  useEffect(() => {
+    const getClasses = async () => {};
+  }, []);
+
+  return <></>;
+};
+
+export default Classficiation;
