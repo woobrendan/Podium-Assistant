1cc3a9ccce745e6bc8b5c100f0ecff3e0687f5bb
Brendan Woo <woo.brendan@gmail.com>
2023-03-31 17:49:02 -0700

    deconstruct classification and pass to classification component

--- podium-helper/src/components/EntryManager/EditVehicle.js
+++ podium-helper/src/components/EntryManager/EditVehicle.js
@@ -1,10 +1,13 @@
 import Classification from "./Classification";
 import InputContainer from "./InputContainer";
 
-const EditVehicle = ({ entry, onInputChange }) => {
+const EditVehicle = ({ entry, onInputChange, classification }) => {
   return (
     <section className="input_vehicle_container">
-      <Classification onInputChange={onInputChange} />
+      <Classification
+        onInputChange={onInputChange}
+        classification={classification}
+      />
       <InputContainer
         val={entry.number}
         name="number"
