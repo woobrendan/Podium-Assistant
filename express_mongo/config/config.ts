970a1f90616ddaa1b7837dd15dc0581050b28c5e
Brendan Woo <woo.brendan@gmail.com>
2023-04-03 14:05:46 -0700

    change test url and replcae connection string with test string

--- express_mongo/config/config.ts
+++ express_mongo/config/config.ts
@@ -6,7 +6,7 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.rmiuql1.mongodb.net/sro23_db`;
 
-const MONGO_TEST = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.rmiuql1.mongodb.net/fuel_test`;
+const MONGO_TEST = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.rmiuql1.mongodb.net/sro23_Test`;
 
 const SERVER_PORT = process.env.SERVER_PORT
   ? Number(process.env.SERVER_PORT)
@@ -14,7 +14,7 @@ const SERVER_PORT = process.env.SERVER_PORT
 
 export const config = {
   mongo: {
-    url: MONGO_URL,
+    url: MONGO_TEST,
   },
   server: {
     port: SERVER_PORT,
