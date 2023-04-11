aa9e2579400d4d8922fce2c5d17fd4e1b33add36
Brendan Woo <woo.brendan@gmail.com>
2023-04-11 14:02:27 -0700

    remove wrapper div and place id inside tablecontainer

--- podium-helper/src/components/Results/ResultTableHeader.js
+++ podium-helper/src/components/Results/ResultTableHeader.js
@@ -12,53 +12,55 @@ import FastLapTable from "../Podium_Results/FastLapTable";
 import "../../Styling/result.scss";
 import { dateToString } from "../../functions/helperFunc";
 
-function ResultTableHeader({ results }) {
+const ResultTableHeader = ({ results }) => {
   const podium1 = results.result1;
 
   return (
-    <div id="result-table-container">
-      <TableContainer sx={{ minWidth: 600, maxWidth: 1000 }} component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead className="result-table-head">
-            <TableRow>
-              <TableCell
-                colSpan={podium1.firstPlace.driver3 ? 4 : 3}
-                align="center"
-              >
-                {results.event}
-              </TableCell>
-              <TableCell colSpan={1} align="right">
-                {results.series}
-              </TableCell>
-              <TableCell colSpan={2} align="right">
-                {dateToString(results.date)}
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableHead>
-            <TableRow>
-              <TableCell>Place</TableCell>
-              <TableCell align="right">#</TableCell>
-              <TableCell align="right">Driver 1</TableCell>
-              {podium1.firstPlace.driver2 && (
-                <TableCell align="right">Driver 2</TableCell>
-              )}
-              {podium1.firstPlace.driver3 && (
-                <TableCell align="right">Driver 3</TableCell>
-              )}
-              <TableCell align="right">Team</TableCell>
-              <TableCell align="right">Vehicle</TableCell>
-            </TableRow>
-          </TableHead>
-          <ResultTableBody results={results.result1} />
-          {results.result2 && <ResultTableBody results={results.result2} />}
-          {results.result3 && <ResultTableBody results={results.result3} />}
-          {results.result4 && <ResultTableBody results={results.result4} />}
-          <FastLapTable fastLap={results.fastLap} />
-        </Table>
-      </TableContainer>
-    </div>
+    <TableContainer
+      sx={{ minWidth: 600, maxWidth: 1000 }}
+      component={Paper}
+      id="result-table-container"
+    >
+      <Table aria-label="simple table">
+        <TableHead className="result-table-head">
+          <TableRow>
+            <TableCell
+              colSpan={podium1.firstPlace.driver3 ? 4 : 3}
+              align="center"
+            >
+              {results.event}
+            </TableCell>
+            <TableCell colSpan={1} align="right">
+              {results.series}
+            </TableCell>
+            <TableCell colSpan={2} align="right">
+              {dateToString(results.date)}
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableHead>
+          <TableRow>
+            <TableCell>Place</TableCell>
+            <TableCell align="right">#</TableCell>
+            <TableCell align="right">Driver 1</TableCell>
+            {podium1.firstPlace.driver2 && (
+              <TableCell align="right">Driver 2</TableCell>
+            )}
+            {podium1.firstPlace.driver3 && (
+              <TableCell align="right">Driver 3</TableCell>
+            )}
+            <TableCell align="right">Team</TableCell>
+            <TableCell align="right">Vehicle</TableCell>
+          </TableRow>
+        </TableHead>
+        <ResultTableBody results={results.result1} />
+        {results.result2 && <ResultTableBody results={results.result2} />}
+        {results.result3 && <ResultTableBody results={results.result3} />}
+        {results.result4 && <ResultTableBody results={results.result4} />}
+        <FastLapTable fastLap={results.fastLap} />
+      </Table>
+    </TableContainer>
   );
-}
+};
 
 export default ResultTableHeader;
