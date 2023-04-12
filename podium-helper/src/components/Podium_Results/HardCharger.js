7b1e37d2e3665d2ddba8936d50178962a0cebfcd
Brendan Woo <woo.brendan@gmail.com>
2023-04-12 14:24:53 -0700

    add table head for hard charger and positions gained

--- podium-helper/src/components/Podium_Results/HardCharger.js
+++ podium-helper/src/components/Podium_Results/HardCharger.js
@@ -1,5 +1,19 @@
 const HardCharger = () => {
-  return <></>;
+  return (
+    <>
+      <TableHead>
+        <TableRow>
+          <TableCell colSpan={2}>Driver</TableCell>
+          <TableCell colSpan={3} align="center">
+            E-Boost Hard Charger
+          </TableCell>
+          <TableCell align="right" colSpan={2}>
+            Positions Gained
+          </TableCell>
+        </TableRow>
+      </TableHead>
+    </>
+  );
 };
 
 export default HardCharger;
