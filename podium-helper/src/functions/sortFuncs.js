3e74980178b2d2d0317523ed6837dc055f23c2e9
Brendan Woo <woo.brendan@gmail.com>
2023-04-12 11:34:24 -0700

    create compareByDate func and exporrt

--- podium-helper/src/functions/sortFuncs.js
+++ podium-helper/src/functions/sortFuncs.js
@@ -1,4 +1,4 @@
-import {gtwca, gt4a, gtam, igtc} from '../functions/helperFunc';
+import { gtwca, gt4a, gtam, igtc } from "../functions/helperFunc";
 
 const sortByVehicleType = (listOfEntries) => {
   const gt3 = [];
@@ -6,13 +6,13 @@ const sortByVehicleType = (listOfEntries) => {
   const gt4 = [];
   const tc = [];
   listOfEntries.forEach((entry) => {
-    if (entry.vehicle.includes('GT3')) gt3.push(entry)
-    else if (entry.vehicle.includes('GT2')) gt2.push(entry)
-    else if (entry.vehicle.includes('GT4')) gt4.push(entry)
-    else tc.push(entry)
-  })
-  return [...gt3, ...gt2, ...gt4, ...tc]
-}
+    if (entry.vehicle.includes("GT3")) gt3.push(entry);
+    else if (entry.vehicle.includes("GT2")) gt2.push(entry);
+    else if (entry.vehicle.includes("GT4")) gt4.push(entry);
+    else tc.push(entry);
+  });
+  return [...gt3, ...gt2, ...gt4, ...tc];
+};
 
 const sortBySeries = (listOfEntries) => {
   const gtwc = [];
@@ -22,86 +22,110 @@ const sortBySeries = (listOfEntries) => {
   const tc = [];
   listOfEntries.forEach((entry) => {
     const nameOfSeries = entry.series;
-    if (nameOfSeries === gtwca) gtwc.push(entry)
-    else if (nameOfSeries === gtam) gta.push(entry)
-    else if (nameOfSeries === gt4a) gt4.push(entry)
-    else if (nameOfSeries === igtc) igtcList.push(entry)
-    else tc.push(entry)
-  })
-  return [...gtwc, ...igtcList, ...gta, ...gt4, ...tc]
-}
-
+    if (nameOfSeries === gtwca) gtwc.push(entry);
+    else if (nameOfSeries === gtam) gta.push(entry);
+    else if (nameOfSeries === gt4a) gt4.push(entry);
+    else if (nameOfSeries === igtc) igtcList.push(entry);
+    else tc.push(entry);
+  });
+  return [...gtwc, ...igtcList, ...gta, ...gt4, ...tc];
+};
 
 const sortByManufacturer = (listOfEntries) => {
   const compareVehicles = (a, b) => {
     if (a.vehicle < b.vehicle) return -1;
     if (a.vehicle > b.vehicle) return 1;
     return 0;
-  }
-  return listOfEntries.sort(compareVehicles)
-}
+  };
+  return listOfEntries.sort(compareVehicles);
+};
 
 const sortByClass = (listOfEntries) => {
   const pro = [];
   const gt3proam = [];
   const gt4proam = [];
-  const gt3Silver = []
+  const gt3Silver = [];
   const gt3am = [];
   const gt4am = [];
   const gt4aSilver = [];
   const sro3 = [];
-  const masters = []
+  const masters = [];
   const gt2 = [];
-  const gt4 = []
+  const gt4 = [];
   const tcx = [];
   const tc = [];
   const tca = [];
-  
-  listOfEntries.forEach(entry => {
+
+  listOfEntries.forEach((entry) => {
     const nameOfClass = entry.class;
     const nameOfSeries = entry.series;
-    switch(nameOfClass) {
-      case 'Pro': 
+    switch (nameOfClass) {
+      case "Pro":
         pro.push(entry);
         break;
-      case 'Pro-Am': 
+      case "Pro-Am":
         nameOfSeries === gt4a ? gt4proam.push(entry) : gt3proam.push(entry);
         break;
-      case 'Am': 
+      case "Am":
         nameOfSeries === gt4a ? gt4am.push(entry) : gt3am.push(entry);
         break;
-      case 'Silver': 
+      case "Silver":
         nameOfSeries === gt4a ? gt4aSilver.push(entry) : gt3Silver.push(entry);
         break;
-      case 'SRO3': 
+      case "SRO3":
         sro3.push(entry);
         break;
-      case 'Masters': 
+      case "Masters":
         masters.push(entry);
         break;
-      case 'GT2': 
-        gt2.push(entry) 
+      case "GT2":
+        gt2.push(entry);
         break;
-      case 'GT4': 
+      case "GT4":
         gt4.push(entry);
         break;
-      case 'TCX': 
+      case "TCX":
         tcx.push(entry);
         break;
-      case 'TC': 
+      case "TC":
         tc.push(entry);
         break;
-      default: 
+      default:
         tca.push(entry);
         break;
     }
-  })
-  return [...pro, ...gt3proam, ...gt3Silver, ...gt3am, ...sro3, ...masters, ...gt2, ...gt4, ...gt4proam, ...gt4aSilver, ...gt4am, ...tcx, ...tc, ...tca ]
-}
+  });
+  return [
+    ...pro,
+    ...gt3proam,
+    ...gt3Silver,
+    ...gt3am,
+    ...sro3,
+    ...masters,
+    ...gt2,
+    ...gt4,
+    ...gt4proam,
+    ...gt4aSilver,
+    ...gt4am,
+    ...tcx,
+    ...tc,
+    ...tca,
+  ];
+};
+
+const compareByDate = (a, b) => {
+  const getEndDay = (dateStr) => {
+    return Number(dateStr.split("-"[1])); //[ 'April 28', '30' ]
+  };
+  const aLastDay = getEndDay(a.date);
+  const bLastDay = getEndDay(b.date);
+  return aLastDay - bLastDay;
+};
 
 export {
   sortBySeries,
   sortByVehicleType,
   sortByManufacturer,
-  sortByClass
-}
+  sortByClass,
+  compareByDate,
+};
