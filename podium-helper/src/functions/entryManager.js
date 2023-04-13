4caebfa4ec05433a7c394a5a8dfb5eeaf5f68411
Brendan Woo <woo.brendan@gmail.com>
2023-04-13 11:47:44 -0700

    create file to house func for entry management. move initalentrystate inside this file

---
+++ podium-helper/src/functions/entryManager.js
@@ -0,0 +1,17 @@
+const initialEntryState = {
+  team: "",
+  driver1: {
+    name: "",
+    nationality: "",
+    rating: "",
+  },
+
+  vehicle: "",
+  classification: "",
+  number: "",
+  carImage: "",
+  series: "",
+  year: 2023,
+};
+
+export { initialEntryState };
