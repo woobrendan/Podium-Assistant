67283cbfb2e64a158d72ea687f34d3255f6e974f
Brendan Woo <woo.brendan@gmail.com>
2023-04-14 11:54:12 -0700

    deconstruct driver 2. add to checkDriverVals a num val for which driver it is. add if block to adjust corresponding drivererr. add statement to check series then check for driver2

--- podium-helper/src/functions/entryManager.js
+++ podium-helper/src/functions/entryManager.js
@@ -1,3 +1,5 @@
+import { gtwca, gt4a } from "./helperFunc";
+
 const initialEntryState = {
   team: "",
   driver1: {
@@ -21,17 +23,24 @@ const errorState = {
   hasNumberErr: true,
   hasSeriesErr: true,
   hasDriverErr: true,
+  hasDriver2Err: false,
 };
 
 const checkEntryErrors = (entry, error, setError) => {
-  const { team, series, vehicle, classification, number, driver1 } = entry;
+  const { team, series, vehicle, classification, number, driver1, driver2 } =
+    entry;
   const errCopy = { ...error };
   const checkEmptyStr = (val) => (val === "" ? true : false);
-  const checkDriverVals = (driver) => {
+  const checkDriverVals = (driver, num) => {
     for (const key in driver) {
       if (checkEmptyStr(driver[key])) {
-        errCopy.hasDriverErr = true;
-        return;
+        if (num === 1) {
+          errCopy.hasDriverErr = true;
+          return;
+        } else {
+          errCopy.hasDriver2Err = true;
+          return;
+        }
       }
     }
     errCopy.hasDriverErr = false;
@@ -53,7 +62,9 @@ const checkEntryErrors = (entry, error, setError) => {
   checkEmptyStr(number)
     ? (errCopy.hasNumberErr = true)
     : (errCopy.hasNumberErr = false);
-  checkDriverVals(driver1);
+
+  checkDriverVals(driver1, 1);
+  if (series === gtwca || series === gt4a) checkDriverVals(driver2, 2);
 
   // loop through the error state, if a key returns true set error state to copy and exit
   for (const key in errCopy) {
