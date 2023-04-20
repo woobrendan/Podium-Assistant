08f1ea750e4a03c0951d3fef52659ce3f0c5f045
Brendan Woo <woo.brendan@gmail.com>
2023-04-20 10:56:47 -0700

    deconstruct val for drivers. change else ifs to be ifs, change each if block return to be explicit

--- podium-helper/src/components/Competitor/searchAllEntries.js
+++ podium-helper/src/components/Competitor/searchAllEntries.js
@@ -1,20 +1,52 @@
+const searchAllEntries = (list, searchValue) => {
+  const filtered = list.filter((val) => {
+    const { driver1, driver2 } = val;
 
-export default function searchAllEntries(list, searchValue) {
-  const filtered = list.filter(val => {
-    const driver1 = val.driver1;
-    const driver2 = val.driver2;
-    if (!searchValue) return val
-    else if (driver1.name.toLowerCase().includes(searchValue.toLowerCase())) return val;
-    else if (driver2 && driver2.name.toLowerCase().includes(searchValue.toLowerCase())) return val;
-    else if (val.vehicle.toLowerCase().includes(searchValue.toLowerCase())) return val;
-    else if (val.team.toLowerCase().includes(searchValue.toLowerCase())) return val;
-    else if (driver1.rating.toLowerCase().includes(searchValue.toLowerCase())) return val;
-    else if (driver2 && driver2.rating.toLowerCase().includes(searchValue.toLowerCase())) return val;
-    else if (driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())) return val; 
-    else if (driver2 && driver2.nationality.toLowerCase().includes(searchValue.toLowerCase())) return val; 
-    else if (val.series.toLowerCase().includes(searchValue.toLowerCase())) return val;
-    else if (val.number.includes(searchValue)) return val;
-    else if (val.class.includes(searchValue)) return val;
+    if (!searchValue) return val;
+    if (driver1.name.toLowerCase().includes(searchValue.toLowerCase())) {
+      return val;
+    }
+    if (
+      driver2 &&
+      driver2.name.toLowerCase().includes(searchValue.toLowerCase())
+    ) {
+      return val;
+    }
+    if (val.vehicle.toLowerCase().includes(searchValue.toLowerCase())) {
+      return val;
+    }
+    if (val.team.toLowerCase().includes(searchValue.toLowerCase())) {
+      return val;
+    }
+    if (driver1.rating.toLowerCase().includes(searchValue.toLowerCase())) {
+      return val;
+    }
+    if (
+      driver2 &&
+      driver2.rating.toLowerCase().includes(searchValue.toLowerCase())
+    ) {
+      return val;
+    }
+    if (driver1.nationality.toLowerCase().includes(searchValue.toLowerCase())) {
+      return val;
+    }
+    if (
+      driver2 &&
+      driver2.nationality.toLowerCase().includes(searchValue.toLowerCase())
+    ) {
+      return val;
+    }
+    if (val.series.toLowerCase().includes(searchValue.toLowerCase())) {
+      return val;
+    }
+    if (val.number.includes(searchValue)) {
+      return val;
+    }
+    if (val.class.includes(searchValue)) {
+      return val;
+    }
   });
   return filtered;
-}
+};
+
+export default searchAllEntries;
