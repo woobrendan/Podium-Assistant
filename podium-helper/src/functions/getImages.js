9d3bb5c7e12752e7570346212ba8b37fdf3b0a6e
Brendan Woo <woo.brendan@gmail.com>
2023-04-26 17:15:00 -0500

    create new getManufLogo method to use new vehicleLogo obj to get logos

--- podium-helper/src/functions/getImages.js
+++ podium-helper/src/functions/getImages.js
@@ -33,27 +33,53 @@ import {
   gtam_gt3,
   gtam_gt4,
 } from "../images/class_logos";
-import { grCup, gt4a } from "./helperFunc";
+import { gt4a } from "./helperFunc";
+
+// const getManufLogo = (vehicle) => {
+//   if (vehicle.includes("Porsche")) return porscheLogo;
+//   if (vehicle.includes("BMW")) return bmwLogo;
+//   if (vehicle.includes("Mercedes")) return mercedesLogo;
+//   if (vehicle.includes("Acura")) return acuraLogo;
+//   if (vehicle.includes("Aston")) return astonMartin;
+//   if (vehicle.includes("Ferrari")) return ferrari;
+//   if (vehicle.includes("Ginetta")) return ginetta;
+//   if (vehicle.includes("Honda")) return honda;
+//   if (vehicle.includes("Lambo")) return lamborghini;
+//   if (vehicle.includes("MINI")) return MINI;
+//   if (vehicle.includes("Toyota")) return toyota;
+//   if (vehicle.includes("Audi")) return audi;
+//   if (vehicle.includes("Corvette")) return corvette;
+//   if (vehicle.includes("Hyundai")) return hyundai;
+//   if (vehicle.includes("Nissan")) return nissan;
+//   if (vehicle.includes("Saleen")) return saleen;
+//   if (vehicle.includes("Ford")) return ford;
+//   return sro;
+// };
+
+const vehicleLogos = {
+  Porsche: porscheLogo,
+  BMW: bmwLogo,
+  Mercedes: mercedesLogo,
+  Acura: acuraLogo,
+  Aston: astonMartin,
+  Ferrari: ferrari,
+  Ginetta: ginetta,
+  Honda: honda,
+  Lamborghini: lamborghini,
+  MINI: MINI,
+  Toyota: toyota,
+  Audi: audi,
+  Corvette: corvette,
+  Hyundai: hyundai,
+  Nissan: nissan,
+  Saleen: saleen,
+  Ford: ford,
+};
 
 const getManufLogo = (vehicle) => {
-  if (vehicle.includes("Porsche")) return porscheLogo;
-  if (vehicle.includes("BMW")) return bmwLogo;
-  if (vehicle.includes("Mercedes")) return mercedesLogo;
-  if (vehicle.includes("Acura")) return acuraLogo;
-  if (vehicle.includes("Aston")) return astonMartin;
-  if (vehicle.includes("Ferrari")) return ferrari;
-  if (vehicle.includes("Ginetta")) return ginetta;
-  if (vehicle.includes("Honda")) return honda;
-  if (vehicle.includes("Lambo")) return lamborghini;
-  if (vehicle.includes("MINI")) return MINI;
-  if (vehicle.includes("Toyota")) return toyota;
-  if (vehicle.includes("Audi")) return audi;
-  if (vehicle.includes("Corvette")) return corvette;
-  if (vehicle.includes("Hyundai")) return hyundai;
-  if (vehicle.includes("Nissan")) return nissan;
-  if (vehicle.includes("Saleen")) return saleen;
-  if (vehicle.includes("Ford")) return ford;
-  return sro;
+  const vehicleName = vehicle.split(" ")[0];
+  const logo = vehicleLogos[vehicleName];
+  return logo ? logo : sro;
 };
 
 // classif === classification
