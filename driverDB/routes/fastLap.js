28262565734280c10e1be991ce725bfbe5bcb5c5
Brendan Woo <woo.brendan@gmail.com>
2023-04-27 09:40:06 -0500

    remove consolee logs

--- driverDB/routes/fastLap.js
+++ driverDB/routes/fastLap.js
@@ -1,20 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-  router.post('/', (req, res) => {
+  router.post("/", (req, res) => {
     const fastLap = req.body;
-    console.log('req.body', fastLap)
-    const queryString = 
-    `INSERT INTO fastlaps (driver_id, laptime) VALUES ($1, $2) RETURNING *;`;
+    const queryString = `INSERT INTO fastlaps (driver_id, laptime) VALUES ($1, $2) RETURNING *;`;
     return db
       .query(queryString, [fastLap.driver, fastLap.lapTime])
-      .then(result => {
-        console.log('result rows:', result.rows[0])
-        return result.rows[0].id
+      .then((result) => {
+        return result.rows[0].id;
       })
-      .catch(err => console.log(err.message))
+      .catch((err) => console.log(err.message));
   });
   return router;
-}
-
+};
