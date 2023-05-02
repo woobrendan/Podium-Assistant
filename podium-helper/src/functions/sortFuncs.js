a37b30ecd1a51b83cb1a8ab619f78dff07964ea7
Brendan Woo <woo.brendan@gmail.com>
2023-05-02 16:02:59 -0700

    create compareresultdates func and export

--- podium-helper/src/functions/sortFuncs.js
+++ podium-helper/src/functions/sortFuncs.js
@@ -163,10 +163,17 @@ const compareByDate = (a, b) => {
   return aDate.endDay - bDate.endDay;
 };
 
+const compareResultDates = (a, b) => {
+  const dateA = new Date(a.date);
+  const dateB = new Date(b.date);
+  return dateB - dateA;
+};
+
 export {
   sortBySeries,
   sortByVehicleType,
   sortByManufacturer,
   sortByClass,
   compareByDate,
+  compareResultDates,
 };
