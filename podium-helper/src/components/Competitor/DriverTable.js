603a963ab788d90769ecd49c8e56d79e3c2907b8
Brendan Woo <woo.brendan@gmail.com>
2023-05-04 11:41:52 -0700

    remove wrapper div from tablecontainer that was unused

--- podium-helper/src/components/Competitor/DriverTable.js
+++ podium-helper/src/components/Competitor/DriverTable.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@mui/material'
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 
 function createData(name, nationality, rating) {
   return { name, nationality, rating };
@@ -7,46 +15,61 @@ function createData(name, nationality, rating) {
 
 function DriverTable(props) {
   const tableRows = (drivers) => {
-    const data = [createData(drivers.driver1.name, drivers.driver1.nationality, drivers.driver1.rating)];
+    const data = [
+      createData(
+        drivers.driver1.name,
+        drivers.driver1.nationality,
+        drivers.driver1.rating,
+      ),
+    ];
     if (drivers.driver2) {
-      data.push(createData(drivers.driver2.name, drivers.driver2.nationality, drivers.driver2.rating))
+      data.push(
+        createData(
+          drivers.driver2.name,
+          drivers.driver2.nationality,
+          drivers.driver2.rating,
+        ),
+      );
     }
     if (drivers.driver3) {
-      data.push(createData(drivers.driver3.name, drivers.driver3.nationality, drivers.driver3.rating))
+      data.push(
+        createData(
+          drivers.driver3.name,
+          drivers.driver3.nationality,
+          drivers.driver3.rating,
+        ),
+      );
     }
-    return data
-  }
+    return data;
+  };
 
   return (
-    <div className="entry-driver-details">
-      <TableContainer component={Paper} className="driver-table">
-        <Table sx={{ minWidth: 300 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Driver</TableCell>
-              <TableCell align="right">Nat.</TableCell>
-              <TableCell align="right">Rating</TableCell>
+    <TableContainer component={Paper} className="driver-table">
+      <Table sx={{ minWidth: 300 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Driver</TableCell>
+            <TableCell align="right">Nat.</TableCell>
+            <TableCell align="right">Rating</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tableRows(props.drivers).map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.nationality}</TableCell>
+              <TableCell align="right">{row.rating}</TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {tableRows(props.drivers).map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.nationality}</TableCell>
-                <TableCell align="right">{row.rating}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
-
 }
 
-export default DriverTable
+export default DriverTable;
