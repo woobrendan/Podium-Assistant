4113f5040656290dc1be9f8d51c446462fc75944
Brendan Woo <woo.brendan@gmail.com>
2023-05-05 11:17:39 -0700

    add in template from fastlap change values over to be hard charger

--- podium-helper/src/components/Podium_Results/HardChargeCard.js
+++ podium-helper/src/components/Podium_Results/HardChargeCard.js
@@ -1,5 +1,39 @@
 const HardChargeCard = () => {
-  return <></>;
+  return (
+    <Card className="hard_charger_container">
+      <h2>{series.name === grCup ? "Hard Charger" : "E-Boost Hard Charger"}</h2>
+      <Box className="hard_charger__input">
+        <FormControl>
+          <InputLabel>Entry</InputLabel>
+          <Select
+            className="form-control"
+            name="entry"
+            label="Entry"
+            value={hardCharge.entry}
+            onChange={handleChange}
+          >
+            {mapSingleDrivers}
+          </Select>
+        </FormControl>
+      </Box>
+      <Box className="hard_charger__input">
+        <TextField
+          id="outlined"
+          label="Positions Gained"
+          name="positions"
+          value={hardCharge.gain}
+          onChange={handleChange}
+        />
+      </Box>
+      <Button
+        variant="contained"
+        color={isSubmitted ? "success" : "error"}
+        onClick={handleClick}
+      >
+        {isSubmitted ? "Update" : "Submit"}
+      </Button>
+    </Card>
+  );
 };
 
 export default HardChargeCard;
