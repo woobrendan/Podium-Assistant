963c06afb79effd6e51ff3577b1633c3a023ef8a
Brendan Woo <woo.brendan@gmail.com>
2023-05-05 12:04:44 -0700

    add import for mui components. add wrapper div to whole component,  give classname podium_card to card

--- podium-helper/src/components/Podium_Results/HardChargeCard.js
+++ podium-helper/src/components/Podium_Results/HardChargeCard.js
@@ -1,5 +1,16 @@
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  TextField,
+  Button,
+  Card,
+  Box,
+  MenuItem,
+} from "@mui/material";
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { grCup } from "../../functions/helperFunc";
 
 const HardChargeCard = ({ series, handleSubmit }) => {
   const [hardCharge, setHardCharge] = useState({ entry: "", gain: null });
@@ -20,47 +31,51 @@ const HardChargeCard = ({ series, handleSubmit }) => {
   };
 
   return (
-    <Card className="hard_charger_container">
-      <h2>{series.name === grCup ? "Hard Charger" : "E-Boost Hard Charger"}</h2>
-      <Box className="hard_charger__input">
-        <FormControl>
-          <InputLabel>Entry</InputLabel>
-          <Select
-            className="form-control"
-            name="entry"
-            label="Entry"
-            value={hardCharge.entry}
+    <div className="results-container">
+      <Card className="hard_charger_container podium_card">
+        <h2>
+          {series.name === grCup ? "Hard Charger" : "E-Boost Hard Charger"}
+        </h2>
+        <Box className="hard_charger__input">
+          <FormControl>
+            <InputLabel>Entry</InputLabel>
+            <Select
+              className="form-control"
+              name="entry"
+              label="Entry"
+              value={hardCharge.entry}
+              onChange={handleChange}
+            >
+              {entries
+                .filter((entry) => entry.series === series)
+                .map((entry) => {
+                  return (
+                    <MenuItem key={index} value={entry}>
+                      #{entry.number} - {entry.driver1} & {entry.driver2}
+                    </MenuItem>
+                  );
+                })}
+            </Select>
+          </FormControl>
+        </Box>
+        <Box className="hard_charger__input">
+          <TextField
+            id="outlined"
+            label="Positions Gained"
+            name="gain"
+            value={hardCharge.gain}
             onChange={handleChange}
-          >
-            {entries
-              .filter((entry) => entry.series === series)
-              .map((entry) => {
-                return (
-                  <MenuItem key={index} value={entry}>
-                    #{entry.number} - {entry.driver1} & {entry.driver2}
-                  </MenuItem>
-                );
-              })}
-          </Select>
-        </FormControl>
-      </Box>
-      <Box className="hard_charger__input">
-        <TextField
-          id="outlined"
-          label="Positions Gained"
-          name="gain"
-          value={hardCharge.gain}
-          onChange={handleChange}
-        />
-      </Box>
-      <Button
-        variant="contained"
-        color={isSubmitted ? "success" : "error"}
-        onClick={handleClick}
-      >
-        {isSubmitted ? "Update" : "Submit"}
-      </Button>
-    </Card>
+          />
+        </Box>
+        <Button
+          variant="contained"
+          color={isSubmitted ? "success" : "error"}
+          onClick={handleClick}
+        >
+          {isSubmitted ? "Update" : "Submit"}
+        </Button>
+      </Card>
+    </div>
   );
 };
 
