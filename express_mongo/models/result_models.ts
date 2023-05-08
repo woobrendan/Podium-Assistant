8b4794173beb7a4ae0d7b4b8c3973fd8ae19c75d
Brendan Woo <woo.brendan@gmail.com>
2023-05-08 12:43:36 -0700

    fix inside fullresultinterface, have hardcharge as charger to match others models and schemas

--- express_mongo/models/result_models.ts
+++ express_mongo/models/result_models.ts
@@ -31,7 +31,7 @@ export interface FullResultInterface {
   date: string;
   event: string;
   fastLap: FastLapInterface;
-  hardCharge?: HardChargerInterface;
+  hardCharger?: HardChargerInterface;
   result1: ResultInterface;
   result2?: ResultInterface;
   result3?: ResultInterface;
