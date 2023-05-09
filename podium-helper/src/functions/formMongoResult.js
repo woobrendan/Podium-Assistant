5efb2bd0dd41f8564c002ad260774d39b0e759d6
Brendan Woo <woo.brendan@gmail.com>
2023-05-09 15:29:57 -0700

    create func placementResult and implement throughout replace large if blocks and copy statements

--- podium-helper/src/functions/formMongoResult.js
+++ podium-helper/src/functions/formMongoResult.js
@@ -1,14 +1,9 @@
 const mongoResult = (results, fastLap) => {
-  const result1 = results.result1;
-  const result2 = results.result2;
-  const result3 = results.result3;
-  const result4 = results.result4;
-  const { hardCharger } = results;
+  const { result1, result2, result3, result4, hardCharger, series } = results;
 
   const copy = {
-    date: results.date,
-    event: results.event,
-    series: results.series.name,
+    ...results,
+    series: series.name,
     fastLap: {
       driver: fastLap.driver,
       laptime: fastLap.laptime,
@@ -42,36 +37,12 @@ const mongoResult = (results, fastLap) => {
 
   //// Result 1, Second Place //////
   if (result1.secondPlace) {
-    copy.result1.secondPlace = {
-      driver1: result1.secondPlace.driver1.name,
-      number: result1.secondPlace.number,
-      vehicle: result1.secondPlace.vehicle,
-      team: result1.secondPlace.team,
-    };
-  }
-
-  if (result1.secondPlace.driver2) {
-    copy.result1.secondPlace.driver2 = result1.secondPlace.driver2.name;
-  }
-  if (result1.secondPlace.driver3) {
-    copy.result1.secondPlace.driver3 = result1.secondPlace.driver3.name;
+    copy.result1.secondPlace = placementResult(result1.secondPlace);
   }
 
   //// Result 1, Third Place //////
   if (result1.thirdPlace) {
-    copy.result1.thirdPlace = {
-      driver1: result1.thirdPlace.driver1.name,
-      number: result1.thirdPlace.number,
-      vehicle: result1.thirdPlace.vehicle,
-      team: result1.thirdPlace.team,
-    };
-  }
-
-  if (result1.thirdPlace.driver2) {
-    copy.result1.thirdPlace.driver2 = result1.thirdPlace.driver2.name;
-  }
-  if (result1.thirdPlace.driver3) {
-    copy.result1.thirdPlace.driver3 = result1.thirdPlace.driver3.name;
+    copy.result1.thirdPlace = placementResult(result1.thirdPlace);
   }
 
   //// Result 2, First Place //////
@@ -95,36 +66,12 @@ const mongoResult = (results, fastLap) => {
 
     //// Result 2, Second Place //////
     if (result2.secondPlace) {
-      copy.result2.secondPlace = {
-        driver1: result2.secondPlace.driver1.name,
-        number: result2.secondPlace.number,
-        vehicle: result2.secondPlace.vehicle,
-        team: result2.secondPlace.team,
-      };
-    }
-
-    if (result2.secondPlace.driver2) {
-      copy.result2.secondPlace.driver2 = result2.secondPlace.driver2.name;
-    }
-    if (result2.secondPlace.driver3) {
-      copy.result2.secondPlace.driver3 = result2.secondPlace.driver3.name;
+      copy.result2.secondPlace = placementResult(result2.secondPlace);
     }
 
     //// Result 2, Third Place //////
     if (result2.thirdPlace) {
-      copy.result2.thirdPlace = {
-        driver1: result2.thirdPlace.driver1.name,
-        number: result2.thirdPlace.number,
-        vehicle: result2.thirdPlace.vehicle,
-        team: result2.thirdPlace.team,
-      };
-    }
-
-    if (result2.thirdPlace.driver2) {
-      copy.result2.thirdPlace.driver2 = result2.thirdPlace.driver2.name;
-    }
-    if (result2.thirdPlace.driver3) {
-      copy.result2.thirdPlace.driver3 = result2.thirdPlace.driver3.name;
+      copy.result2.thirdPlace = placementResult(result2.thirdPlace);
     }
   }
 
@@ -149,36 +96,12 @@ const mongoResult = (results, fastLap) => {
 
     //// Result 3, Second Place //////
     if (result3.secondPlace) {
-      copy.result3.secondPlace = {
-        driver1: result3.secondPlace.driver1.name,
-        number: result3.secondPlace.number,
-        vehicle: result3.secondPlace.vehicle,
-        team: result3.secondPlace.team,
-      };
-    }
-
-    if (result3.secondPlace.driver2) {
-      copy.result3.secondPlace.driver2 = result3.secondPlace.driver2.name;
-    }
-    if (result3.secondPlace.driver3) {
-      copy.result3.secondPlace.driver3 = result3.secondPlace.driver3.name;
+      copy.result3.secondPlace = placementResult(result3.secondPlace);
     }
 
     //// Result 3, Third Place //////
     if (result3.thirdPlace) {
-      copy.result3.thirdPlace = {
-        driver1: result3.thirdPlace.driver1.name,
-        number: result3.thirdPlace.number,
-        vehicle: result3.thirdPlace.vehicle,
-        team: result3.thirdPlace.team,
-      };
-    }
-
-    if (result3.thirdPlace.driver2) {
-      copy.result3.thirdPlace.driver2 = result3.thirdPlace.driver2.name;
-    }
-    if (result3.thirdPlace.driver3) {
-      copy.result3.thirdPlace.driver3 = result3.thirdPlace.driver3.name;
+      copy.result3.thirdPlace = placementResult(result3.thirdPlace);
     }
   }
 
@@ -202,40 +125,28 @@ const mongoResult = (results, fastLap) => {
 
     //// Result 4, Second Place //////
     if (result4.secondPlace) {
-      copy.result4.secondPlace = {
-        driver1: result4.secondPlace.driver1.name,
-        number: result4.secondPlace.number,
-        vehicle: result4.secondPlace.vehicle,
-        team: result4.secondPlace.team,
-      };
-    }
-
-    if (result4.secondPlace.driver2) {
-      copy.result4.secondPlace.driver2 = result4.secondPlace.driver2.name;
-    }
-    if (result4.secondPlace.driver3) {
-      copy.result4.secondPlace.driver3 = result4.secondPlace.driver3.name;
+      copy.result3.secondPlace = placementResult(result3.secondPlace);
     }
 
     //// Result 4, Third Place //////
     if (result4.thirdPlace) {
-      copy.result4.thirdPlace = {
-        driver1: result4.thirdPlace.driver1.name,
-        number: result4.thirdPlace.number,
-        vehicle: result4.thirdPlace.vehicle,
-        team: result4.thirdPlace.team,
-      };
-    }
-
-    if (result4.thirdPlace.driver2) {
-      copy.result4.thirdPlace.driver2 = result4.thirdPlace.driver2.name;
-    }
-    if (result4.thirdPlace.driver3) {
-      copy.result4.thirdPlace.driver3 = result4.thirdPlace.driver3.name;
+      copy.result4.thirdPlace = placementResult(result4.thirdPlace);
     }
   }
 
   return copy;
+};
+
+const placementResult = (placeRes) => {
+  const { driver1, driver2, driver3, number, vehicle, team } = placeRes;
+  return {
+    driver1: driver1.name,
+    ...(driver2 ? { driver2: { ...driver2 } } : {}),
+    ...(driver3 ? { driver2: { ...driver3 } } : {}),
+    number,
+    vehicle,
+    team,
+  };
 };
 
 export default mongoResult;
