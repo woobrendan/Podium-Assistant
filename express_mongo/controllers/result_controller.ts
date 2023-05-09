59de1ed677dcf0c4a741730851ae33139a3965b6
Brendan Woo <woo.brendan@gmail.com>
2023-05-09 15:11:52 -0700

    remove commented out result structuring. import hardChargeResult and use for hardcharger. change result1 to use resultBuilder

--- express_mongo/controllers/result_controller.ts
+++ express_mongo/controllers/result_controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import mongoose from "mongoose";
 import Result from "../models/result_schema";
-import { resultBuilder } from "../functions/helperFunc";
+import { resultBuilder, hardChargeResult } from "../functions/helperFunc";
 import { ResultInterface } from "../models/result_models";
 
 const createResult = (req: Request, res: Response) => {
@@ -18,78 +18,16 @@ const createResult = (req: Request, res: Response) => {
     result4?: ResultInterface;
   } = req.body.results;
 
-  const { entry } = hardCharger;
   const result = new Result({
     ...req.body.results,
     fastLap: { ...fastLap },
     ...(hardCharger
-      ? {
-          hardCharger: {
-            ...hardCharger,
-            entry: {
-              ...entry,
-              driver1: {
-                ...entry.driver1,
-              },
-              ...(entry.driver2 ? { driver2: { ...entry.driver2 } } : {}),
-              ...(entry.driver3 ? { driver3: { ...entry.driver3 } } : {}),
-            },
-          },
-        }
+      ? { hardCharger: { ...hardChargeResult(hardCharger) } }
       : {}),
-    result1: {
-      ...result1,
-      firstPlace: { ...result1.firstPlace },
-      ...(result1.secondPlace
-        ? { secondPlace: { ...result1.secondPlace } }
-        : {}),
-      ...(result1.thirdPlace ? { thirdPlace: { ...result1.thirdPlace } } : {}),
-    },
+    result1: { ...resultBuilder(result1) },
     ...(result2 ? { result2: { ...resultBuilder(result2) } } : {}),
     ...(result3 ? { result3: { ...resultBuilder(result3) } } : {}),
     ...(result4 ? { result4: { ...resultBuilder(result4) } } : {}),
-    // ...(result2
-    //   ? {
-    //       result2: {
-    //         ...result2,
-    //         firstPlace: { ...result2.firstPlace },
-    //         ...(result2.secondPlace
-    //           ? { secondPlace: { ...result2.secondPlace } }
-    //           : {}),
-    //         ...(result2.thirdPlace
-    //           ? { thirdPlace: { ...result2.thirdPlace } }
-    //           : {}),
-    //       },
-    //     }
-    //   : {}),
-    // ...(result3
-    //   ? {
-    //       result3: {
-    //         ...result3,
-    //         firstPlace: { ...result3.firstPlace },
-    //         ...(result3.secondPlace
-    //           ? { secondPlace: { ...result3.secondPlace } }
-    //           : {}),
-    //         ...(result3.thirdPlace
-    //           ? { thirdPlace: { ...result3.thirdPlace } }
-    //           : {}),
-    //       },
-    //     }
-    //   : {}),
-    // ...(result4
-    //   ? {
-    //       result4: {
-    //         ...result4,
-    //         firstPlace: { ...result4.firstPlace },
-    //         ...(result4.secondPlace
-    //           ? { secondPlace: { ...result4.secondPlace } }
-    //           : {}),
-    //         ...(result4.thirdPlace
-    //           ? { thirdPlace: { ...result4.thirdPlace } }
-    //           : {}),
-    //       },
-    //     }
-    //   : {}),
   });
 
   return result
