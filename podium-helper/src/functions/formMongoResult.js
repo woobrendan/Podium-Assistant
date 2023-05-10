c437e236f9ea1056c0d7270e4d98489359488882
Brendan Woo <woo.brendan@gmail.com>
2023-05-10 15:49:27 -0700

    change fast lap to be spread from param. change each result to ternerary and render using buildFullResult func. fix in fullBuildResult to use palcementResult for second and third

--- podium-helper/src/functions/formMongoResult.js
+++ podium-helper/src/functions/formMongoResult.js
@@ -1,13 +1,9 @@
 const mongoResult = (results, fastLap) => {
   const { result1, result2, result3, result4, hardCharger, series } = results;
-
-  const copy = {
+  return {
     ...results,
     series: series.name,
-    fastLap: {
-      driver: fastLap.driver,
-      laptime: fastLap.laptime,
-    },
+    fastLap: { ...fastLap },
     ...(hardCharger
       ? {
           hardCharger: {
@@ -16,125 +12,11 @@ const mongoResult = (results, fastLap) => {
           },
         }
       : {}),
-    result1: {
-      class: result1.class,
-      firstPlace: {
-        driver1: result1.firstPlace.driver1.name,
-        number: result1.firstPlace.number,
-        vehicle: result1.firstPlace.vehicle,
-        team: result1.firstPlace.team,
-      },
-    },
+    result1: buildFullResult(result1),
+    ...(result2 ? { result2: buildFullResult(result2) } : {}),
+    ...(result3 ? { result3: buildFullResult(result3) } : {}),
+    ...(result4 ? { result4: buildFullResult(result4) } : {}),
   };
-
-  //checking result 1 for second or third drivers
-  if (result1.firstPlace.driver2) {
-    copy.result1.firstPlace.driver2 = result1.firstPlace.driver2.name;
-  }
-  if (result1.firstPlace.driver3) {
-    copy.result1.firstPlace.driver3 = result1.firstPlace.driver3.name;
-  }
-
-  //// Result 1, Second Place //////
-  if (result1.secondPlace) {
-    copy.result1.secondPlace = placementResult(result1.secondPlace);
-  }
-
-  //// Result 1, Third Place //////
-  if (result1.thirdPlace) {
-    copy.result1.thirdPlace = placementResult(result1.thirdPlace);
-  }
-
-  //// Result 2, First Place //////
-  if (result2) {
-    copy["result2"] = {
-      class: result2.class,
-      firstPlace: {
-        driver1: result2.firstPlace.driver1.name,
-        number: result2.firstPlace.number,
-        vehicle: result2.firstPlace.vehicle,
-        team: result2.firstPlace.team,
-      },
-    };
-
-    if (result2.firstPlace.driver2) {
-      copy.result2.firstPlace.driver2 = result2.firstPlace.driver2.name;
-    }
-    if (result2.firstPlace.driver3) {
-      copy.result2.firstPlace.driver3 = result2.firstPlace.driver3.name;
-    }
-
-    //// Result 2, Second Place //////
-    if (result2.secondPlace) {
-      copy.result2.secondPlace = placementResult(result2.secondPlace);
-    }
-
-    //// Result 2, Third Place //////
-    if (result2.thirdPlace) {
-      copy.result2.thirdPlace = placementResult(result2.thirdPlace);
-    }
-  }
-
-  //// Result 3, First Place //////
-  if (result3) {
-    copy["result3"] = {
-      class: result3.class,
-      firstPlace: {
-        driver1: result3.firstPlace.driver1.name,
-        number: result3.firstPlace.number,
-        vehicle: result3.firstPlace.vehicle,
-        team: result3.firstPlace.team,
-      },
-    };
-
-    if (result3.firstPlace.driver2) {
-      copy.result3.firstPlace.driver2 = result3.firstPlace.driver2.name;
-    }
-    if (result3.firstPlace.driver3) {
-      copy.result3.firstPlace.driver3 = result3.firstPlace.driver3.name;
-    }
-
-    //// Result 3, Second Place //////
-    if (result3.secondPlace) {
-      copy.result3.secondPlace = placementResult(result3.secondPlace);
-    }
-
-    //// Result 3, Third Place //////
-    if (result3.thirdPlace) {
-      copy.result3.thirdPlace = placementResult(result3.thirdPlace);
-    }
-  }
-
-  //// Result 4, First Place //////
-  if (result4) {
-    copy["result4"] = {
-      class: result4.class,
-      firstPlace: {
-        driver1: result4.firstPlace.driver1.name,
-        number: result4.firstPlace.number,
-        vehicle: result4.firstPlace.vehicle,
-        team: result4.firstPlace.team,
-      },
-    };
-    if (result4.firstPlace.driver2) {
-      copy.result4.firstPlace.driver2 = result4.firstPlace.driver2.name;
-    }
-    if (result4.firstPlace.driver3) {
-      copy.result4.firstPlace.driver3 = result4.firstPlace.driver3.name;
-    }
-
-    //// Result 4, Second Place //////
-    if (result4.secondPlace) {
-      copy.result3.secondPlace = placementResult(result3.secondPlace);
-    }
-
-    //// Result 4, Third Place //////
-    if (result4.thirdPlace) {
-      copy.result4.thirdPlace = placementResult(result4.thirdPlace);
-    }
-  }
-
-  return copy;
 };
 
 const placementResult = (placeRes) => {
@@ -154,8 +36,8 @@ const buildFullResult = (result) => {
   return {
     ...result,
     firstPlace: placementResult(firstPlace),
-    ...(secondPlace ? { secondPlace: { ...secondPlace } } : {}),
-    ...(thirdPlace ? { thirdPlace: { ...thirdPlace } } : {}),
+    ...(secondPlace ? { secondPlace: placementResult(secondPlace) } : {}),
+    ...(thirdPlace ? { thirdPlace: placementResult(thirdPlace) } : {}),
   };
 };
 
