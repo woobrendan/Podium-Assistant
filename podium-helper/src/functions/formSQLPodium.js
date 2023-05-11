8e19ce823f28c162714560c78297db148192f997
Brendan Woo <woo.brendan@gmail.com>
2023-05-11 14:17:25 -0700

    create func getResult to clean up repeated code. change return value and use ternerary for each result

--- podium-helper/src/functions/formSQLPodium.js
+++ podium-helper/src/functions/formSQLPodium.js
@@ -1,48 +1,33 @@
-import { getIdFromArray, getVehicleId } from './helperFunc';
+import { getIdFromArray, getVehicleId } from "./helperFunc";
 
 // create copy of results useState to get proper values to pass. change values of drivers into their id number for backend
 const formSQLPodium = (results) => {
+  const { result1, result2, result3, result4 } = results;
 
-  const result1 = results.result1;
-  const result2 = results.result2;
-  const result3 = results.result3;
-  const result4 = results.result4;
-
-  const copy = {
+  return {
     ...results,
-    result1: {
-      class: getIdFromArray(result1.class, classCategory),
-      firstPlace: getVehicleId(result1.firstPlace.number, vehicles),
-      secondPlace: (getVehicleId(result1.secondPlace.number, vehicles) || null),
-      thirdPlace: (getVehicleId(result1.thirdPlace.number, vehicles) || null)
-    },
-    result2: {
-      class: getIdFromArray(result2.class, classCategory),
-      firstPlace: getVehicleId(result2.firstPlace.number, vehicles),
-      secondPlace: (getVehicleId(result2.secondPlace.number, vehicles) || null),
-      thirdPlace: (getVehicleId(result2.thirdPlace.number, vehicles) || null)
-    },
+    result1: getResult(result1),
+    ...(result2 ? { result2: getResult(result2) } : {}),
+    ...(result3 ? { result3: getResult(result3) } : {}),
+    ...(result4 ? { result4: getResult(result4) } : {}),
     event: getIdFromArray(results.event, events),
     series: getIdFromArray(results.series, series),
-    fastLap: {...value, id: getIdFromArray(value.driver, drivers)}
-  }
-  if (result3) {
-    copy['result3'] = {
-      class: getIdFromArray(result3.class, classCategory),
-      firstPlace: getVehicleId(result3.firstPlace.number, vehicles),
-      secondPlace: (getVehicleId(result3.secondPlace.number, vehicles) || null),
-      thirdPlace: (getVehicleId(result3.thirdPlace.number, vehicles) || null)
-    }
-  }
-  if (result4) {
-    copy['result4'] = {
-      class: getIdFromArray(result4.class, classCategory),
-      firstPlace: getVehicleId(result4.firstPlace.number, vehicles),
-      secondPlace: (getVehicleId(result4.secondPlace.number, vehicles) || null),
-      thirdPlace: (getVehicleId(result4.thirdPlace.number, vehicles) || null)
-    }
-  }
-  return copy;
-}
+    fastLap: { ...value, id: getIdFromArray(value.driver, drivers) },
+  };
+};
+
+const getResult = (result) => {
+  const { firstPlace, secondPlace, thirdPlace } = result;
+  return {
+    class: getIdFromArray(result.class, classCategory),
+    firstPlace: getVehicleId(firstPlace.number, vehicles),
+    ...(secondPlace
+      ? { secondPlace: getVehicleId(secondPlace.number, vehicles) }
+      : null),
+    ...(thirdPlace
+      ? { thirdPlace: getVehicleId(thirdPlace.number, vehicles) }
+      : null),
+  };
+};
 
 module.exports = formSQLPodium;
