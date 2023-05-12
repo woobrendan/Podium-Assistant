6f958a834c3cf7cc66711b9b563de6aefbd0d22e
Brendan Woo <woo.brendan@gmail.com>
2023-05-12 12:25:02 -0700

    change component to be arrow func, and remove inline styling

--- podium-helper/src/components/NoResults.js
+++ podium-helper/src/components/NoResults.js
@@ -1,26 +1,22 @@
-import { Card, CardContent, Typography, CardMedia } from "@mui/material"
-import missing from '../images/Missing_car.png';
+import { Card, CardContent, Typography, CardMedia } from "@mui/material";
+import missing from "../images/Missing_car.png";
 
-export default function NoResults() {
-
+const NoResults = () => {
   return (
     <div className="No-Result">
-      <Card sx={{ minWidth: 600 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={missing}
-        alt="No Vehicles Found"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h2" component="div" color="error">
-          No Results Found
-        </Typography>
-        <Typography variant="body2" color="error">
-          We couldn't find what you were searching for. Try searching again
-        </Typography>
-      </CardContent>
-    </Card>
+      <Card>
+        <CardMedia component="img" image={missing} alt="No Vehicles Found" />
+        <CardContent>
+          <Typography gutterBottom variant="h2" component="div" color="error">
+            No Results Found
+          </Typography>
+          <Typography variant="body2" color="error">
+            We couldn't find what you were searching for. Try searching again
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
-  )
-}
+  );
+};
+
+export default NoResults;
