e1a11746a155b5b19c7f79616c8c32b984ba50f1
Brendan Woo <woo.brendan@gmail.com>
2023-08-21 16:39:11 -0500

    change server connection tto production

--- express_mongo/config/config.ts
+++ express_mongo/config/config.ts
@@ -9,14 +9,14 @@ const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.rm
 const MONGO_TEST = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.rmiuql1.mongodb.net/sro23_Test`;
 
 const SERVER_PORT = process.env.SERVER_PORT
-  ? Number(process.env.SERVER_PORT)
-  : 2020;
+    ? Number(process.env.SERVER_PORT)
+    : 2020;
 
 export const config = {
-  mongo: {
-    url: MONGO_TEST,
-  },
-  server: {
-    port: SERVER_PORT,
-  },
+    mongo: {
+        url: MONGO_URL,
+    },
+    server: {
+        port: SERVER_PORT,
+    },
 };
