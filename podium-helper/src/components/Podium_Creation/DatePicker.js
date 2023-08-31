befa9ddfd0e60f170b063af491e683d4e70df20b
Brendan Woo <woo.brendan@gmail.com>
2023-08-31 11:04:21 -0400

    rerformat eslint

--- podium-helper/src/components/Podium_Creation/DatePicker.js
+++ podium-helper/src/components/Podium_Creation/DatePicker.js
@@ -5,30 +5,30 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
 const DatePicker = ({ getValue, today }) => {
-  const [date, setDate] = useState(today);
+    const [date, setDate] = useState(today);
 
-  const handleChange = (e, newValue) => {
-    const newDate = newValue.toISOString().split("T")[0];
-    setDate(newDate);
-    getValue(e);
-  };
+    const handleChange = (e, newValue) => {
+        const newDate = newValue.toISOString().split("T")[0];
+        setDate(newDate);
+        getValue(e);
+    };
 
-  return (
-    <div id="date-picker">
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <Stack spacing={3}>
-          <DesktopDatePicker
-            label="Date"
-            inputFormat="yyyy-MM-dd"
-            name="date"
-            value={date}
-            onChange={(e) => handleChange(e)}
-            renderInput={(params) => <TextField {...params} />}
-          />
-        </Stack>
-      </LocalizationProvider>
-    </div>
-  );
+    return (
+        <div id="date-picker">
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <Stack spacing={3}>
+                    <DesktopDatePicker
+                        label="Date"
+                        inputFormat="yyyy-MM-dd"
+                        name="date"
+                        value={date}
+                        onChange={(e) => handleChange(e)}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </Stack>
+            </LocalizationProvider>
+        </div>
+    );
 };
 
 export default DatePicker;
