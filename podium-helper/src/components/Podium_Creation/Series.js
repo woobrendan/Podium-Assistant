ac541eaa34a73e3b239137dcf4af37a2b477f4f1
Brendan Woo <woo.brendan@gmail.com>
2023-09-26 20:32:30 -0400

    remove box surrounding form control

--- podium-helper/src/components/Podium_Creation/Series.js
+++ podium-helper/src/components/Podium_Creation/Series.js
@@ -4,48 +4,57 @@ import axios from "axios";
 import { sortSeriesNames } from "../../functions/sortFuncs";
 
 const Series = ({ getValue, comp }) => {
-  const [seriesName, setSeriesName] = useState("");
-  const [series, setSeries] = useState([]);
+    const [seriesName, setSeriesName] = useState("");
+    const [series, setSeries] = useState([]);
 
-  useEffect(() => {
-    const getAndSetSeries = async () => {
-      try {
-        const seriesList = await axios.get("http://localhost:2020/api/series");
-        const sortedSeries = seriesList.data.series.sort(sortSeriesNames);
-        const list =
-          comp === "entryManager" ? ["All", ...sortedSeries] : sortedSeries;
-        setSeries(list);
-      } catch (err) {
-        console.error(err);
-      }
+    useEffect(() => {
+        const getAndSetSeries = async () => {
+            try {
+                const seriesList = await axios.get(
+                    "http://localhost:2020/api/series",
+                );
+                const sortedSeries =
+                    seriesList.data.series.sort(sortSeriesNames);
+                const list =
+                    comp === "entryManager"
+                        ? ["All", ...sortedSeries]
+                        : sortedSeries;
+                setSeries(list);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        getAndSetSeries();
+    }, []);
+
+    const handleChange = (event) => {
+        setSeriesName(event.target.value);
+        getValue(event);
     };
-    getAndSetSeries();
-  }, []);
 
-  const handleChange = (event) => {
-    setSeriesName(event.target.value);
-    getValue(event);
-  };
-
-  return (
-    <Box data-testid="series_dropdown">
-      <FormControl className="series_dropdown">
-        <InputLabel>Series</InputLabel>
-        <Select
-          name="series"
-          label="Series"
-          value={seriesName}
-          onChange={(e) => handleChange(e)}
-        >
-          {series.map((serie, index) => (
-            <MenuItem key={index} value={serie} data-testid={serie.name}>
-              {serie === "All" ? "All" : serie.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </Box>
-  );
+    return (
+        // <Box data-testid="series_dropdown">
+        <FormControl className="series_dropdown">
+            <InputLabel>Series</InputLabel>
+            <Select
+                name="series"
+                label="Series"
+                value={seriesName}
+                onChange={(e) => handleChange(e)}
+            >
+                {series.map((serie, index) => (
+                    <MenuItem
+                        key={index}
+                        value={serie}
+                        data-testid={serie.name}
+                    >
+                        {serie === "All" ? "All" : serie.name}
+                    </MenuItem>
+                ))}
+            </Select>
+        </FormControl>
+        // </Box>
+    );
 };
 
 export default Series;
