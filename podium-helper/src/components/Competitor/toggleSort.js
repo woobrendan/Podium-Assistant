08ab8c38e4180c45229d9b98269d5eaa1e801dd9
Brendan Woo <woo.brendan@gmail.com>
2023-10-03 14:43:27 -0400

    inside getSortOption clean up return value instead of storing memory. mve mapped sorted options directly inside the component return

--- podium-helper/src/components/Competitor/toggleSort.js
+++ podium-helper/src/components/Competitor/toggleSort.js
@@ -2,49 +2,47 @@ import { FormControl, InputLabel, Select, Box, MenuItem } from "@mui/material";
 import { useState } from "react";
 
 const ToggleSort = ({ getOption, component }) => {
-  const [sortOption, setSortOption] = useState("");
+    const [sortOption, setSortOption] = useState("");
 
-  const getSortOption = (component) => {
-    const competitorSortOptions = [
-      "Number",
-      "Manufacturer",
-      "Vehicle Type",
-      "Class",
-    ];
-    const resultSortOptions = ["Event", "Series"];
-    const option =
-      component === "competitor" ? competitorSortOptions : resultSortOptions;
-    return option;
-  };
+    const getSortOption = (component) => {
+        const competitorSortOptions = [
+            "Number",
+            "Manufacturer",
+            "Vehicle Type",
+            "Class",
+        ];
+        const resultSortOptions = ["Event", "Series"];
+        return component === "competitor"
+            ? competitorSortOptions
+            : resultSortOptions;
+    };
 
-  const handleToggle = (event) => {
-    setSortOption(event.target.value);
-    getOption(event.target.value);
-  };
+    const handleToggle = (event) => {
+        setSortOption(event.target.value);
+        getOption(event.target.value);
+    };
 
-  const mappedOptions = getSortOption(component).map((option) => (
-    <MenuItem key={option} value={option}>
-      {option}
-    </MenuItem>
-  ));
-
-  return (
-    <Box className="sort-option-selector">
-      <FormControl color="error">
-        <InputLabel>
-          {component === "competitor" ? "Sort By" : "Filter"}
-        </InputLabel>
-        <Select
-          name="sort-by"
-          label="Sort By"
-          value={sortOption}
-          onChange={handleToggle}
-        >
-          {mappedOptions}
-        </Select>
-      </FormControl>
-    </Box>
-  );
+    return (
+        <Box className="sort-option-selector">
+            <FormControl color="error">
+                <InputLabel>
+                    {component === "competitor" ? "Sort By" : "Filter"}
+                </InputLabel>
+                <Select
+                    name="sort-by"
+                    label="Sort By"
+                    value={sortOption}
+                    onChange={handleToggle}
+                >
+                    {getSortOption(component).map((option) => (
+                        <MenuItem key={option} value={option}>
+                            {option}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </Box>
+    );
 };
 
 export default ToggleSort;
