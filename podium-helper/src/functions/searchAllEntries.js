49a7a1c5504fc12d23d99f33d4c94931f490f402
Brendan Woo <woo.brendan@gmail.com>
2023-10-04 17:56:18 -0400

    remove commented out if blocks. add comments for future readability

--- podium-helper/src/functions/searchAllEntries.js
+++ podium-helper/src/functions/searchAllEntries.js
@@ -23,6 +23,8 @@ const searchAllEntries = (entries, searchVal) => {
         const driversArr = [driver1, driver2, driver3].filter(Boolean);
 
         return (
+            //** if any condition returns true, the current iteration of entry will be return to the filter arr */
+            // for each driver in driversArr check if the search val is in their name etc
             driversArr.some((driver) => checkField(driver.name)) ||
             driversArr.some((driver) => checkField(driver.rating)) ||
             driversArr.some((driver) => checkField(driver.nationality)) ||
@@ -32,44 +34,7 @@ const searchAllEntries = (entries, searchVal) => {
             checkField(number) ||
             checkField(classification)
         );
-
-        // if (driver1.name.toLowerCase().includes(lowerVal)) {
-        //     return val;
-        // }
-        // if (vehicle.toLowerCase().includes(lowerVal)) {
-        //     return val;
-        // }
-        // if (team.toLowerCase().includes(lowerVal)) {
-        //     return val;
-        // }
-        // if (driver1.rating.toLowerCase().includes(lowerVal)) {
-        //     return val;
-        // }
-
-        // if (driver1.nationality.toLowerCase().includes(lowerVal)) {
-        //     return val;
-        // }
-        // if (series.toLowerCase().includes(lowerVal)) {
-        //     return val;
-        // }
-        // if (number.includes(searchVal)) {
-        //     return val;
-        // }
-        // if (classification.includes(searchVal)) {
-        //     return val;
-        // }
-
-        // if (driver2?.name.toLowerCase().includes(lowerVal)) {
-        //     return val;
-        // }
-        // if (driver2?.rating.toLowerCase().includes(lowerVal)) {
-        //     return val;
-        // }
-        // if (driver2?.nationality.toLowerCase().includes(lowerVal)) {
-        //     return val;
-        // }
     });
-    // return filtered;
 };
 
 export default searchAllEntries;
