c9dcf59fbe9d278905393121e29e306849762ba1
Brendan Woo <woo.brendan@gmail.com>
2023-10-04 17:29:17 -0400

    create variable lowerVal to that takes searchval and lowers and trims. then replace all instances of searchval.tolowercase

--- podium-helper/src/functions/searchAllEntries.js
+++ podium-helper/src/functions/searchAllEntries.js
@@ -1,48 +1,58 @@
 const searchAllEntries = (entries, searchVal) => {
-  if (!searchVal.trim()) {
-    return entries;
-  }
-  const filtered = entries.filter((val) => {
-    const { driver1, driver2, series, number, team, vehicle, classification } =
-      val;
+    const lowerVal = searchVal.toLowerCase().trim();
 
-    if (driver1.name.toLowerCase().includes(searchVal.toLowerCase())) {
-      return val;
-    }
-    if (vehicle.toLowerCase().includes(searchVal.toLowerCase())) {
-      return val;
-    }
-    if (team.toLowerCase().includes(searchVal.toLowerCase())) {
-      return val;
-    }
-    if (driver1.rating.toLowerCase().includes(searchVal.toLowerCase())) {
-      return val;
+    if (!searchVal) {
+        return entries;
     }
 
-    if (driver1.nationality.toLowerCase().includes(searchVal.toLowerCase())) {
-      return val;
-    }
-    if (series.toLowerCase().includes(searchVal.toLowerCase())) {
-      return val;
-    }
-    if (number.includes(searchVal)) {
-      return val;
-    }
-    if (classification.includes(searchVal)) {
-      return val;
-    }
+    const filtered = entries.filter((val) => {
+        const {
+            driver1,
+            driver2,
+            series,
+            number,
+            team,
+            vehicle,
+            classification,
+        } = val;
 
-    if (driver2?.name.toLowerCase().includes(searchVal.toLowerCase())) {
-      return val;
-    }
-    if (driver2?.rating.toLowerCase().includes(searchVal.toLowerCase())) {
-      return val;
-    }
-    if (driver2?.nationality.toLowerCase().includes(searchVal.toLowerCase())) {
-      return val;
-    }
-  });
-  return filtered;
+        if (driver1.name.toLowerCase().includes(lowerVal)) {
+            return val;
+        }
+        if (vehicle.toLowerCase().includes(lowerVal)) {
+            return val;
+        }
+        if (team.toLowerCase().includes(lowerVal)) {
+            return val;
+        }
+        if (driver1.rating.toLowerCase().includes(lowerVal)) {
+            return val;
+        }
+
+        if (driver1.nationality.toLowerCase().includes(lowerVal)) {
+            return val;
+        }
+        if (series.toLowerCase().includes(lowerVal)) {
+            return val;
+        }
+        if (number.includes(searchVal)) {
+            return val;
+        }
+        if (classification.includes(searchVal)) {
+            return val;
+        }
+
+        if (driver2?.name.toLowerCase().includes(lowerVal)) {
+            return val;
+        }
+        if (driver2?.rating.toLowerCase().includes(lowerVal)) {
+            return val;
+        }
+        if (driver2?.nationality.toLowerCase().includes(lowerVal)) {
+            return val;
+        }
+    });
+    return filtered;
 };
 
 export default searchAllEntries;
