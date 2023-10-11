4c1baa55f3a8d2012bfb80216fc3af5e4074f2b5
Brendan Woo <woo.brendan@gmail.com>
2023-10-11 13:43:19 -0700

    remove getEventById route handler and add year handler

--- express_mongo/routes/eventRoutes.ts
+++ express_mongo/routes/eventRoutes.ts
@@ -4,7 +4,8 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", controller.getAllEvents);
-router.get("/:eventId", controller.getEventById);
+// router.get("/:eventId", controller.getEventById);
+router.get("/:year", controller.getEventByYear);
 router.patch("/:eventId", controller.updateEvent);
 
 export = router;
