c526780089f0b394bdb585269e01afe90e8ce144
Brendan Woo <woo.brendan@gmail.com>
2023-12-19 16:29:00 -0500

    remove func for fetchapientries

--- podium-helper/src/store/entryActions.js
+++ podium-helper/src/store/entryActions.js
@@ -12,29 +12,6 @@ export const fetchEntry = () => {
     };
 };
 
-export const fetchApiEntries = async () => {
-    const formId = process.env.FORM_ID;
-    const token = process.env.TKSPICE;
-    console.log("token", token);
-    try {
-        const url = "https://api.webconnex.com/v2/public/search/tickets";
-        const entries = await axios.get(url, {
-            params: {
-                product: "ticketspice.com",
-                formId: formId,
-                port: "443",
-            },
-            headers: {
-                apiKey: token,
-                "User-Agent": `node v14.17.6`,
-            },
-        });
-        console.log("my val-----", entries);
-    } catch (err) {
-        console.log("Error fetching api", err);
-    }
-};
-
 export const updateEntry = async (entry) => {
     try {
         axios.patch(`http://localhost:2020/entries/${entry._id}`, entry);
