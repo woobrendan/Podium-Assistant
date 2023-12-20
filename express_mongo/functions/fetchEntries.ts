a0d8d129d77d6a69077033e52607404a0b9ec058
Brendan Woo <woo.brendan@gmail.com>
2023-12-20 11:30:16 -0500

    add in formid param and get form id from env. add in commments to get proper data

--- express_mongo/functions/fetchEntries.ts
+++ express_mongo/functions/fetchEntries.ts
@@ -6,12 +6,14 @@ dotenv.config();
 export const fetchApiEntries = async () => {
     try {
         const token = process.env.TKSPICE;
+        const form = process.env.FORM_ID;
         const url = "https://api.webconnex.com/v2/public/search/tickets";
 
         const entries = await axios.get(url, {
             params: {
                 product: "ticketspice.com",
                 port: "443",
+                formId: form,
             },
 
             headers: {
@@ -19,7 +21,9 @@ export const fetchApiEntries = async () => {
             },
         });
 
-        console.log("my val-----", entries.data);
+        console.log("my val-----", entries.data.data[1]);
+        //filter out lumirank first then pass to converter
+        // take in data and process same as used in entrant file
     } catch (err) {
         console.log("Error fetching api", err);
     }
