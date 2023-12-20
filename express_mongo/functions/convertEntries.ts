a0a414d7381f70660cfd31d33982e26d36f3156a
Brendan Woo <woo.brendan@gmail.com>
2023-12-20 15:18:33 -0500

    complete more logic for convert entry, declare newEntry and loop through labels and field

--- express_mongo/functions/convertEntries.ts
+++ express_mongo/functions/convertEntries.ts
@@ -1,3 +1,35 @@
-import { ApiEntryInterface } from "../models/models";
+import { ApiEntryInterface, FieldData } from "../models/models";
 
-const convertEntry = (entry: ApiEntryInterface) => {};
+const labels = [
+    "Car Series", // GT4, GTWCA
+    "Championship / Class", // pro-am
+    "Registered Car #",
+    "team",
+    "driver1",
+    "driver1category",
+    "driver1nationality",
+    "driver2",
+    "driver2category",
+    "driver2nationality",
+    "car",
+    "Team Sponsors - Please seperate each Sponsor with a comma",
+];
+
+const convertEntry = (entry: ApiEntryInterface) => {
+    const newEntry = {
+        id: entry.id,
+        event: entry.eventLabel,
+        created: entry.dateCreated,
+    };
+
+    for (const label of labels) {
+        for (const field of entry.fieldData) {
+            if (
+                label === "Championship / Class" &&
+                field.path.includes("carType.")
+            ) {
+                //newEntry.class = conver
+            }
+        }
+    }
+};
