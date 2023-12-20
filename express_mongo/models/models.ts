5d370b0139e1557029ff302180d3cf22e38f58f4
Brendan Woo <woo.brendan@gmail.com>
2023-12-20 11:29:49 -0500

    create interface for fieldData and apientries

--- express_mongo/models/models.ts
+++ express_mongo/models/models.ts
@@ -1,18 +1,52 @@
 export interface DriverInfo {
-  name: string;
-  rating: string;
-  nationality: string;
+    name: string;
+    rating: string;
+    nationality: string;
 }
 
 export interface EntryInterface {
-  team: string;
-  vehicle: string;
-  series: string;
-  number: string;
-  driver1: DriverInfo;
-  driver2?: DriverInfo;
-  driver3?: DriverInfo;
-  classification: string;
-  carImage: string;
-  year: number;
+    team: string;
+    vehicle: string;
+    series: string;
+    number: string;
+    driver1: DriverInfo;
+    driver2?: DriverInfo;
+    driver3?: DriverInfo;
+    classification: string;
+    carImage: string;
+    year: number;
+}
+
+interface FieldData {
+    amount?: string;
+    label: string;
+    path: string;
+    value: string | boolean;
+}
+
+export interface ApiEntryInterface {
+    id: number;
+    displayId: string;
+    formId: number;
+    formName: string;
+    formAccRef: string;
+    orderCustomerId: number;
+    customerId: number;
+    orderId: number;
+    orderDisplayId: string;
+    orderNumber: string;
+    orderEmail: string;
+    levelLabel: string;
+    levelKey: string;
+    amount: string;
+    fee: string;
+    total: string;
+    currency: string;
+    status: string;
+    fieldData: FieldData[];
+    eventLabel: string;
+    metadata: null | any;
+    sourceType: string;
+    dateCreated: Date;
+    dateUpdated: Date;
 }
