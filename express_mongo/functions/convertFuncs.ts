8f006f7752000cd5e1f6c0c447a6eb074258b3c7
Brendan Woo <woo.brendan@gmail.com>
2023-12-21 14:30:13 -0500

    add in vehicles list, and create func getManuf that manipulates string to return just manuf

--- express_mongo/functions/convertFuncs.ts
+++ express_mongo/functions/convertFuncs.ts
@@ -15,6 +15,43 @@ const labels = [
     "Team Sponsors - Please seperate each Sponsor with a comma",
 ];
 
+//to be updated later as vehicles come through
+const vehicles: { [key: string]: string } = {
+    acuraNsxGt3Evo22: "Acura NSX GT3 EVO22",
+    astonMartinVantageAmrGt3: "Aston Martin Vantage AMR GT3",
+    audiR8LmsGt2: "Audi R8 LMS GT2",
+    bmwM4Gt3: "BMW M4 GT3",
+    "chevroletCorvetteZ06Gt3.r": "Chevrolet Corvette Z06 GT3.R",
+    ferrari296Gt3: "Ferrari 296 GT3",
+    fordMustangGt3: "Ford Mustang GT3",
+    "mercedes-amgGt3": "Mercedes-AMG GT3",
+    "ktmX-bowGt2": "KTM X-Bow GT2",
+    nissanZGt4: "Nissan Z GT4",
+    bmwM4Gt4: "BMW M4 GT4",
+    fordMustangGt4: "Ford Mustang GT4",
+    mclarenArturaGt4: "McLaren Artura GT4",
+    "mercedes-amgGt4": "Mercedes-AMG GT4",
+    porsche718CaymanGt4RsClubsport: "Porsche 718 Cayman GT4 RS Clubsport",
+    toyotaGazooRacingGrSupraGt4Evo: "Toyota Gazoo Racing GR Supra GT4 EVO",
+    acuraIntegraTypeS: "Acura Integra Type S TCX",
+    subaruBrz: "Subaru BRZ",
+    audiR8LmsGt4: "Audi R8 LMS GT4",
+    audiR8Lms: "Audi R8 LMS",
+};
+
+const getManuf = (vehicle: string) => {
+    const car = vehicles[vehicle] || `${vehicle} not in vehicle list`;
+    const carArr = car.split(" ");
+
+    if (carArr[0] === "Aston") {
+        return `${carArr[0]} ${carArr[1]}`;
+    } else if (carArr[0] === "Mercedes-AMG") {
+        return carArr[0].split("-")[0];
+    } else {
+        return carArr[0];
+    }
+};
+
 const convertClassif = (classif: string): string | undefined => {
     const classList: Record<string, string> = {
         sro3: "SRO3",
