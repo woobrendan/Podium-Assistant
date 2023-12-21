38e883e898149644b49c8aa229e14bdb6b7f75e8
Brendan Woo <woo.brendan@gmail.com>
2023-12-21 13:46:31 -0500

    create file to house functions for converting entries from api, move over labels array and classif func

--- express_mongo/functions/convertEntries.ts
+++ express_mongo/functions/convertEntries.ts
@@ -1,36 +1,5 @@
 import { ApiEntryInterface, FieldData } from "../models/models";
-
-const labels = [
-    "Car Series", // GT4, GTWCA
-    "Championship / Class", // pro-am
-    "Registered Car #",
-    "team",
-    "driver1",
-    "driver1category",
-    "driver1nationality",
-    "driver2",
-    "driver2category",
-    "driver2nationality",
-    "car",
-    "Team Sponsors - Please seperate each Sponsor with a comma",
-];
-
-const convertClassif = (classif: string): string | undefined => {
-    const classList: Record<string, string> = {
-        sro3: "SRO3",
-        gt4: "GT4",
-        gt2: "GT2",
-        proAm: "Pro-Am",
-        proPro: "Pro",
-        am: "Am",
-        silver: "Silver",
-        TCX: "TCX",
-        TC: "TC",
-        TCA: "TCA",
-    };
-
-    return classList[classif];
-};
+import { labels, convertClassif } from "./convertFuncs";
 
 const getFieldPathVal = (field: FieldData) => {
     return field.path.split(".")[1];
