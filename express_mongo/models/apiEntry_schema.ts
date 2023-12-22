847e2e77e9b62c1ca752fc9849f0746b748de6d1
Brendan Woo <woo.brendan@gmail.com>
2023-12-22 14:16:31 -0500

    complete schema adding details for apiEntrySchema and export mongoose model

--- express_mongo/models/apiEntry_schema.ts
+++ express_mongo/models/apiEntry_schema.ts
@@ -1,2 +1,28 @@
 import mongoose, { Document, Schema } from "mongoose";
-import { ApiEntryInterface } from "./models";
+import { ConvertedApiEntry } from "./models";
+
+export interface ApiEntryModel extends ConvertedApiEntry, Document {}
+
+const apiEntrySchema: Schema = new Schema({
+    id: { type: Number, required: true },
+    event: { type: String, required: true },
+    created: { type: String, required: true },
+    series: { type: String, required: true },
+    class: { type: String, required: true },
+    number: { type: String, required: true },
+    team: { type: String, required: true },
+    driver1firstName: { type: String, required: true },
+    driver1lastName: { type: String, required: true },
+    driver1category: { type: String },
+    driver1nationality: { type: String },
+    driver2firstName: { type: String },
+    driver2lastName: { type: String },
+    driver2category: { type: String },
+    driver2nationality: { type: String },
+    car: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    sponsors: { type: String, required: true },
+});
+
+export default mongoose.model<ApiEntryModel>("ApiEntries", apiEntrySchema);
+export { apiEntrySchema };
