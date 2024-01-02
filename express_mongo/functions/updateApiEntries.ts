ec5e58e58c3d0eb776f17909aaedbad0e1029da1
Brendan Woo <woo.brendan@gmail.com>
2024-01-02 17:29:19 -0500

    create function updateById that checks the db for each entry, then console.log

--- express_mongo/functions/updateApiEntries.ts
+++ express_mongo/functions/updateApiEntries.ts
@@ -1,7 +1,25 @@
 import { fetchApiEntries } from "./fetchEntries";
+import { ConvertedApiEntry } from "../models/models";
+import ApiEntries from "../models/apiEntry_schema";
 
 const updateApiEntries = async () => {
-    const entries = await fetchApiEntries();
+    const entries: ConvertedApiEntry[] = await fetchApiEntries();
+
+    for (const entry of entries) {
+        updateById(entry);
+    }
 };
 
 export default updateApiEntries;
+
+// check if id exists, if it does check if needs updating. if no id, create new entry
+const updateById = async (entry: ConvertedApiEntry) => {
+    const entryId = entry.tk_id;
+
+    try {
+        const db_entry = await ApiEntries.findById(entryId);
+        db_entry ? console.log("found") : console.log("not found");
+    } catch (error) {
+        console.log("error");
+    }
+};
