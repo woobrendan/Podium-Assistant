31175b7624a5b6f8dae952bf9874876d5561c131
Brendan Woo <woo.brendan@gmail.com>
2024-01-03 17:47:42 -0500

    create function compare objects to compare keys and values

--- express_mongo/functions/helperFunc.ts
+++ express_mongo/functions/helperFunc.ts
@@ -1,28 +1,37 @@
 import { ResultInterface, HardChargerInterface } from "../models/result_models";
 
 const resultBuilder = (result: ResultInterface) => {
-  const { firstPlace, secondPlace, thirdPlace } = result;
-  return {
-    ...result,
-    firstPlace: { ...firstPlace },
-    ...(secondPlace ? { secondPlace: { ...secondPlace } } : {}),
-    ...(thirdPlace ? { thirdPlace: { ...thirdPlace } } : {}),
-  };
+    const { firstPlace, secondPlace, thirdPlace } = result;
+    return {
+        ...result,
+        firstPlace: { ...firstPlace },
+        ...(secondPlace ? { secondPlace: { ...secondPlace } } : {}),
+        ...(thirdPlace ? { thirdPlace: { ...thirdPlace } } : {}),
+    };
 };
 
 const hardChargeResult = (result: HardChargerInterface) => {
-  const { entry } = result;
-  return {
-    ...result,
-    entry: {
-      ...entry,
-      driver1: {
-        ...entry.driver1,
-      },
-      ...(entry.driver2 ? { driver2: { ...entry.driver2 } } : {}),
-      ...(entry.driver3 ? { driver3: { ...entry.driver3 } } : {}),
-    },
-  };
+    const { entry } = result;
+    return {
+        ...result,
+        entry: {
+            ...entry,
+            driver1: {
+                ...entry.driver1,
+            },
+            ...(entry.driver2 ? { driver2: { ...entry.driver2 } } : {}),
+            ...(entry.driver3 ? { driver3: { ...entry.driver3 } } : {}),
+        },
+    };
 };
 
-export { resultBuilder, hardChargeResult };
+const compareObjects = (db_entry: any, entry2: any) => {
+    for (const key in entry2) {
+        if (db_entry[key] !== entry2[key]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+export { resultBuilder, hardChargeResult, compareObjects };
