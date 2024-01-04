6a6c194741cc2f6226c3bdcc12d772dabe970630
Brendan Woo <woo.brendan@gmail.com>
2024-01-04 17:14:08 -0500

    add export for contoller

--- express_mongo/controllers/api_entry_controller.ts
+++ express_mongo/controllers/api_entry_controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import mongoose from "mongoose";
 import ApiEntries from "../models/apiEntry_schema";
 
@@ -6,4 +6,8 @@ const getAllEntries = async (req: Request, res: Response) => {
     return ApiEntries.find()
         .then((entry) => res.status(201).json({ entry }))
         .catch((error) => res.status(500).json({ error }));
+};
+
+export default {
+    getAllEntries,
 };
