0e8d57603f3dcc7c3d944ebb6fc9116f7ae5ffa4
Brendan Woo <woo.brendan@gmail.com>
2024-01-04 17:57:53 -0500

    move updateentry inside the for loop of updateapientries func

--- express_mongo/functions/updateApiEntries.ts
+++ express_mongo/functions/updateApiEntries.ts
@@ -8,50 +8,46 @@ const updateApiEntries = async () => {
     const entries: ConvertedApiEntry[] = await fetchApiEntries();
 
     for (const entry of entries) {
-        updateById(entry);
+        // check if id exists, if it does check if needs updating. if no id, create new entry
+        const entryId = entry.tk_id;
+
+        try {
+            const db_entry = await ApiEntries.findOne({ tk_id: entryId });
+            if (db_entry) {
+                const db_test = db_entry.toObject();
+
+                const dbNoIdEntry = {
+                    ...db_test,
+                    _id: undefined,
+                };
+
+                const entryNoId = {
+                    _id: undefined,
+                    ...entry,
+                    __v: 0,
+                };
+
+                if (!compareObjects(dbNoIdEntry, entryNoId)) {
+                    db_entry.set(entry);
+                    db_entry
+                        .save()
+                        .then((saved) => {
+                            console.log(`Successfully updated: ${saved.team} ${saved.number}`);
+                        })
+                        .catch((err) => {
+                            console.log(`Failed to update: ${db_entry.team} ${db_entry.number}`);
+                        });
+                }
+            } else {
+                await addEntry(entry);
+            }
+        } catch (error) {
+            console.log("Error updating by ID: ", error);
+        }
     }
 };
 
 export default updateApiEntries;
-
-// check if id exists, if it does check if needs updating. if no id, create new entry
-const updateById = async (entry: ConvertedApiEntry) => {
-    const entryId = entry.tk_id;
-
-    try {
-        const db_entry = await ApiEntries.findOne({ tk_id: entryId });
-        if (db_entry) {
-            const db_test = db_entry.toObject();
-
-            const dbNoIdEntry = {
-                ...db_test,
-                _id: undefined,
-            };
-
-            const entryNoId = {
-                _id: undefined,
-                ...entry,
-                __v: 0,
-            };
-
-            if (!compareObjects(dbNoIdEntry, entryNoId)) {
-                db_entry.set(entry);
-                db_entry
-                    .save()
-                    .then((saved) => {
-                        console.log(`Successfully updated: ${saved.team} ${saved.number}`);
-                    })
-                    .catch((err) => {
-                        console.log(`Failed to update: ${db_entry.team} ${db_entry.number}`);
-                    });
-            }
-        } else {
-            await addEntry(entry);
-        }
-    } catch (error) {
-        console.log("Error updating by ID: ", error);
-    }
-};
 
 const addEntry = async (entry: ConvertedApiEntry) => {
     const newEntry = new ApiEntries({
