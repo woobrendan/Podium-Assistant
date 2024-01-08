dd1801706218c0dd9e726b2e63390a7871ee567d
Brendan Woo <woo.brendan@gmail.com>
2024-01-08 14:35:13 -0500

    create route handler for event entries

--- express_mongo/routes/apiEntryRoutes.ts
+++ express_mongo/routes/apiEntryRoutes.ts
@@ -3,8 +3,11 @@ import controller from "../controllers/api_entry_controller";
 
 const router = express.Router();
 
+// api/entries ....
+
 //router.post("/", controller.createEntry);
 router.get("/", controller.getAllEntries);
+router.get("/events", controller.getEntriesByEvent);
 router.get("/:entryId", controller.getEntryById);
 //router.patch("/:entryId", controller.updateEntry);
 //router.delete("/:entryId", controller.deleteEntry);
