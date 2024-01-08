97f7b7102f217fc96c2c2a8623f8f162b18d387e
Brendan Woo <woo.brendan@gmail.com>
2024-01-08 14:35:29 -0500

    create controller for get entries by event

--- express_mongo/controllers/api_entry_controller.ts
+++ express_mongo/controllers/api_entry_controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import mongoose from "mongoose";
 import ApiEntries from "../models/apiEntry_schema";
+import { entriesByEvent } from "../functions/helperFunc";
 
 const getAllEntries = async (req: Request, res: Response) => {
     return ApiEntries.find()
@@ -19,10 +20,25 @@ const getEntryById = async (req: Request, res: Response) => {
     }
 };
 
+const getEntriesByEvent = async (req: Request, res: Response) => {
+    try {
+        const entries = await ApiEntries.find();
+        if (entries) {
+            const eventEntries = entriesByEvent(entries);
+            return res.status(200).json({ eventEntries });
+        } else {
+            res.status(400).json({ message: "Entrie Not Found" });
+        }
+    } catch (error) {
+        return res.status(500).json({ error });
+    }
+};
+
 //get entries sorted by event
 // get entries per event
 
 export default {
     getAllEntries,
     getEntryById,
+    getEntriesByEvent,
 };
