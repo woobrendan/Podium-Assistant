8e26a78484364f74f05010c668f4d154a29bbcbf
Brendan Woo <woo.brendan@gmail.com>
2024-01-10 17:24:49 -0500

    remove competito params, remove getSortingOption func and instead hardcode the list of options to sort by

--- podium-helper/src/components/Competitor/toggleSort.js
+++ podium-helper/src/components/Competitor/toggleSort.js
@@ -1,21 +1,8 @@
 import { FormControl, InputLabel, Select, Box, MenuItem } from "@mui/material";
 import { useState } from "react";
 
-const ToggleSort = ({ getOption, component }) => {
+const ToggleSort = ({ getOption }) => {
     const [sortOption, setSortOption] = useState("");
-
-    const getSortOption = (component) => {
-        const competitorSortOptions = [
-            "Number",
-            "Manufacturer",
-            "Vehicle Type",
-            "Class",
-        ];
-        const resultSortOptions = ["Event", "Series"];
-        return component === "competitor"
-            ? competitorSortOptions
-            : resultSortOptions;
-    };
 
     const handleToggle = (event) => {
         setSortOption(event.target.value);
@@ -25,16 +12,9 @@ const ToggleSort = ({ getOption, component }) => {
     return (
         <Box className="sort-option-selector">
             <FormControl color="error">
-                <InputLabel>
-                    {component === "competitor" ? "Sort By" : "Filter"}
-                </InputLabel>
-                <Select
-                    name="sort-by"
-                    label="Sort By"
-                    value={sortOption}
-                    onChange={handleToggle}
-                >
-                    {getSortOption(component).map((option) => (
+                <InputLabel>Sort By</InputLabel>
+                <Select name="sort-by" label="Sort By" value={sortOption} onChange={handleToggle}>
+                    {["Number", "Manufacturer", "Vehicle Type", "Class"].map((option) => (
                         <MenuItem key={option} value={option}>
                             {option}
                         </MenuItem>
