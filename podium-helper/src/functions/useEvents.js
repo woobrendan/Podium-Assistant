fe535e70cdc87f474fb9a049f2e23d9a0d6846ce
Brendan Woo <woo.brendan@gmail.com>
2024-01-11 12:08:01 -0500

    update getCurrent event to check if today is before event startdate instead of after

--- podium-helper/src/functions/useEvents.js
+++ podium-helper/src/functions/useEvents.js
@@ -11,9 +11,7 @@ const useEvents = () => {
             try {
                 const date = new Date();
                 const year = date.getFullYear();
-                const eventList = await axios.get(
-                    `http://localhost:2020/api/events/${year}`,
-                );
+                const eventList = await axios.get(`http://localhost:2020/api/events/${year}`);
 
                 // return list of events for the current year
                 const currentYearEvents = eventList.data.events;
@@ -34,8 +32,8 @@ const useEvents = () => {
         for (let i = 0; i < events.length; i++) {
             const eventStartDate = new Date(events[i].startDate);
 
-            // check if today is past start date
-            if (today >= eventStartDate) {
+            // check if today is before or equal to start date
+            if (today <= eventStartDate) {
                 // check if today is not past start of next event, and isnt the last event
                 if (i < events.length - 1) {
                     const nextEvent = new Date(events[i + 1].startDate);
