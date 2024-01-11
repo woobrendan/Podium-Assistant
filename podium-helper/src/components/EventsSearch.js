8530d89b80743993688c8ce8b72678d92b675bea
Brendan Woo <woo.brendan@gmail.com>
2024-01-11 12:32:23 -0500

    add in date and year, and create var yearString, to add year if component is podium

--- podium-helper/src/components/EventsSearch.js
+++ podium-helper/src/components/EventsSearch.js
@@ -4,66 +4,69 @@ import useEvents from "../functions/useEvents";
 import axios from "axios";
 
 const EventSearch = ({ component, getValue }) => {
-  let { events, currentEventName } = useEvents();
-  const [eventName, setEventName] = useState("");
-  const [eventList, setEventList] = useState([]);
+    let { events, currentEventName } = useEvents();
+    const [eventName, setEventName] = useState("");
+    const [eventList, setEventList] = useState([]);
 
-  useEffect(() => {
-    component === "podium" ? setEventName(currentEventName) : setEventName("");
-  }, [currentEventName]);
+    useEffect(() => {
+        component === "podium" ? setEventName(currentEventName) : setEventName("");
+    }, [currentEventName]);
 
-  useEffect(() => {
-    if (component !== "podium") {
-      getEvents();
-    }
-  }, []);
-
-  const getEvents = async () => {
-    try {
-      const data = await axios.get("http://localhost:2020/api/events");
-      const eventArr = data.data.events;
-
-      const uniqueObjects = [];
-      const tempObject = {};
-
-      eventArr.forEach((event) => {
-        const name = event.name;
-        if (!tempObject[name]) {
-          tempObject[name] = true;
-          uniqueObjects.push(event);
+    useEffect(() => {
+        if (component !== "podium") {
+            getEvents();
         }
-      });
+    }, []);
 
-      setEventList(uniqueObjects);
-    } catch (err) {
-      console.log("Error fetching events: ", err);
-    }
-  };
+    const getEvents = async () => {
+        try {
+            const date = new Date();
+            const year = date.getFullYear();
+            let yearString = `http://localhost:2020/api/events`;
 
-  const handleChange = (event) => {
-    setEventName(event.target.value);
-    getValue(event);
-  };
+            if (component === "podium") {
+                yearString += `/${year}`;
+            }
 
-  return (
-    <Box data-testid="event_dropdown">
-      <FormControl className="event_dropdown">
-        <InputLabel>Events</InputLabel>
-        <Select
-          name="event"
-          label="Events"
-          value={eventName}
-          onChange={(e) => handleChange(e)}
-        >
-          {(component === "podium" ? events : eventList).map((event, index) => (
-            <MenuItem key={index} value={event.name} data-testid={event.name}>
-              {event.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </Box>
-  );
+            const data = await axios.get(yearString);
+            const eventArr = data.data.events;
+
+            const uniqueObjects = [];
+            const tempObject = {};
+
+            eventArr.forEach((event) => {
+                const name = event.name;
+                if (!tempObject[name]) {
+                    tempObject[name] = true;
+                    uniqueObjects.push(event);
+                }
+            });
+
+            setEventList(uniqueObjects);
+        } catch (err) {
+            console.log("Error fetching events: ", err);
+        }
+    };
+
+    const handleChange = (event) => {
+        setEventName(event.target.value);
+        getValue(event);
+    };
+
+    return (
+        <Box data-testid="event_dropdown">
+            <FormControl className="event_dropdown">
+                <InputLabel>Events</InputLabel>
+                <Select name="event" label="Events" value={eventName} onChange={(e) => handleChange(e)}>
+                    {(component === "podium" ? events : eventList).map((event, index) => (
+                        <MenuItem key={index} value={event.name} data-testid={event.name}>
+                            {event.name}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        </Box>
+    );
 };
 
 export default EventSearch;
