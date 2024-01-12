0988fb8aac3b7dcf32a3ea59fd7ce97d9b41b2d8
Brendan Woo <woo.brendan@gmail.com>
2024-01-12 17:34:33 -0500

    add in new route for creating entries

--- express_mongo/routes/apiEntryRoutes.ts
+++ express_mongo/routes/apiEntryRoutes.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // api/entries ....
 
-//router.post("/", controller.createEntry);
+router.post("/", controller.createEntry);
 router.get("/", controller.getAllEntries);
 router.get("/events", controller.getEntriesByEvent);
 router.get("/events/:event", controller.getEntryByEvent);
