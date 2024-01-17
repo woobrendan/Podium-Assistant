8bc184e25002d35ef7351efe7e2af439c102f743
Brendan Woo <woo.brendan@gmail.com>
2024-01-17 17:15:47 -0500

    create seperate div not using MUI. create new func handlechange

--- podium-helper/src/components/EntryManager/modal/FiaRating.js
+++ podium-helper/src/components/EntryManager/modal/FiaRating.js
@@ -6,25 +6,39 @@ const FiaRating = (currentRating) => {
 
     const ratingList = ["N/A", "Bronze", "Silver", "Gold", "Platinum"];
 
+    const handleChange = (e) => {
+        setRating(e.target.value);
+    };
+
     return (
-        <Box id="class_dropdown">
-            <FormControl>
-                <InputLabel>Class</InputLabel>
-                <Select
-                    className="form-control"
-                    name="classification"
-                    label="Class"
-                    value={rating}
-                    onChange={(e) => setRating(e.target.value)}
-                >
-                    {ratingList.map((ratingStr, index) => (
-                        <MenuItem key={index} value={ratingStr} data-testid={ratingStr}>
-                            {ratingStr}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-        </Box>
+        //<Box id="class_dropdown">
+        //    <FormControl>
+        //        <InputLabel>Class</InputLabel>
+        //        <Select
+        //            className="form-control"
+        //            name="classification"
+        //            label="Class"
+        //            value={rating}
+        //            onChange={(e) => setRating(e.target.value)}
+        //        >
+        //            {ratingList.map((ratingStr, index) => (
+        //                <MenuItem key={index} value={ratingStr} data-testid={ratingStr}>
+        //                    {ratingStr}
+        //                </MenuItem>
+        //            ))}
+        //        </Select>
+        //    </FormControl>
+        //</Box>
+        <div>
+            <label htmlFor="dropdown">FIA Rating:</label>
+            <select value={rating} onChange={handleChange}>
+                {ratingList.map((ratingStr, index) => (
+                    <option value={ratingStr} key={index}>
+                        ratingStr
+                    </option>
+                ))}
+            </select>
+        </div>
     );
 };
 
