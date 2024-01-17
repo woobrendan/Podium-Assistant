5ddd2ab7873b67dcea91d2b8354ec37594b37017
Brendan Woo <woo.brendan@gmail.com>
2024-01-17 17:34:31 -0500

    remove MUI components, deconstruct props, pass onchange to handleChange, pass name to select and update ratingStr display value to be value not string

--- podium-helper/src/components/EntryManager/modal/FiaRating.js
+++ podium-helper/src/components/EntryManager/modal/FiaRating.js
@@ -1,40 +1,22 @@
 import { useState } from "react";
-import { FormControl, InputLabel, Select, Box, MenuItem } from "@mui/material";
 
-const FiaRating = (currentRating) => {
+const FiaRating = ({ currentRating, name, onChange }) => {
     const [rating, setRating] = useState(currentRating ? currentRating : "");
 
     const ratingList = ["N/A", "Bronze", "Silver", "Gold", "Platinum"];
 
     const handleChange = (e) => {
         setRating(e.target.value);
+        onChange(e);
     };
 
     return (
-        //<Box id="class_dropdown">
-        //    <FormControl>
-        //        <InputLabel>Class</InputLabel>
-        //        <Select
-        //            className="form-control"
-        //            name="classification"
-        //            label="Class"
-        //            value={rating}
-        //            onChange={(e) => setRating(e.target.value)}
-        //        >
-        //            {ratingList.map((ratingStr, index) => (
-        //                <MenuItem key={index} value={ratingStr} data-testid={ratingStr}>
-        //                    {ratingStr}
-        //                </MenuItem>
-        //            ))}
-        //        </Select>
-        //    </FormControl>
-        //</Box>
         <div>
             <label htmlFor="dropdown">FIA Rating:</label>
-            <select value={rating} onChange={handleChange}>
+            <select value={rating} onChange={handleChange} name={name}>
                 {ratingList.map((ratingStr, index) => (
                     <option value={ratingStr} key={index}>
-                        ratingStr
+                        {ratingStr}
                     </option>
                 ))}
             </select>
