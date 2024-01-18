523804e0bbc806a8e55d076b23705d9a0d39c793
Brendan Woo <woo.brendan@gmail.com>
2024-01-18 14:19:44 -0500

    Create func to convert entries from old format to the new. change interfance from EntryInterface to entrymodel. deconstruct values from entry and pass to new format

--- express_mongo/functions/convertFuncs.ts
+++ express_mongo/functions/convertFuncs.ts
@@ -1,4 +1,5 @@
-import { ApiEntryInterface, FieldData } from "../models/models";
+import { EntryModel } from "../models/entry_schema";
+import { FieldData } from "../models/models";
 
 const labels = [
     "Car Series", // GT4, GTWCA
@@ -120,6 +121,23 @@ const convertSeries = (series: string): string => {
     };
 
     return seriesList[series] || `Series Error w/ ${series}`;
+};
+
+const convertOldtoNewEntry = (entry: EntryModel) => {
+    const { _id, team, number, event, series, vehicle, classification, year, created } = entry;
+    // convert from old to new, then try to update again
+    const newFormat = {
+        _id,
+        event,
+        created,
+        series,
+        class: classification,
+        number,
+        team,
+        car: vehicle,
+    };
+
+    //pass update function to update controller
 };
 
 export { labels, convertClassif, getFieldPathVal, getDriverName, getManuf, carTypes, vehicles, convertSeries };
