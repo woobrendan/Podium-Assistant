a7ad1f985931f1786d2552f2aace242e4e5fbf07
Brendan Woo <woo.brendan@gmail.com>
2024-01-19 15:09:39 -0500

    add sponsors to deconstruction and pass to new format:

--- express_mongo/functions/oldEntrytoApiEntry.ts
+++ express_mongo/functions/oldEntrytoApiEntry.ts
@@ -2,7 +2,7 @@ import { EntryModel } from "../models/entry_schema";
 import { getManuf, getDriverInfo } from "../functions/helperFunc";
 
 const convertOldtoNewEntry = (entry: EntryModel) => {
-    const { _id, team, number, event, series, vehicle, classification, year, created } = entry;
+    const { _id, team, number, event, series, vehicle, classification, year, created, sponsors } = entry;
     // convert from old to new, then try to update again
     const newFormat = {
         _id,
@@ -17,6 +17,7 @@ const convertOldtoNewEntry = (entry: EntryModel) => {
         ...getDriverInfo(entry.driver1, 1),
         ...(entry.driver2?.name ? getDriverInfo(entry.driver2, 2) : {}),
         ...(entry.driver3?.name ? getDriverInfo(entry.driver3, 3) : {}),
+        sponsors,
     };
 
     return newFormat;
