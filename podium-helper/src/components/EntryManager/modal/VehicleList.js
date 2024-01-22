5529ccbd7578b4dc9a23d76ec1be5f9079be5052
Brendan Woo <woo.brendan@gmail.com>
2024-01-22 13:29:42 -0500

    import use state, add deconstruct of props seires and veheicles. pass to use state vehicle vehicleName or empty string. create handleChange func. pass vehicle as value to select

--- podium-helper/src/components/EntryManager/modal/VehicleList.js
+++ podium-helper/src/components/EntryManager/modal/VehicleList.js
@@ -1,8 +1,15 @@
-const VehicleList = () => {
+import { useState } from "react";
+import vehicles from "./vehicles";
+
+const VehicleList = ({ series, vehicleName }) => {
+    const [vehicle, setVehicle] = useState(vehicleName ? vehicleName : "");
+    const handleChange = (e) => {
+        setVehicle(e.target.value);
+    };
     return (
         <div id="class_dropdown">
             <label htmlFor="dropdown">Class:</label>
-            <select value={className} onChange={handleChange} name="classification">
+            <select value={vehicle} onChange={handleChange} name="classification">
                 {/*{getSeriesClasses(seriesList).map((classif, index) => (
                     <option value={classif} key={index}>
                         {classif}
