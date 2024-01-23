b16f2f94d5cfae384215de8f9f7508147ffb2982
Brendan Woo <woo.brendan@gmail.com>
2024-01-23 15:46:20 -0500

    remove old input container for vehicle. pass onchange to vehicelist. deconstruct vehiucle and number for other components

--- podium-helper/src/components/EntryManager/modal/EditVehicle.js
+++ podium-helper/src/components/EntryManager/modal/EditVehicle.js
@@ -3,12 +3,12 @@ import InputContainer from "./InputContainer";
 import VehicleList from "./VehicleList";
 
 const EditVehicle = ({ entry, onInputChange, classification, series }) => {
+    const { vehicle, number } = entry;
     return (
         <section className="input_vehicle_container">
             <Classification onInputChange={onInputChange} classification={classification} series={series} />
-            <InputContainer val={entry.number} name="number" onInputChange={onInputChange} label="Number" />
-            {/*<InputContainer val={entry.vehicle} name="vehicle" onInputChange={onInputChange} label="Vehicle" />*/}
-            <VehicleList series={entry.series} vehicleName={entry.vehicle} classif={entry.classification} />
+            <InputContainer val={number} name="number" onInputChange={onInputChange} label="Number" />
+            <VehicleList series={series} vehicleName={vehicle} classif={classification} onChange={onInputChange} />
         </section>
     );
 };
