71c8cb07aeb4b2cf305df56f5edbb915c2fa8682
Brendan Woo <woo.brendan@gmail.com>
2024-01-23 14:43:21 -0500

    create func getVehicleArr and return array of vehicles

--- podium-helper/src/components/EntryManager/modal/VehicleList.js
+++ podium-helper/src/components/EntryManager/modal/VehicleList.js
@@ -1,11 +1,25 @@
 import { useState } from "react";
+import { shortenName } from "../../../functions/helperFunc";
 import vehicles from "./vehicles";
 
-const VehicleList = ({ series, vehicleName }) => {
+const VehicleList = ({ series, vehicleName, classif }) => {
     const [vehicle, setVehicle] = useState(vehicleName ? vehicleName : "");
     const handleChange = (e) => {
         setVehicle(e.target.value);
     };
+
+    //take in long hand of series name
+    const getVehicleArr = (seriesName, className) => {
+        const series = shortenName(seriesName);
+        const vehicleStr = vehicles[series];
+
+        if (series === "GTWCA" || series === "PGT4A") {
+            return vehicleStr;
+        } else {
+            return vehicleStr[classif];
+        }
+    };
+
     return (
         <div id="class_dropdown">
             <label htmlFor="dropdown">Class:</label>
