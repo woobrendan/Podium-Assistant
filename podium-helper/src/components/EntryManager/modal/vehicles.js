61ca40e78b6ade2f11eff1340a364823e3ce66bf
Brendan Woo <woo.brendan@gmail.com>
2024-01-23 13:34:24 -0500

    create tcam key and array value

--- podium-helper/src/components/EntryManager/modal/vehicles.js
+++ podium-helper/src/components/EntryManager/modal/vehicles.js
@@ -93,6 +93,23 @@ const vehicles = {
         "Saleen S1",
         "Toyota Gazoo Racing GR Supra GT4 EVO",
     ],
+    tcam: [
+        "Acura Integra Type S TCX",
+        "BMW M2 CS (Cup)",
+        "Honda Civic Type-R TCX",
+        "Ginetta GTA",
+        "Honda Civic Type-R (2017 - )",
+        "Hyundai Elantra N Cup",
+        "Hyundai Veloster N DCT TC (2021 - )",
+        "Hyundai Veloster N TC (2019 - )",
+        "Mazda 3 TC",
+        "Mini JCW Pro TC",
+        "Honda Civic Si (2017 - )",
+        "Honda Civic Si FE1 (2022 - )",
+        "Hyundai Veloster Turbo TCA",
+        "Mini Cooper JCW (2019 - )",
+        "Subaru BRZ (2022 - )",
+    ],
 };
 
 export default vehicles;
