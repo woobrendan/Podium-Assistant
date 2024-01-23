f65d4cbda5782176bae2a3493654195109800066
Brendan Woo <woo.brendan@gmail.com>
2024-01-23 15:37:28 -0500

    add in component for vehicleList pass props

--- podium-helper/src/components/EntryManager/modal/EditVehicle.js
+++ podium-helper/src/components/EntryManager/modal/EditVehicle.js
@@ -1,12 +1,14 @@
 import Classification from "./Classification";
 import InputContainer from "./InputContainer";
+import VehicleList from "./VehicleList";
 
 const EditVehicle = ({ entry, onInputChange, classification, series }) => {
     return (
         <section className="input_vehicle_container">
             <Classification onInputChange={onInputChange} classification={classification} series={series} />
             <InputContainer val={entry.number} name="number" onInputChange={onInputChange} label="Number" />
-            <InputContainer val={entry.vehicle} name="vehicle" onInputChange={onInputChange} label="Vehicle" />
+            {/*<InputContainer val={entry.vehicle} name="vehicle" onInputChange={onInputChange} label="Vehicle" />*/}
+            <VehicleList series={entry.series} vehicleName={entry.vehicle} classif={entry.classification} />
         </section>
     );
 };
