231d2c631cf4b8a34c4f757e4d8ba9d3f3916c6c
Brendan Woo <woo.brendan@gmail.com>
2024-01-25 17:15:56 -0500

    add props entryEvent and add that to ternerary for usestate eventName. change dependency array value to be dispatch. create handle change func

--- podium-helper/src/components/EntryManager/modal/EventSelect.js
+++ podium-helper/src/components/EntryManager/modal/EventSelect.js
@@ -2,14 +2,18 @@ import { useSelector, useDispatch } from "react-redux";
 import { useState, useEffect } from "react";
 import { fetchEvents } from "../../../store/events/eventActions";
 
-const EventSelect = () => {
+const EventSelect = ({ entryEvent }) => {
     const currentYearEvents = useSelector((state) => state.events.currentYear);
     const dispatch = useDispatch();
-    const [eventName, setEventName] = useState("");
+    const [eventName, setEventName] = useState(entryEvent ? entryEvent : "");
 
     useEffect(() => {
         dispatch(fetchEvents());
-    }, [currentYearEvents]);
+    }, [dispatch]);
+
+    const handleChange = (e) => {
+        setEventName(e.target.value);
+    };
 
     return (
         <div id="class_dropdown">
