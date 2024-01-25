d94d6621510180cab6eccfa61feaabf9f9c542d0
Brendan Woo <woo.brendan@gmail.com>
2024-01-25 16:45:31 -0500

    import div with select for events, import useselector and usedispatch, initialize curerntyearevents and dispatch

--- podium-helper/src/components/EntryManager/modal/EventSelect.js
+++ podium-helper/src/components/EntryManager/modal/EventSelect.js
@@ -1,5 +1,23 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { fetchEvents } from "../../../store/events/eventActions";
+
 const EventSelect = () => {
-    return <></>;
+    const currentYearEvents = useSelector((state) => state.events.currentYear);
+    const dispatch = useDispatch();
+
+    return (
+        <div id="class_dropdown">
+            <label htmlFor="dropdown">Class:</label>
+            <select value={className} onChange={handleChange} name="classification">
+                {currentYearEvents.map((event, index) => (
+                    <option value={event} key={index}>
+                        {event}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
 };
 
 export default EventSelect;
