0bbc950839af3ecd671098c69523973b9cda7593
Brendan Woo <woo.brendan@gmail.com>
2024-01-29 14:51:52 -0500

    update import for convertEntryFormat to come form two up folders. comment out old fetch for entries

--- podium-helper/src/store/entries/entryActions.js
+++ podium-helper/src/store/entries/entryActions.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 import { entryActions } from "./entry_slice";
-import { convertEntryFormat } from "../functions/helperFunc";
+import { convertEntryFormat } from "../../functions/helperFunc";
 
-export const fetchEntry = () => {
-    return async (dispatch) => {
-        try {
-            const entries = await axios.get("http://localhost:2020/entries");
-            dispatch(entryActions.setEntries(entries.data.entry));
-        } catch (err) {
-            console.error(err);
-        }
-    };
-};
+//export const fetchEntry = () => {
+//    return async (dispatch) => {
+//        try {
+//            const entries = await axios.get("http://localhost:2020/entries");
+//            dispatch(entryActions.setEntries(entries.data.entry));
+//        } catch (err) {
+//            console.error(err);
+//        }
+//    };
+//};
 
 export const fetchApiEntry = () => {
     return async (dispatch) => {
