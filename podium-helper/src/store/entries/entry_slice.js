e664ec0f932a58c8f21c28987b7dd35282e4f169
Brendan Woo <woo.brendan@gmail.com>
2024-01-30 15:28:38 -0500

    add new reducer addeventEntries, format indents

--- podium-helper/src/store/entries/entry_slice.js
+++ podium-helper/src/store/entries/entry_slice.js
@@ -2,38 +2,41 @@ import { createSlice } from "@reduxjs/toolkit";
 import { deleteEntry, updateEntry } from "./entryActions";
 
 const entrySlice = createSlice({
-  name: "entries",
-  initialState: {
-    entriesArray: [],
-  },
-  reducers: {
-    setEntries(state, action) {
-      state.entriesArray = action.payload;
+    name: "entries",
+    initialState: {
+        entriesArray: [],
+        eventEntries: [],
     },
+    reducers: {
+        setEntries(state, action) {
+            state.entriesArray = action.payload;
+        },
 
-    addEntry(state, action) {
-      state.entriesArray = [...state.entriesArray, action.payload];
+        setEventEntries(state, action) {
+            state.eventEntries = action.payload;
+        },
+
+        addEntry(state, action) {
+            state.entriesArray = [...state.entriesArray, action.payload];
+        },
+
+        removeEntry(state, action) {
+            const entries = [...state.entriesArray];
+            const adjusted = entries.filter((entry) => entry._id !== action.payload._id);
+            state.entriesArray = adjusted;
+            deleteEntry(action.payload);
+        },
+
+        updateEntry(state, action) {
+            updateEntry(action.payload);
+            const entries = [...state.entriesArray];
+            const newEntries = entries.map((entry) =>
+                entry._id === action.payload._id ? { ...action.payload } : entry,
+            );
+
+            state.entriesArray = [...newEntries];
+        },
     },
-
-    removeEntry(state, action) {
-      const entries = [...state.entriesArray];
-      const adjusted = entries.filter(
-        (entry) => entry._id !== action.payload._id,
-      );
-      state.entriesArray = adjusted;
-      deleteEntry(action.payload);
-    },
-
-    updateEntry(state, action) {
-      updateEntry(action.payload);
-      const entries = [...state.entriesArray];
-      const newEntries = entries.map((entry) =>
-        entry._id === action.payload._id ? { ...action.payload } : entry,
-      );
-
-      state.entriesArray = [...newEntries];
-    },
-  },
 });
 
 export const entryActions = entrySlice.actions;
