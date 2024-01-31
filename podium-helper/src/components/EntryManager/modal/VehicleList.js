d2ff2fcd864b0400ec39ef754cb723322d3b6f66
Brendan Woo <woo.brendan@gmail.com>
2024-01-31 15:22:36 -0500

    remove double under line to fix css targeting for dropdown vehicle id

--- podium-helper/src/components/EntryManager/modal/VehicleList.js
+++ podium-helper/src/components/EntryManager/modal/VehicleList.js
@@ -32,7 +32,7 @@ const VehicleList = ({ series, vehicleName, classif, onChange }) => {
     };
 
     return (
-        <div className="dropdown" id="dropdown__vehicle">
+        <div className="dropdown" id="dropdown_vehicle">
             <label htmlFor="dropdown">Vehicle:</label>
             <select value={vehicle} onChange={handleChange} name="vehicle">
                 <option value="" disabled>
