2c5554f96b3ae8aca2d92940ad124734055c7b54
Brendan Woo <woo.brendan@gmail.com>
2024-02-01 11:13:59 -0500

    inside singledrivers function clean code using deconstruction of entry and pass to each driver or if block

--- podium-helper/src/functions/podiumResultHelpers.js
+++ podium-helper/src/functions/podiumResultHelpers.js
@@ -3,62 +3,64 @@ import WinnerPodium from "../components/Podium_Creation/WinnerPodium";
 
 //determine if entry is single or two drivers and return corresponding menu item
 const numOfDriverMenuItem = (entry, index) => {
-  const { driver1, driver2, driver3, number } = entry;
-  let driverStr = `#${number} - ${driver1.name}`;
-  driverStr += driver2 ? ` & ${driver2.name}` : "";
-  driverStr += driver3 ? ` & ${driver3.name}` : "";
+    const { driver1, driver2, driver3, number } = entry;
+    let driverStr = `#${number} - ${driver1.name}`;
+    driverStr += driver2 ? ` & ${driver2.name}` : "";
+    driverStr += driver3 ? ` & ${driver3.name}` : "";
 
-  return (
-    <MenuItem key={index} value={entry}>
-      {driverStr}
-    </MenuItem>
-  );
+    return (
+        <MenuItem key={index} value={entry}>
+            {driverStr}
+        </MenuItem>
+    );
 };
 
 //dynamically render appropriate amount of WinnerPodium components based on need per series class requirements
 const numOfPodiumDisplays = (series, submit, results, entries) => {
-  const mappedSeries = series.class.map((classification, index) => (
-    <WinnerPodium
-      key={classification}
-      seriesName={series.name}
-      onClick={submit}
-      results={results}
-      classification={classification}
-      resultNum={index + 1}
-      entries={entries}
-    />
-  ));
-  return mappedSeries;
+    const mappedSeries = series.class.map((classification, index) => (
+        <WinnerPodium
+            key={classification}
+            seriesName={series.name}
+            onClick={submit}
+            results={results}
+            classification={classification}
+            resultNum={index + 1}
+            entries={entries}
+        />
+    ));
+    return mappedSeries;
 };
 
 //take in entry object and get every single driver into list. List is used for Fast Lap component
 const singleDrivers = (entryArray, series) => {
-  const drivers = [];
-  const seriesFilteredDrivers = entryArray.filter(
-    (entry) => entry.series === series.name,
-  );
-  for (const entry of seriesFilteredDrivers) {
-    drivers.push({
-      number: entry.number,
-      driver: entry.driver1.name,
-      vehicle: entry.vehicle,
-    });
-    if (entry.driver2) {
-      drivers.push({
-        number: entry.number,
-        driver: entry.driver2.name,
-        vehicle: entry.vehicle,
-      });
+    const drivers = [];
+    const seriesFilteredDrivers = entryArray.filter((entry) => entry.series === series.name);
+    for (const entry of seriesFilteredDrivers) {
+        const { number, vehicle, driver2, driver3 } = entry;
+
+        drivers.push({
+            number,
+            driver: entry.driver1.name,
+            vehicle,
+        });
+
+        if (driver2) {
+            drivers.push({
+                number,
+                driver: driver2.name,
+                vehicle,
+            });
+        }
+
+        if (driver3) {
+            drivers.push({
+                number,
+                driver: driver3.name,
+                vehicle,
+            });
+        }
     }
-    if (entry.driver3) {
-      drivers.push({
-        number: entry.number,
-        driver: entry.driver3.name,
-        vehicle: entry.vehicle,
-      });
-    }
-  }
-  return drivers.sort((a, b) => a.number - b.number);
+    return drivers.sort((a, b) => a.number - b.number);
 };
 
 export { numOfDriverMenuItem, numOfPodiumDisplays, singleDrivers };
