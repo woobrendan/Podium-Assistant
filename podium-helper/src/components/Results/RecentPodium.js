a3eb7a5fc7b17d38bf5f93b94b703277d2af05ee
Brendan Woo <woo.brendan@gmail.com>
2024-02-01 11:01:02 -0500

    update import for fetchData

--- podium-helper/src/components/Results/RecentPodium.js
+++ podium-helper/src/components/Results/RecentPodium.js
@@ -4,24 +4,24 @@ import { printPage } from "../../functions/helperFunc";
 import { Button } from "@mui/material";
 import "../../Styling/result.scss";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchData } from "../../store/resultsActions";
+import { fetchData } from "../../store/results/resultsActions";
 
 const RecentPodium = () => {
-  const dispatch = useDispatch();
-  const recentResult = useSelector((state) => state.results.recentPodium);
+    const dispatch = useDispatch();
+    const recentResult = useSelector((state) => state.results.recentPodium);
 
-  useEffect(() => {
-    dispatch(fetchData());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchData());
+    }, [dispatch]);
 
-  return (
-    <div className="recent-podium-container">
-      <Button onClick={() => printPage()} variant="contained" color="success">
-        Print Page
-      </Button>
-      {recentResult && <ResultTableHeader results={recentResult} />}
-    </div>
-  );
+    return (
+        <div className="recent-podium-container">
+            <Button onClick={() => printPage()} variant="contained" color="success">
+                Print Page
+            </Button>
+            {recentResult && <ResultTableHeader results={recentResult} />}
+        </div>
+    );
 };
 
 export default RecentPodium;
