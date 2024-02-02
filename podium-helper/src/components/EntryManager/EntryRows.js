fd44aa7315affab3208513e38aede8c2dad20ff0
Brendan Woo <woo.brendan@gmail.com>
2024-02-02 12:26:48 -0500

    deconstruct entry values and pass to cells

--- podium-helper/src/components/EntryManager/EntryRows.js
+++ podium-helper/src/components/EntryManager/EntryRows.js
@@ -5,21 +5,22 @@ import EditModal from "./modal/EditModal";
 
 const EntryRows = ({ entry }) => {
     const [showModal, setShowModal] = useState(false);
+    const { number, team, vehicle, classification, event, series, driver1, driver2, driver3 } = entry;
 
     return (
         <>
             <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
                 <TableCell component="th" scope="row">
-                    {shortenName(entry.series)}
+                    {shortenName(series)}
                 </TableCell>
-                <TableCell align="right">{entry.number}</TableCell>
-                <TableCell align="right">{entry.team}</TableCell>
-                <TableCell align="right">{entry.driver1.name}</TableCell>
-                <TableCell align="right">{entry.driver2 ? entry.driver2.name : ""}</TableCell>
-                <TableCell align="right">{entry.driver3 ? entry.driver3.name : ""}</TableCell>
-                <TableCell align="right">{entry.vehicle}</TableCell>
-                <TableCell align="right">{entry.classification}</TableCell>
-                <TableCell align="right">{entry.event}</TableCell>
+                <TableCell align="right">{number}</TableCell>
+                <TableCell align="right">{team}</TableCell>
+                <TableCell align="right">{driver1.name}</TableCell>
+                <TableCell align="right">{driver2 ? driver2.name : ""}</TableCell>
+                <TableCell align="right">{driver3 ? driver3.name : ""}</TableCell>
+                <TableCell align="right">{vehicle}</TableCell>
+                <TableCell align="right">{classification}</TableCell>
+                <TableCell align="right">{event}</TableCell>
                 <TableCell align="right">
                     <Button variant="contained" onClick={() => setShowModal(!showModal)}>
                         Edit
