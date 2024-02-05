f98bd692b16a4fb719992c4ee0a7cd36848c5a5e
Brendan Woo <woo.brendan@gmail.com>
2024-02-05 13:52:40 -0500

    remove useeffect and get and set series function. replace with use selector for series list. remove usestate for series and create variable to determine list

--- podium-helper/src/components/Podium_Creation/Series.js
+++ podium-helper/src/components/Podium_Creation/Series.js
@@ -1,31 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
-import axios from "axios";
-import { sortSeriesNames } from "../../functions/sortFuncs.js";
+import { useSelector } from "react-redux";
 
 const Series = ({ getValue, comp }) => {
     const [seriesName, setSeriesName] = useState("");
-    const [series, setSeries] = useState([]);
 
-    useEffect(() => {
-        const getAndSetSeries = async () => {
-            try {
-                const seriesList = await axios.get(
-                    "http://localhost:2020/api/series",
-                );
-                const sortedSeries =
-                    seriesList.data.series.sort(sortSeriesNames);
-                const list =
-                    comp === "entryManager"
-                        ? ["All", ...sortedSeries]
-                        : sortedSeries;
-                setSeries(list);
-            } catch (err) {
-                console.error(err);
-            }
-        };
-        getAndSetSeries();
-    }, []);
+    const seriesList = useSelector((state) => state.series.seriesArr);
+
+    const series = comp === "entryManager" ? ["All", ...seriesList] : seriesList;
 
     const handleChange = (event) => {
         setSeriesName(event.target.value);
@@ -33,27 +15,16 @@ const Series = ({ getValue, comp }) => {
     };
 
     return (
-        // <Box data-testid="series_dropdown">
         <FormControl className="series_dropdown">
             <InputLabel>Series</InputLabel>
-            <Select
-                name="series"
-                label="Series"
-                value={seriesName}
-                onChange={(e) => handleChange(e)}
-            >
+            <Select name="series" label="Series" value={seriesName} onChange={(e) => handleChange(e)}>
                 {series.map((serie, index) => (
-                    <MenuItem
-                        key={index}
-                        value={serie}
-                        data-testid={serie.name}
-                    >
+                    <MenuItem key={index} value={serie} data-testid={serie.name}>
                         {serie === "All" ? "All" : serie.name}
                     </MenuItem>
                 ))}
             </Select>
         </FormControl>
-        // </Box>
     );
 };
 
