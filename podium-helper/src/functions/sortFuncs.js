84b5988e94652d088d903adb8b8f5ac0e178c558
Brendan Woo <woo.brendan@gmail.com>
2024-02-05 13:54:18 -0500

    remove unused sortseriesbyname function

--- podium-helper/src/functions/sortFuncs.js
+++ podium-helper/src/functions/sortFuncs.js
@@ -139,16 +139,4 @@ const compareResultDates = (a, b) => {
     }
 };
 
-const sortSeriesNames = (a, b) => {
-    if (a.name < b.name) {
-        return -1;
-    }
-
-    if (a.name > b.name) {
-        return 1;
-    }
-
-    return 0;
-};
-
-export { sortBySeries, sortByVehicleType, sortByManufacturer, sortByClass, compareResultDates, sortSeriesNames };
+export { sortBySeries, sortByVehicleType, sortByManufacturer, sortByClass, compareResultDates };
