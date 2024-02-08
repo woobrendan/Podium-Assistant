c15d150492dafb5b9ab5e445bbbd743ede8b3592
Brendan Woo <woo.brendan@gmail.com>
2024-02-08 13:57:56 -0800

    create container unit for classes of hard charger

---
+++ podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
@@ -0,0 +1,8 @@
+const HardChargeContainer = () => {
+    return (
+        <div className="hard_charge_container">
+            <label htmlFor="dropdown">Driver:</label>
+            <select name="hard_drivers"></select>
+        </div>
+    );
+};
