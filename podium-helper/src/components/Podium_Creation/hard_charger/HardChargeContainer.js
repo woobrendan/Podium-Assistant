097264759d62dca6cc4d45e7472352f982938198
Brendan Woo <woo.brendan@gmail.com>
2024-02-08 14:30:34 -0800

    create usestate for hardcharger, pass values to each select and input

--- podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
+++ podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
@@ -1,17 +1,28 @@
+import { useState } from "react";
+
 const HardChargeContainer = () => {
+    const [hardCharger, setHardCharger] = useState({
+        entryNum: "",
+        startPos: "",
+        gain: "",
+        class: "",
+    });
+
     return (
         <div className="hard_charge_container">
             <div className="hard_driver">
                 <label>Driver:</label>
-                <select name="hard_drivers">{/*// loop through entries*/}</select>
+                <select name="hard_drivers" value={hardCharger.entryNum}>
+                    {/*// loop through entries*/}
+                </select>
             </div>
             <div className="hard_start">
                 <label>Start Pos:</label>
-                <input type="text" />
+                <input type="text" value={hardCharger.startPos} />
             </div>
             <div className="hard_gain">
                 <label>Gain:</label>
-                <select name="hard_drivers">{/*// loop through entries*/}</select>
+                <input type="text" value={hardCharger.gain} />
             </div>
         </div>
     );
