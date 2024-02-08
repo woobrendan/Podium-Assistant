7ed5912eeb249d512d50ebf3c17cd492c08ec571
Brendan Woo <woo.brendan@gmail.com>
2024-02-08 14:25:01 -0800

    create divs for drirver select and for start pos and gain. export component

--- podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
+++ podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
@@ -1,8 +1,20 @@
 const HardChargeContainer = () => {
     return (
         <div className="hard_charge_container">
-            <label htmlFor="dropdown">Driver:</label>
-            <select name="hard_drivers"></select>
+            <div className="hard_driver">
+                <label>Driver:</label>
+                <select name="hard_drivers">{/*// loop through entries*/}</select>
+            </div>
+            <div className="hard_start">
+                <label>Start Pos:</label>
+                <input type="text" />
+            </div>
+            <div className="hard_gain">
+                <label>Gain:</label>
+                <select name="hard_drivers">{/*// loop through entries*/}</select>
+            </div>
         </div>
     );
 };
+
+export default HardChargeContainer;
