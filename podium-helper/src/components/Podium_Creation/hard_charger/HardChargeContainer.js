69c7fccb2a69e433fd0146029143ecb203769682
Brendan Woo <woo.brendan@gmail.com>
2024-02-08 16:35:26 -0800

    create handle change function and pass to each select and input

--- podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
+++ podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
@@ -8,21 +8,28 @@ const HardChargeContainer = () => {
         class: "",
     });
 
+    const handleChange = (e) => {
+        setHardCharger((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
     return (
         <div className="hard_charge_container">
             <div className="hard_driver">
                 <label>Driver:</label>
-                <select name="hard_drivers" value={hardCharger.entryNum}>
+                <select name="entryNum" value={hardCharger.entryNum} onChange={handleChange}>
                     {/*// loop through entries*/}
                 </select>
             </div>
             <div className="hard_start">
                 <label>Start Pos:</label>
-                <input type="text" value={hardCharger.startPos} />
+                <input type="text" value={hardCharger.startPos} name="startPos" onChange={handleChange} />
             </div>
             <div className="hard_gain">
                 <label>Gain:</label>
-                <input type="text" value={hardCharger.gain} />
+                <input type="text" value={hardCharger.gain} name="gain" onChange={handleChange} />
             </div>
         </div>
     );
