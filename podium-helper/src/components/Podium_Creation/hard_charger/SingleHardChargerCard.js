afc1e3e897ef1717c76cd8f205076d094a431e97
Brendan Woo <woo.brendan@gmail.com>
2024-02-09 17:17:41 -0800

    copy logic from hard container to use for isngle purpose

---
+++ podium-helper/src/components/Podium_Creation/hard_charger/SingleHardChargerCard.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
+const SingleHardChargerCard = ({ classif, index, series }) => {
+    const eventEntries = useSelector((state) => state.entries.eventEntries);
+    const [hardCharger, setHardCharger] = useState({
+        entryNum: "",
+        startPos: "",
+        gain: "",
+        class: "",
+    });
+
+    const handleChange = (e) => {
+        setHardCharger((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+    return (
+        <div className="hard_charge_container">
+            <div className="hard_driver">
+                <label>Driver:</label>
+                <select name="entryNum" value={hardCharger.entryNum} onChange={handleChange}>
+                    <option value="" disabled>
+                        Select Driver(s)
+                    </option>
+                    {eventEntries
+                        .filter((entry) => entry.series === series.name)
+                        .sort((a, b) => a.number - b.number)
+                        .map((entry, index) => {
+                            return (
+                                <option value={entry.number} key={index}>
+                                    #{entry.number} - {entry.driver1.name}{" "}
+                                    {entry.driver2 ? `& ${entry.driver2.name}` : " "}
+                                </option>
+                            );
+                        })}
+                </select>
+            </div>
+            <div className="hard_start">
+                <label>Start Pos:</label>
+                <input type="text" value={hardCharger.startPos} name="startPos" onChange={handleChange} />
+            </div>
+            <div className="hard_gain">
+                <label>Gain:</label>
+                <input type="text" value={hardCharger.gain} name="gain" onChange={handleChange} />
+            </div>
+        </div>
+    );
+};
+
+export default SingleHardChargerCard;
