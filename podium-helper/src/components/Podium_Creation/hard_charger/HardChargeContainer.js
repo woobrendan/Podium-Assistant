c56ab1ce051d10fb7ad2da3aea6066dc91e5f690
Brendan Woo <woo.brendan@gmail.com>
2024-02-09 17:37:42 -0800

    create hard container value to loop over hard charger cards. create handle submit function

--- podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
+++ podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
@@ -1,24 +1,13 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import SingleHardChargerCard from "./SingleHardChargerCard";
 
 const HardChargeContainer = ({ series, resultNum }) => {
     const eventEntries = useSelector((state) => state.entries.eventEntries);
-    const [hardCharger, setHardCharger] = useState({
-        entryNum: "",
-        startPos: "",
-        gain: "",
-        class: "",
-    });
-
-    const handleChange = (e) => {
-        setHardCharger((prev) => ({
-            ...prev,
-            [e.target.name]: e.target.value,
-        }));
-    };
+    const [allHardChargers, setAllHardChargers] = useState({});
 
     //handle submit pass back result number
-    const classes = null;
+    let classes = null;
     if (series.name === "GT America") {
         classes = series.class.slice(0, -1);
     } else if (series.name === "TC America") {
@@ -28,40 +17,22 @@ const HardChargeContainer = ({ series, resultNum }) => {
     //    classes = series.name === "Pirelli GT4 America" ? ["GT4"] : ["GT3"];
     //}
 
-    //   const display = classes.map((classif, index) => (
-    //
-    //   ))
+    const handleSingleSubmit = (resultNumber, value) => {
+        setAllHardChargers((prev) => ({
+            ...prev,
+            [`hardCharger${resultNumber}`]: value,
+        }));
+    };
+
+    const hardContainers = classes.map((classif, index) => (
+        <SingleHardChargerCard classif={classif} resultNum={index} onSubmit={handleSingleSubmit} />
+    ));
 
     return (
-        <div className="hard_charge_container">
-            <div className="hard_driver">
-                <label>Driver:</label>
-                <select name="entryNum" value={hardCharger.entryNum} onChange={handleChange}>
-                    <option value="" disabled>
-                        Select Driver(s)
-                    </option>
-                    {eventEntries
-                        .filter((entry) => entry.series === series.name)
-                        .sort((a, b) => a.number - b.number)
-                        .map((entry, index) => {
-                            return (
-                                <option value={entry.number} key={index}>
-                                    #{entry.number} - {entry.driver1.name}{" "}
-                                    {entry.driver2 ? `& ${entry.driver2.name}` : " "}
-                                </option>
-                            );
-                        })}
-                </select>
-            </div>
-            <div className="hard_start">
-                <label>Start Pos:</label>
-                <input type="text" value={hardCharger.startPos} name="startPos" onChange={handleChange} />
-            </div>
-            <div className="hard_gain">
-                <label>Gain:</label>
-                <input type="text" value={hardCharger.gain} name="gain" onChange={handleChange} />
-            </div>
-        </div>
+        <>
+            {hardContainers()}
+            {/* Button to submit */}
+        </>
     );
 };
 
