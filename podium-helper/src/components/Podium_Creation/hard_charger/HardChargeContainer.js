e22c01d3e628252958a86ffb531fcdd6ca0eafae
Brendan Woo <woo.brendan@gmail.com>
2024-02-09 17:05:52 -0800

    import event entries from redux store, then loop through entires, filter out series and sort nums, pass to options for select

--- podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
+++ podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const HardChargeContainer = ({ series, resultNum }) => {
+    const eventEntries = useSelector((state) => state.entries.eventEntries);
     const [hardCharger, setHardCharger] = useState({
         entryNum: "",
         startPos: "",
@@ -21,9 +23,10 @@ const HardChargeContainer = ({ series, resultNum }) => {
         classes = series.class.slice(0, -1);
     } else if (series.name === "TC America") {
         classes = series.class;
-    } else {
-        classes = series.name === "Pirelli GT4 America" ? ["GT4"] : ["GT3"];
     }
+    //} else {
+    //    classes = series.name === "Pirelli GT4 America" ? ["GT4"] : ["GT3"];
+    //}
 
     //   const display = classes.map((classif, index) => (
     //
@@ -34,7 +37,20 @@ const HardChargeContainer = ({ series, resultNum }) => {
             <div className="hard_driver">
                 <label>Driver:</label>
                 <select name="entryNum" value={hardCharger.entryNum} onChange={handleChange}>
-                    {/*// loop through entries*/}
+                    <option value="" disabled>
+                        Select Driver(s)
+                    </option>
+                    {eventEntries
+                        .filter((entry) => entry.series === series.name)
+                        .sort((a, b) => a.number - b.number)
+                        .map((entry, index) => {
+                            return (
+                                <option value={entry.number} key={index}>
+                                    #{entry.number} - {entry.driver1.name}{" "}
+                                    {entry.driver2 ? `& ${entry.driver2.name}` : " "}
+                                </option>
+                            );
+                        })}
                 </select>
             </div>
             <div className="hard_start">
