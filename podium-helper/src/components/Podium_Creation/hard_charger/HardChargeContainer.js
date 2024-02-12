b0bb7f1ab8bc11f69b008775ed462b76e0f44037
Brendan Woo <woo.brendan@gmail.com>
2024-02-12 11:57:19 -0800

    add logic from podiumresulthelper file and make container file

---
+++ podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
@@ -0,0 +1,36 @@
+import SingleHardChargerCard from "./SingleHardChargerCard";
+import { grCup } from "../../../functions/helperFunc";
+import { Card } from "@mui/material";
+
+const HardChargeContainer = (series, onSubmit) => {
+    let classes = null;
+    if (series.name === "GT America") {
+        classes = series.class.slice(0, -1);
+    } else if (series.name === "TC America") {
+        classes = series.class;
+    } else {
+        classes = series.name === "Pirelli GT4 America" ? ["GT4"] : ["GT3"];
+    }
+    const mappedHardCharge = classes.map((classif, index) => (
+        <SingleHardChargerCard
+            key={index}
+            classif={classif}
+            resultNum={index + 1}
+            onSubmit={onSubmit}
+            series={series}
+        />
+    ));
+
+    //return mappedHardCharge;
+    return (
+        <div className="results-container">
+            <Card className="podium_card">
+                <h2>{series.name === grCup ? "Hard Charger" : "E-Boost Hard Charger"}</h2>
+
+                {mappedHardCharge}
+            </Card>
+        </div>
+    );
+};
+
+export default HardChargeContainer;
