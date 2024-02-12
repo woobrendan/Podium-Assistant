dd6cb0c15d46dac202ca336b1b153c35b4d40db0
Brendan Woo <woo.brendan@gmail.com>
2024-02-12 11:40:01 -0800

    create numOfHardChargeDisplays to get classes array, then return mappedhardchage

--- podium-helper/src/functions/podiumResultHelpers.js
+++ podium-helper/src/functions/podiumResultHelpers.js
@@ -1,5 +1,6 @@
 import { MenuItem } from "@mui/material";
 import WinnerPodium from "../components/Podium_Creation/WinnerPodium";
+import SingleHardChargerCard from "../components/Podium_Creation/hard_charger/SingleHardChargerCard";
 
 //determine if entry is single or two drivers and return corresponding menu item
 const numOfDriverMenuItem = (entry, index) => {
@@ -29,6 +30,28 @@ const numOfPodiumDisplays = (series, submit) => {
     return mappedSeries;
 };
 
+const numOfHardChargeDisplays = (series, onSubmit) => {
+    let classes = null;
+    if (series.name === "GT America") {
+        classes = series.class.slice(0, -1);
+    } else if (series.name === "TC America") {
+        classes = series.class;
+    } else {
+        classes = series.name === "Pirelli GT4 America" ? ["GT4"] : ["GT3"];
+    }
+    const mappedHardCharge = classes.map((classif, index) => (
+        <SingleHardChargerCard
+            key={index}
+            classif={classif}
+            resultNum={index + 1}
+            onSubmit={onSubmit}
+            series={series}
+        />
+    ));
+
+    return mappedHardCharge;
+};
+
 //take in entry object and get every single driver into list. List is used for Fast Lap component
 const singleDrivers = (entryArray, seriesVal) => {
     const drivers = entryArray
@@ -45,6 +68,4 @@ const singleDrivers = (entryArray, seriesVal) => {
     return drivers.sort((a, b) => a.number - b.number);
 };
 
-//const numOfHardChargeDisplays = (series, onSubmit, )
-
-export { numOfDriverMenuItem, numOfPodiumDisplays, singleDrivers };
+export { numOfDriverMenuItem, numOfPodiumDisplays, singleDrivers, numOfHardChargeDisplays };
