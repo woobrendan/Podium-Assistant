cd8d644b3302cd229a0c9b3f8414ab03bf01a3d4
Brendan Woo <woo.brendan@gmail.com>
2024-02-14 12:44:33 -0800

    create model for multi hard charger and export interface

--- express_mongo/models/result_models.ts
+++ express_mongo/models/result_models.ts
@@ -1,41 +1,48 @@
 import { EntryInterface } from "./models";
 
 export interface FastLapInterface {
-  driver: string;
-  entry: EntryInterface;
-  laptime: string;
+    driver: string;
+    entry: EntryInterface;
+    laptime: string;
 }
 
 export interface HardChargerInterface {
-  entry: EntryInterface;
-  gain: number;
-  startPos: number;
+    entry: EntryInterface;
+    gain: number;
+    startPos: number;
+}
+
+export interface MultiHardCharger {
+    entry: EntryInterface;
+    gain: number;
+    startPos: number;
+    class: string;
 }
 
 interface EntryResultInterfance {
-  team: string;
-  vehicle: string;
-  number: string;
-  driver1: string;
-  driver2?: string;
-  driver3?: string;
+    team: string;
+    vehicle: string;
+    number: string;
+    driver1: string;
+    driver2?: string;
+    driver3?: string;
 }
 
 export interface ResultInterface {
-  class: string;
-  firstPlace: EntryResultInterfance;
-  secondPlace?: EntryResultInterfance;
-  thirdPlace?: EntryResultInterfance;
+    class: string;
+    firstPlace: EntryResultInterfance;
+    secondPlace?: EntryResultInterfance;
+    thirdPlace?: EntryResultInterfance;
 }
 
 export interface FullResultInterface {
-  series: string;
-  date: string;
-  event: string;
-  fastLap: FastLapInterface;
-  hardCharger?: HardChargerInterface;
-  result1: ResultInterface;
-  result2?: ResultInterface;
-  result3?: ResultInterface;
-  result4?: ResultInterface;
+    series: string;
+    date: string;
+    event: string;
+    fastLap: FastLapInterface;
+    hardCharger?: HardChargerInterface;
+    result1: ResultInterface;
+    result2?: ResultInterface;
+    result3?: ResultInterface;
+    result4?: ResultInterface;
 }
