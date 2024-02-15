6372096b4aacebda75bb825befecbcc86babde72
Brendan Woo <woo.brendan@gmail.com>
2024-02-14 16:43:24 -0800

    use useselector to get event entries, change class.map to be classes foreach, then create logic to get entries based on series, then pass as new prop to singlehardchar cards

--- podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
+++ podium-helper/src/components/Podium_Creation/hard_charger/HardChargeContainer.js
@@ -1,8 +1,11 @@
 import SingleHardChargerCard from "./SingleHardChargerCard";
 import { grCup } from "../../../functions/helperFunc";
 import { Card } from "@mui/material";
+import { useSelector } from "react-redux";
 
 const HardChargeContainer = ({ series, onSubmit }) => {
+    let eventEntries = useSelector((state) => state.entry.eventEntries);
+
     let classes = null;
     if (series.name === "GT America") {
         classes = series.class.slice(0, -1);
@@ -11,22 +14,35 @@ const HardChargeContainer = ({ series, onSubmit }) => {
     } else {
         classes = series.name === "Pirelli GT4 America" ? ["GT4"] : ["GT3"];
     }
-    const mappedHardCharge = classes.map((classif, index) => (
-        <SingleHardChargerCard
-            key={index}
-            classif={classif}
-            resultNum={index + 1}
-            onSubmit={onSubmit}
-            series={series}
-        />
-    ));
+
+    const hardChargeCards = [];
+
+    classes.forEach((classif, index) => {
+        let entries = eventEntries;
+        if (series.name === "GT World Challenge America" || series.name === "Pirelli GT4 America") {
+            entries = eventEntries.filter((entry) => entry.series === series.name);
+        } else {
+            entries = eventEntries.filter((entry) => entry.series === series.name && entry.classification === classif);
+        }
+
+        hardChargeCards.push(
+            <SingleHardChargerCard
+                key={index}
+                classif={classif}
+                resultNum={index + 1}
+                onSubmit={onSubmit}
+                series={series}
+                entries={entries}
+            />,
+        );
+    });
 
     //return mappedHardCharge;
     return (
         <div className="results-container">
             <Card className="podium_card">
                 <h2>{series.name === grCup ? "Hard Charger" : "E-Boost Hard Charger"}</h2>
-                <div className="hard_charge_container">{mappedHardCharge}</div>
+                <div className="hard_charge_container">{hardChargeCards}</div>
             </Card>
         </div>
     );
