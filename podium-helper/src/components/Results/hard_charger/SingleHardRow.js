37d59e549797d0729f2a0b2f9cfec039a5cbcc21
Brendan Woo <woo.brendan@gmail.com>
2024-02-16 17:21:50 -0800

    create propr hardCharger and deconstruct vvalues. create driverstr val and implment tablerow logic

--- podium-helper/src/components/Results/hard_charger/SingleHardRow.js
+++ podium-helper/src/components/Results/hard_charger/SingleHardRow.js
@@ -1,48 +1,42 @@
-import { TableCell, TableRow, TableBody } from "@mui/material";
+import { TableCell, TableRow } from "@mui/material";
 
-const SingleHardRow = () => {
+const SingleHardRow = ({ hardCharger }) => {
+    const { gain, entry, startPos } = hardCharger;
+
+    let driverStr = `${entry.driver1.name}`;
+    driverStr += entry.driver2 ? ` & ${entry.driver2.name}` : "";
+    driverStr += entry.driver3 ? ` & ${entry.driver3.name}` : "";
+    const finishPos = startPos - gain;
+
+    const hardClass = hardCharger.class;
     return (
-        <TableBody>
-            <TableRow>
-                <TableCell scope="row" colSpan={1}>
-                    {hardClass}
-                </TableCell>
-                <TableCell align="left" colSpan={hardClass ? 1 : 2}>
-                    {driverStr}
-                </TableCell>
-                <TableCell align="left" colSpan={1}>
-                    #{number}
-                </TableCell>
+        <TableRow>
+            <TableCell scope="row" colSpan={1}>
+                {hardClass}
+            </TableCell>
+            <TableCell align="left" colSpan={hardClass ? 1 : 2}>
+                {driverStr}
+            </TableCell>
+            <TableCell align="left" colSpan={1}>
+                #{entry.number}
+            </TableCell>
 
-                {startPos ? (
-                    <>
-                        <TableCell align="right" colSpan={1}>
-                            {startPos}
-                        </TableCell>
-                        <TableCell align="right" colSpan={1}>
-                            {finishPos}
-                        </TableCell>
-                    </>
-                ) : (
-                    <>
-                        <TableCell align="left" colSpan={2}>
-                            {team}
-                        </TableCell>
-                        <TableCell align="left" colSpan={1}>
-                            {vehicle}
-                        </TableCell>
-                    </>
-                )}
-                {entry.driver3 && (
-                    <TableCell align="right" colSpan={1}>
-                        {}
-                    </TableCell>
-                )}
+            <TableCell align="right" colSpan={1}>
+                {startPos}
+            </TableCell>
+            <TableCell align="right" colSpan={1}>
+                {finishPos}
+            </TableCell>
+
+            {entry.driver3 && (
                 <TableCell align="right" colSpan={1}>
-                    +{gain}
+                    {}
                 </TableCell>
-            </TableRow>
-        </TableBody>
+            )}
+            <TableCell align="right" colSpan={1}>
+                +{gain}
+            </TableCell>
+        </TableRow>
     );
 };
 
