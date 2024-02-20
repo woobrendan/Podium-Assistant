97159b571270c3756b17219ba930b5e41567efb9
Brendan Woo <woo.brendan@gmail.com>
2024-02-20 13:57:33 -0800

    create fastLapcardsand  passthrough fucntionsto getsingleFastLap Cad

--- podium-helper/src/components/Podium_Creation/fast_lap/FastLapContainer.js
+++ podium-helper/src/components/Podium_Creation/fast_lap/FastLapContainer.js
@@ -1,12 +1,30 @@
 import { Card } from "@mui/material";
 import { grCup } from "../../../functions/helperFunc";
+import { useSelector } from "react-redux";
+import SingleFastLapCard from "./SingleFastLapCard";
+import { getAwardEntries, getClassArr } from "../../../functions/podiumResultHelpers";
 
 const FastLapContainer = ({ series }) => {
+    const eventEntries = useSelector((state) => state.entry.eventEntries);
+
+    const fastLapCards = getClassArr(series).map((classif, index) => {
+        const entries = getAwardEntries(eventEntries, series, classif);
+
+        return (
+            <SingleFastLapCard
+                key={index}
+                classif={classif}
+                resultNum={index + 1}
+                //onSubmit={onSubmit}
+                entries={entries}
+            />
+        );
+    });
     return (
         <div className="results-container">
             <Card className="podium_card">
                 <h2>{series.name === grCup ? "Fast Lap" : "CrowdStrike Fast Lap"}</h2>
-                <div className="fast_lap_container">{/* fast lap cards */}</div>
+                <div className="fast_lap_container">{fastLapCards}</div>
             </Card>
         </div>
     );
