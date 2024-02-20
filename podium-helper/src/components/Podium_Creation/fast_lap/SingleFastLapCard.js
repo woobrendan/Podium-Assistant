ac61a1bbb1c324e24b9a15e06940860d48a0aaad
Brendan Woo <woo.brendan@gmail.com>
2024-02-20 12:34:59 -0800

    import suestate and set up submitted anffast lapstate.create handlechange func

--- podium-helper/src/components/Podium_Creation/fast_lap/SingleFastLapCard.js
+++ podium-helper/src/components/Podium_Creation/fast_lap/SingleFastLapCard.js
@@ -1,4 +1,18 @@
+import { useState } from "react";
+
 const SingleFastLapCard = () => {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [fastLap, setFastLap] = useState({
+        driver: "",
+        laptime: "",
+    });
+
+    const handleChange = (e) => {
+        setFastLap((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
     return <></>;
 };
 
