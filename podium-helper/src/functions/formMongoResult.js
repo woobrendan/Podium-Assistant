2a3511018f0355b449f09ea4c08ed62fe5dfea33
Brendan Woo <woo.brendan@gmail.com>
2024-02-22 16:03:58 -0800

    rermove esult from param. add in fastlap 1-3 into deconstruction, and add logic to spread out fastlap results

--- podium-helper/src/functions/formMongoResult.js
+++ podium-helper/src/functions/formMongoResult.js
@@ -1,16 +1,30 @@
-const mongoResult = (results, fastLap) => {
-    const { result1, result2, result3, result4, hardCharge1, hardCharge2, hardCharge3, series } = results;
+const mongoResult = (results) => {
+    const {
+        result1,
+        result2,
+        result3,
+        result4,
+        hardCharge1,
+        hardCharge2,
+        hardCharge3,
+        series,
+        fastLap1,
+        fastLap2,
+        fastLap3,
+    } = results;
     return {
         ...results,
         series: series.name,
-        fastLap: { ...fastLap, entry: { ...fastLap.entry } },
-        ...(hardCharge1 ? { hardCharge1: hardCharge1 } : {}),
-        ...(hardCharge2 ? { hardCharge2: hardCharge2 } : {}),
-        ...(hardCharge3 ? { hardCharge3: hardCharge3 } : {}),
         result1: buildFullResult(result1),
         ...(result2 ? { result2: buildFullResult(result2) } : {}),
         ...(result3 ? { result3: buildFullResult(result3) } : {}),
         ...(result4 ? { result4: buildFullResult(result4) } : {}),
+        ...(hardCharge1 ? { hardCharge1: hardCharge1 } : {}),
+        ...(hardCharge2 ? { hardCharge2: hardCharge2 } : {}),
+        ...(hardCharge3 ? { hardCharge3: hardCharge3 } : {}),
+        fastLap1,
+        ...(fastLap2 ? { fastLap2: fastLap2 } : {}),
+        ...(fastLap3 ? { fastLap3: fastLap3 } : {}),
     };
 };
 
