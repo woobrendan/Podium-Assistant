6c5a74f86b25eec737ecece575ae3814d37108da
Brendan Woo <woo.brendan@gmail.com>
2024-02-23 15:01:21 -0800

    deconstruct fast lap 1-3, then create var allfastlaps then pass to fastlapheaeder component

--- podium-helper/src/components/Results/ResultTableHeader.js
+++ podium-helper/src/components/Results/ResultTableHeader.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { Table, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
 import ResultTableBody from "./ResultTableBody";
-import FastLapTable from "./FastLapTable";
+//import FastLapTable from "./FastLapTable";
 import "../../Styling/result.scss";
 import { dateToString } from "../../functions/dateFuncs";
 import MultiHardHeader from "./hard_charger/MultiHardHeader";
+import FastLapHeader from "./fast_lap/fastLapHeader";
 
 const ResultTableHeader = ({ results }) => {
-    const { event, series, date, fastLap, result1, result2, result3, result4, hardCharge1, hardCharge2, hardCharge3 } =
-        results;
+    const {
+        event,
+        series,
+        date,
+        fastLap1,
+        fastLap2,
+        fastLap3,
+        result1,
+        result2,
+        result3,
+        result4,
+        hardCharge1,
+        hardCharge2,
+        hardCharge3,
+    } = results;
 
     const { driver2, driver3 } = result1.firstPlace;
     const allHardCharger = { hardCharge1, hardCharge2, hardCharge3 };
+    const allFastLaps = { fastLap1, fastLap2, fastLap3 };
 
     return (
         <TableContainer component={Paper} id="result-table-container">
@@ -44,7 +59,8 @@ const ResultTableHeader = ({ results }) => {
                 {result2 && <ResultTableBody results={result2} />}
                 {result3 && <ResultTableBody results={result3} />}
                 {result4 && <ResultTableBody results={result4} />}
-                <FastLapTable fastLap={fastLap} series={series} />
+                {/*<FastLapTable fastLap={fastLap} series={series} />*/}
+                <FastLapHeader allFastLaps={allFastLaps} />
                 {hardCharge1 && <MultiHardHeader allHardCharger={allHardCharger} />}
             </Table>
         </TableContainer>
