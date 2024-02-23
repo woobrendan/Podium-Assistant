ac8633fa36072160ca064065b9233b35a2898ff1
Brendan Woo <woo.brendan@gmail.com>
2024-02-23 15:19:58 -0800

    fix header to have only fast lap and drover/time. create mapped fast lap ows

--- podium-helper/src/components/Results/fast_lap/FastLapHeader.js
+++ podium-helper/src/components/Results/fast_lap/FastLapHeader.js
@@ -1,5 +1,6 @@
 import { TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import { grCup } from "../../../functions/helperFunc";
+import SingleFastLap from "./SingleFastLap";
 
 const FastLapHeader = ({ allFastLaps }) => {
     const { fastLap1, fastLap2, fastLap3 } = allFastLaps;
@@ -7,35 +8,22 @@ const FastLapHeader = ({ allFastLaps }) => {
     const { series } = entry;
     const fastLapVals = [fastLap1, fastLap2, fastLap3].filter(Boolean);
 
-    //const mappedFastLapRows = fastLapVals.map()
+    const mappedFastLapRows = fastLapVals.map((result, index) => <SingleFastLap fastLap={result} key={index} />);
 
     return (
         <>
             <TableHead className="table_header">
                 <TableRow>
-                    <TableCell colSpan={entry.driver3 ? 7 : 6} align="center">
+                    <TableCell colSpan={entry.driver3 ? 3 : 2}>Driver</TableCell>
+                    <TableCell colSpan={2} align="center">
                         {series === grCup ? "Fast Lap" : "CrowdStrike Fast Lap"}
                     </TableCell>
-                </TableRow>
-            </TableHead>
-            <TableHead>
-                <TableRow>
-                    <TableCell colSpan={1}>Class</TableCell>
-                    <TableCell colSpan={1}>Driver</TableCell>
-                    <TableCell colSpan={1}>Car #</TableCell>
-                    <TableCell align="right" colSpan={1}>
-                        Start Position
-                    </TableCell>
-                    <TableCell align="right" colSpan={1}>
-                        Finish Position
-                    </TableCell>
-
-                    <TableCell align="right" colSpan={entry.driver3 ? 2 : 1}>
-                        Positions Gained
+                    <TableCell align="right" colSpan={2}>
+                        Lap Time
                     </TableCell>
                 </TableRow>
             </TableHead>
-            {/*<TableBody>{mappedHardRows}</TableBody>*/}
+            <TableBody>{mappedFastLapRows}</TableBody>
         </>
     );
 };
