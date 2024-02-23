ff7694daf1c2308f13b32ee407dde95e8a1a4319
Brendan Woo <woo.brendan@gmail.com>
2024-02-23 14:57:37 -0800

    copy in template from hard charger tmplate. import gr cup and set dynamic header

--- podium-helper/src/components/Results/fast_lap/FastLapHeader.js
+++ podium-helper/src/components/Results/fast_lap/FastLapHeader.js
@@ -1,5 +1,36 @@
+import { TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { grCup } from "../../../functions/helperFunc";
+
 const FastLapHeader = () => {
-    return <></>;
+    return (
+        <>
+            <TableHead className="table_header">
+                <TableRow>
+                    <TableCell colSpan={entry.driver3 ? 7 : 6} align="center">
+                        {series === grCup ? "Fast Lap" : "CrowdStrike Fast Lap"}
+                    </TableCell>
+                </TableRow>
+            </TableHead>
+            <TableHead>
+                <TableRow>
+                    <TableCell colSpan={1}>Class</TableCell>
+                    <TableCell colSpan={1}>Driver</TableCell>
+                    <TableCell colSpan={1}>Car #</TableCell>
+                    <TableCell align="right" colSpan={1}>
+                        Start Position
+                    </TableCell>
+                    <TableCell align="right" colSpan={1}>
+                        Finish Position
+                    </TableCell>
+
+                    <TableCell align="right" colSpan={entry.driver3 ? 2 : 1}>
+                        Positions Gained
+                    </TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>{mappedHardRows}</TableBody>
+        </>
+    );
 };
 
 export default FastLapHeader;
