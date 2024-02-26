16efd5b10fd09acb6584ee809cbfb182e2e6f0dd
Brendan Woo <woo.brendan@gmail.com>
2024-02-26 15:25:49 -0800

    add var for laptime,  entry and drivers and drierstring. create tablerrow and cell

--- podium-helper/src/components/Results/fast_lap/SingleFastLap.js
+++ podium-helper/src/components/Results/fast_lap/SingleFastLap.js
@@ -1,5 +1,17 @@
+import { TableCell, TableRow } from "@mui/material";
+
 const SingleFastLap = ({ fastLap }) => {
-    return <></>;
+    const { laptime, entry } = fastLap;
+    const { driver1, driver2, driver3 } = entry;
+
+    const drivers = [driver1, driver2, driver3].filter(Boolean).map((driver) => driver.name);
+    const driverStr = drivers.join(" & ");
+
+    return (
+        <TableRow>
+            <TableCell>{driverStr}</TableCell>
+        </TableRow>
+    );
 };
 
 export default SingleFastLap;
