9c0700180fe762ef2a2e6e2998730f4bb46043ef
Brendan Woo <woo.brendan@gmail.com>
2024-05-09 13:57:19 -0400

    eslint file

--- express_mongo/models/events_schema.ts
+++ express_mongo/models/events_schema.ts
@@ -1,19 +1,19 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 interface Events {
-  name: string;
-  date: string;
-  year: number;
+	name: string;
+	date: string;
+	year: number;
 }
 
 export interface eventModel extends Events, Document {}
 
 const eventSchema: Schema = new Schema({
-  name: { type: String, required: true },
-  date: { type: String, required: true },
-  startDate: {type: String},
-  endDate: {type: String},
-  year: { type: Number, required: true },
+	name: { type: String, required: true },
+	date: { type: String, required: true },
+	startDate: { type: String },
+	endDate: { type: String },
+	year: { type: Number, required: true },
 });
 
 export default mongoose.model<eventModel>("Event", eventSchema);
