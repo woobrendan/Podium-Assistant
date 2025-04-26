5e5b64984111411ffb4faabcf92a40e8aa3d42fe
Brendan Woo <woo.brendan@gmail.com>
2025-04-26 05:09:17 -0400

    set results interfaces to have fast lap be optional value to be passed

--- express_mongo/models/result_models.ts
+++ express_mongo/models/result_models.ts
@@ -1,46 +1,46 @@
 import { EntryInterface } from "./models";
 
 export interface MultiFastLap {
-    entry: EntryInterface;
-    laptime: string;
-    driver: string;
+	entry: EntryInterface;
+	laptime: string;
+	driver: string;
 }
 
 export interface MultiHardCharge {
-    entry: EntryInterface;
-    gain: number;
-    startPos: number;
-    class: string;
+	entry: EntryInterface;
+	gain: number;
+	startPos: number;
+	class: string;
 }
 
 interface EntryResultInterfance {
-    team: string;
-    vehicle: string;
-    number: string;
-    driver1: string;
-    driver2?: string;
-    driver3?: string;
+	team: string;
+	vehicle: string;
+	number: string;
+	driver1: string;
+	driver2?: string;
+	driver3?: string;
 }
 
 export interface ResultInterface {
-    class: string;
-    firstPlace: EntryResultInterfance;
-    secondPlace?: EntryResultInterfance;
-    thirdPlace?: EntryResultInterfance;
+	class: string;
+	firstPlace: EntryResultInterfance;
+	secondPlace?: EntryResultInterfance;
+	thirdPlace?: EntryResultInterfance;
 }
 
 export interface FullResultInterface {
-    series: string;
-    date: string;
-    event: string;
-    fastLap1: MultiFastLap;
-    fastLap2: MultiFastLap;
-    fastLap3: MultiFastLap;
-    result1: ResultInterface;
-    result2?: ResultInterface;
-    result3?: ResultInterface;
-    result4?: ResultInterface;
-    hardCharge1?: MultiHardCharge;
-    hardCharge2?: MultiHardCharge;
-    hardCharge3?: MultiHardCharge;
+	series: string;
+	date: string;
+	event: string;
+	fastLap1?: MultiFastLap;
+	fastLap2?: MultiFastLap;
+	fastLap3?: MultiFastLap;
+	result1: ResultInterface;
+	result2?: ResultInterface;
+	result3?: ResultInterface;
+	result4?: ResultInterface;
+	hardCharge1?: MultiHardCharge;
+	hardCharge2?: MultiHardCharge;
+	hardCharge3?: MultiHardCharge;
 }
